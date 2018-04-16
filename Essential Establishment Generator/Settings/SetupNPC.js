fcf07cf40ce3d6631f00ab746a62c91ed4251671
ryceg <rhysg3@gmail.com>
2018-04-16 18:09:22 +1000

    ;

--- Essential Establishment Generator/Settings/SetupNPC.js
+++ Essential Establishment Generator/Settings/SetupNPC.js
@@ -1,5 +1,5 @@
 setup.createNPC = function() {
-	var gender = ["man", "woman"],
+	var gender = ["man", "woman"];
   var race = ["human", "human", "human", "human", "human", "human", "half-elf", "half-elf", "elf", "elf", "dwarf", "dwarf", "gnome", "halfling", "half-orc", "dragonborn", "tiefling"];
   var firstname;
 	var age =  ["childlike", "rather young", "eighteen year old", "surprisingly young", "relatively young", "relatively young", "middle aged", "middle aged", "middle aged", "middle aged", "mid aged", "relatively old", "sun wizened", "quite old", "ancient"];
