c9599cea51d73f8b03199ac4cd6dc4fe3c82e069
ryceg <rhysg3@gmail.com>
2018-04-18 16:59:41 +1000

    oops

--- Essential Establishment Generator/Settings/SetupBuilding.js
+++ Essential Establishment Generator/Settings/SetupBuilding.js
@@ -3,6 +3,9 @@ setup.createBuilding = function() {
     var purpose           = ["home", "barber", "alchemist's shop", "blacksmithery", "temple", "tannery", "butchery", "library"];
     var outside           = ["a horse grazing on the bushes nearby", "a rusted shovel near a somewhat overgrown flowerbed", "a well with an old rope, but no bucket to go on the end", "a dog panting by the door", "a cat lazily lounging in the shade", "a muddy pair of boots by the door", "a sign from the local paper which reads '$newspaperheadline'"];
     var disrepair         = ["dilapidated", "ruined", "old", "somewhat rickety", "well-loved", "well-kept", "brand new"];
+    var road1             = ["Castle", "Keep", "Kings", "Queens", "Prince," "Princess", "Lords", "Ladies", "Noble", "Duke", "Duchess", "Rogue", "Priest", "Abbott", "Pope", "Spring", "Winter", "Summer", "Autumn", "Butcher", "Tailor", "Smith", "Potter", "Baker", "Farrier", "Old", "New", "Common", "Main", "High", "Low", "Butcher", "Tailor", "Smith", "Potter", "Baker", "Farrier", "Old", "New", "Common", "Main", "High", "Low", "North", "South", "West", "East"].random();
+    var road2             = ["Street", "Street", "Street", "Street", "Lane", "Lane", "Lane", "Road", "Road", "Road", "Road", "Square", "Square", "Market", "Way", "Crescent", "Close", "Wynd", "Row"].random();
+    var road              = road1 + " " + road2;
     var magicroll         = Math.floor(Math.random() * 80) + 20;
     var pricemodifier     = Math.floor(Math.random() * 10) - 10;
     var sizeroll          = Math.floor(Math.random() * 80) + 20;
@@ -23,6 +26,9 @@ setup.createBuilding = function() {
         purpose           : purpose.random(),
         outside           : outside.random(),
         disrepair         : disrepair.random(),
+        road1             : road1,
+        road2             : road2,
+        road             : road,
         wealthroll        : wealthroll,
         magicroll         : magicroll,
         pricemodifier     : pricemodifier,
