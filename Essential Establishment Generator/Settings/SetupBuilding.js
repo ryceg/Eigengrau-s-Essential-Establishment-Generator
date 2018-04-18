662b431fd2b0a5d128c2200fe41d274e067a46ac
ryceg <rhysg3@gmail.com>
2018-04-18 12:24:43 +1000

    smithy

--- Essential Establishment Generator/Settings/SetupBuilding.js
+++ Essential Establishment Generator/Settings/SetupBuilding.js
@@ -1,5 +1,5 @@
 setup.createBuilding = function() {
-    var material          = ["wooden", "wooden", "wooden", "wooden", "wooden", "stone", "stone", "stone", "stone", "hewn rock", "chiseled stone"];
+    var material          = ["wooden", "wooden", "wooden", "wooden", "wooden", "stone", "stone", "stone", "stone", "hewn rock", "chiseled stone", "wooden", "wooden", "wooden", "wooden", "wooden", "stone", "stone", "stone", "stone", "hewn rock", "chiseled stone", "marble"];
     var purpose           = ["home", "barber", "alchemist's shop", "blacksmithery", "temple", "tannery", "butchery", "library"];
     var outside           = ["a horse grazing on the bushes nearby", "a rusted shovel near a somewhat overgrown flowerbed", "a well with an old rope, but no bucket to go on the end", "a dog panting by the door", "a cat lazily lounging in the shade", "a muddy pair of boots by the door", "a sign from the local paper which reads '$newspaperheadline'"];
     var disrepair         = ["dilapidated", "ruined", "old", "somewhat rickety", "well-loved", "well-kept", "brand new"];
