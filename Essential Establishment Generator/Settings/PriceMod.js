5647947015c3db90b3b314e76ea8c2abe58e9b60
ryceg <rhysg3@gmail.com>
2018-04-20 18:24:12 +1000

    test

---
+++ Essential Establishment Generator/Settings/PriceMod.js
@@ -0,0 +1,19 @@
+setup.getPriceMod = function () {
+      if (tags().length === 0) {
+            return 0;
+      }
+
+      var locations = ["blacksmith", "tavern", "alchemist"];
+      var loc = tags().find(function (tag) {
+            return locations.includes(tag);
+      });
+
+      if (!loc) {
+            return 0;
+      }
+
+      var V = State.variables;
+
+      return Number(V[loc].pricemodifier + 'e-2')
+      || 0;
+  };
