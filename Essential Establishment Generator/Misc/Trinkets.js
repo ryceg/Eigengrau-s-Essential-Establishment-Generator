2327d6273109fd68cbe90919b288b9b5a0524e0d
ryceg <rhysg3@gmail.com>
2018-04-20 15:19:19 +1000

    trinket commenting out

--- Essential Establishment Generator/Misc/Trinkets.js
+++ Essential Establishment Generator/Misc/Trinkets.js
@@ -1,4 +1,4 @@
-setup.createTrinket = function() {
+/*setup.createTrinket = function() {
 
   originalowner     = ["a wizard of immense power", "a poor mother who had to sell it to feed her family", "a chronic gambler who had to make good on a bet", "a man who lost it in a game of cards", "a goblin who thought it was pretty"].random();
   value             = ["next to worthless", "barely a copper", "maybe a copper", "a copper or two", "perhaps three copper", "a meal or two", "a good night at the tavern", "a great night out at the tavern", "a round of beers", "board for a night at a tavern", "a silver, if that", "a handful of silver", "a decent bag of gold", "a sizable hunk of gold", "a considerable amount", "a treasury filled with gold", "a dragon's hoard"].random();
@@ -14,3 +14,4 @@ setup.createTrinket = function() {
       type          : type
     };
 };
+*/
