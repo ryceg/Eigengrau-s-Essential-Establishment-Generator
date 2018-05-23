6179fb5ad71522c1959402ae896bf9abbf471273
ryceg <rhysg3@gmail.com>
2018-05-24 09:50:18 +1000

    rumour start

--- Essential Establishment Generator/Misc/Rumour.js
+++ Essential Establishment Generator/Misc/Rumour.js
@@ -1,8 +1,158 @@
 /*setup.createRumour = function(base) {
-    const
+    var rumour = Object.assign({
+    origin:["a child",
+            "a fat merchant",
+            "a priest",
+            "a sailor",
+            "a soldier",
+            "a magician",
+            "a noble",
+            "a rogue",
+            "a crazy monk",
+            "a drunken farmer",
+            "the butcher",
+            "the tailor"].random(),
+            };
 
+    switch (rumour.origin) {
+              case "a child":
+                  Object.assign(rumour, {
+                      complication: [
+                         "got drunk off his dad's grog",
+                         "got washed out to sea",
+                         "got stuck on a runaway horse",
+                         "found an old well",
+                         "disappeared for 3 days",
+                         "found an old tomb",
+                         "met a weird stranger",
+                         "found a magic item",
+                         "saw a ghost",
+                         "was sleepwalking",
+                         "walked off into the forest",
+                         "stole a loaf of bread from the hermit"
+                         ].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a fat merchant":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a priest":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a sailor":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a soldier":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a magician":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a noble":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a rogue":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a crazy monk":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "a drunken farmer":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "the butcher":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+              case "the tailor":
+                  Object.assign(rumour, {
+                      complication: [].random(),
+                      discovery: [].random(),
+                      result: [].random(),
+                  });
+                  break;
+    }
 
+    switch (rumour.origin) {
+          case "got drunk":
+          case "got washed out to sea":
+          case "got stuck on a runaway horse":
+          case "found an old well":
+          case "disappeared for 3 days":
+          case "found an old tomb":
+          case "met a weird stranger":
+          case "found a magic item":
+          case "were sleepwalking":
+          case "walked off into the forest":
+          }
 
+    switch (rumour.discovery) {
+          case "a new disease":
+          case "a cursed item":
+          case "a sleeping monster":
+          case "a treasure map":
+          case "a villain thought dead, returned to life":
+          case "a book of secrets":
+          case "a key to a vast fortune":
+          case "a supressed truth about the ruling kingdom":
+          case "a door to another plane":
+          }
+
+    switch (rumour.result) {
+          case "people are disappearing!":
+          case "people are sick!":
+          case "the king has decreed strange new laws!":
+          case "the temple has issued strange new tenets!":
+          case "the sun might not come back up!":
+          case "the moon might fracture!":
+          case "the world might be invaded!":
+          case "people are having bad dreams every night!":
+          case "people are unable to sleep!":
+          case "people are afraid to come outside!",
+          }
+
+}, base);
 
 
 }
