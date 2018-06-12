a0f7afeb6c45b9052961ff46194a618b1dba7a4f
ryceg <rhysg3@gmail.com>
2018-06-12 12:56:56 +1000

    test

--- Essential Establishment Generator/Town/professionTester.js
+++ Essential Establishment Generator/Town/professionTester.js
@@ -1,24 +1,29 @@
 setup.getNPCProfession = function(base) {
   var townpop = State.variables.town.population;
   var roll    = (State.variables.town.population / profession.sv); /* Set the number of trades equal to the town's population divided by how many people are needed to support that type of business */
+  var rollPercentage = (roll *= 100);
+  var rollRandom = Math.random(1, 100);
+  var professionCount;
+  var creationInstances;
+
+
 
   if (roll < 1){ /* if the population cannot support 1, then it's converted into a percentage chance that there's one of them */
-    var rollPercentage = (roll *= 100);
-    var rollRandom = Math.random(1, 100);
+
         if (rollRandom > rollPercentage){
-            var professionCount = 1;
+            professionCount = 1;
           else{
-            var professionCount = 0;
+            professionCount = 0;
           }
         }
   }
   else{
-    var professionCount = Math.trunc(roll);
+    professionCount = Math.trunc(roll);
   }
 
   if (professionCount >= 1){
       if (State.variables.NPCArray.profession.count < professionCount){
-          var creationInstances = (professionCount -= State.variables.NPCArray.profession.count);
+          creationInstances = (professionCount -= State.variables.NPCArray.profession.count);
           for (creationInstances = 0; creationInstances > 0; creationInstances--) {
             State.setVar(profession, setup.createNPC({profession: profession}));
           }
