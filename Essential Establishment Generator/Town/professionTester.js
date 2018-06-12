294736fe400966b1d64f69409ea0436ea33da143
ryceg <rhysg3@gmail.com>
2018-06-12 14:03:37 +1000

    test

--- Essential Establishment Generator/Town/professionTester.js
+++ Essential Establishment Generator/Town/professionTester.js
@@ -18,16 +18,16 @@ setup.getNPCProfession = function(profession) { // I'm not really sure how
 
 
   if (professionCount >= 1){ // If there's at least one person
-      if (State.variables.NPCArray.profession.count < professionCount){ // Test to see if the number of people that match that profession in the indexed array is less than the number that should be created
-          creationInstances = (professionCount -= State.variables.NPCArray.profession.count); // if there are less people, define the deficit
+      if (State.variables.NPCArray[0].profession.count < professionCount){ // Test to see if the number of people that match that profession in the indexed array is less than the number that should be created
+          creationInstances = (professionCount -= State.variables.NPCArray[0].profession.count); // if there are less people, define the deficit
           while (creationInstances > 0) { // and use that deficit in a while loop to create NPCs
             creationInstances--
             State.setVar(profession, setup.createNPC({profession: profession})); // Using the setup.createNPC function, with the profession. I don't know if I'm going to run into issues with State.setVar
           }
       }
-      else if (State.variables.NPCArray.profession.count > professionCount){ // if there's more than there should be
-        while (State.variables.NPCArray.indexOf(profession) !== professionCount) { //splice them outta the array.
-              State.variables.NPCArray.splice(State.variables.NPCArray.indexOf(profession), 1); /* Using splice removes the item and reindexes the array. This might cause problems further down the line. */
+      else if (State.variables.NPCArray[0].profession.count > professionCount){ // if there's more than there should be
+        while (State.variables.NPCArray[0].indexOf(profession) !== professionCount) { //splice them outta the array.
+              State.variables.NPCArray[0].splice(State.variables.NPCArray[0].indexOf(profession), 1); /* Using splice removes the item and reindexes the array. This might cause problems further down the line. */
         }
       }
   }
