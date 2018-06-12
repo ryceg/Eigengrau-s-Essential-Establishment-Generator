b45b173b654216bc8da7c7c95b954ac4c8b61f3a
ryceg <rhysg3@gmail.com>
2018-06-12 16:39:55 +1000

    test

--- Essential Establishment Generator/Town/professionTester.js
+++ Essential Establishment Generator/Town/professionTester.js
@@ -4,19 +4,18 @@ setup.getNPCProfession = function(profession) {
   var roll    = (V.town.population / profession.sv);
   var professionCount = (roll >= 1 ? Math.trunc(roll) : Number(random(1, 100) <= roll * 100));
   var creationInstances;
-  var npcs = V.NPCArray.filter(function(npc) { return npc.profession === profession.profession; });
 
   if (professionCount >= 1){
     // If there's at least one person
+    var npcs = V.NPCArray.filter(function(npc) { return npc.profession === profession.profession; });
     if(npcs.length < professionCount) {
         // Create the missing NPCs
-        for(var i = npcs.length - professionCount; i > 0; -- i) {
-            // setup.createNPC({profession: profession.profession});
-            State.setVar(profession.profession, setup.createNPC({profession: profession.profession}));
+        for(var i = professionCount - npcs.length; i > 0; -- i) {
+            setup.createNPC({profession: profession.profession});
         }
     } else if (npcs.length > professionCount) {
         // if there's more than there should be, remove then add just the right amount back
         V.NPCArray = V.NPCArray.filter(function(npc) { return npc.profession !== profession.profession; });
         Array.prototype.push.apply(V.NPCArray, npcs.splice(0, professionCount));
     }}
-}
+};
