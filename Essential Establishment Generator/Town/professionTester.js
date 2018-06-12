e8918fbfe85560b748351c4741fdfbd8c64a30fb
ryceg <rhysg3@gmail.com>
2018-06-12 13:32:43 +1000

    test

--- Essential Establishment Generator/Town/professionTester.js
+++ Essential Establishment Generator/Town/professionTester.js
@@ -1,8 +1,7 @@
 setup.getNPCProfession = function(profession) { // I'm not really sure how
-  var townpop = State.variables.town.population; // define the current town population
   var roll    = (State.variables.town.population / profession.sv); /* Set the number of trades equal to the town's population divided by how many people are needed to support that type of business */
-  var rollPercentage = (roll *= 100);
-  var rollRandom = Math.random(1, 100);
+  var rollPercentage = (roll * 100);
+  var rollRandom = random(1, 100);
   var professionCount;
   var creationInstances;
 
@@ -16,12 +15,12 @@ setup.getNPCProfession = function(profession) { // I'm not really sure how
             professionCount = 0;
         }
       }
-  };
+
 
   if (professionCount >= 1){ // If there's at least one person
       if (State.variables.NPCArray.profession.count < professionCount){ // Test to see if the number of people that match that profession in the indexed array is less than the number that should be created
           creationInstances = (professionCount -= State.variables.NPCArray.profession.count); // if there are less people, define the deficit
-          for (creationInstances = 0; creationInstances > 0; creationInstances--) { // and use that deficit in a for loop to create NPCs
+          while (creationInstances > 0; creationInstances--) { // and use that deficit in a while loop to create NPCs
             State.setVar(profession, setup.createNPC({profession: profession})); // Using the setup.createNPC function, with the profession. I don't know if I'm going to run into issues with State.setVar
           }
       }
