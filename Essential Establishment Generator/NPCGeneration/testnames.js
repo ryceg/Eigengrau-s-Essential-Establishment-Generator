c2be1e6415e0271e6cbd33f9349ffcbd89c7bd3a
ryceg <rhysg3@gmail.com>
2018-07-01 15:04:50 +1000

    test

--- Essential Establishment Generator/NPCGeneration/testnames.js
+++ Essential Establishment Generator/NPCGeneration/testnames.js
@@ -3,10 +3,3 @@ setup.testnames = function() {
   var lastname = JSON.parse(name).human.lastname.random();
   return lastname;
 };
-
-
-testFunc = function() {
-  var name = Story.get("Names").text;
-  /* var lastname = JSON.parse(name).human.lastname.random(); */
-  return name;
-};
