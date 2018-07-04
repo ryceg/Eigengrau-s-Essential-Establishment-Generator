3703d0661ef840114ec585051f5f2345474fde4c
ryceg <rhysg3@gmail.com>
2018-07-04 16:24:51 +1000

    test

--- Essential Establishment Generator/Misc/Guilds/names.js
+++ Essential Establishment Generator/Misc/Guilds/names.js
@@ -3,11 +3,11 @@ var main;
 var adjective;
 var group;
 var unique;
+var temp;
 var name;
-var output;
 switch (guild.type) {
   case "thieves":
-    Object.assign(name, {
+    Object.assign(temp, {
       adjective: ["Clever", "Sneaky", "Cunning", "Conniving", "Honest", "Black", "Invisible", "Silent"].random(),
       main: ["Cutpurses", "Pilferers", "Thieves", "Rogues", "Property Reappropriaters"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -16,7 +16,7 @@ switch (guild.type) {
     break;
 
   case "merchants":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Merchants", "Company", "Sellers and Buyers", "Traders", "Dealers", "Brokers", "Pedlars", "Hawkers", "Distributors"].random(),
       adjective: ["Shrewd", "Thrifty", "Golden Spoon", "Rich", "Miserly"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -25,7 +25,7 @@ switch (guild.type) {
     break;
 
   case "wizards":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Prestidigitators", "Illusionists", "Casters", "Magic Users", "Diviners", "Evokers", "Necromancers", "Abjurers", "Scroll Keepers", "Book Keepers", "Collectors", "Librarians"].random(),
       adjective: ["Arcane", "Magical", "Scholarly", "Absent Minded", "Knowledgeable", "Intelligent", "Unknown", "Eldritch", "Memorized"].random(),
       group: ["Society", "Academy", "University", "Club", "Scholarly Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -34,7 +34,7 @@ switch (guild.type) {
     break;
 
   case "rangers":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Wilderness", "the Woods", "the Lands", "the Forests", "the Trees", "the Animals"].random(),
       adjective: ["Tree Loving", "Padfoot", "Barefoot", "Protective", "Watchful", "Careful", "Honest"].random(),
       group: ["Society", "Group", "Collective", "Brothers", "Brotherhood", "Order", "Protectors", "Defenders", "Conservationists", "Guardians"].random(),
@@ -42,7 +42,7 @@ switch (guild.type) {
     break;
 
   case "seers":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Seers", "Predictionists", "Future Seers", "Observers", "Eyes", "Historians"].random(),
       adjective: ["All Seeing", "All Knowing", "Watchful", "Future"].random(),
       group: ["Society", "Group", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -50,7 +50,7 @@ switch (guild.type) {
     break;
 
   case "priests":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Priests", "Clergy", "Churchpeople", "People of the Cloth", "Robes", "Incense", "Elders", "Preachers"].random(),
       adjective: ["Holy", "Faithful", "Caring", "Civil", "Devout", "Devoted", "Compassionate"].random(),
       group: ["Society", "Group", "League", "Servants", "Collective", "Brothers", "Brotherhood", "Brotherhood", "Priesthood", "Order"].random(),
@@ -58,7 +58,7 @@ switch (guild.type) {
     break;
 
   case "monks":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Monks", "Robes", "Stone", "Rock"].random(),
       adjective: ["Understanding", "Meditating", "Calm", "Unmoving"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -66,7 +66,7 @@ switch (guild.type) {
     break;
 
   case "assassins":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Dagger", "Knife", "Executioners", "Hangmen", "Hitmen", "Killers", "Doctors"].random(),
       adjective: ["Cunning", "Discreet", "Quiet", "Bloody", "Rusted", "Poisoned", "Defiled"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -75,7 +75,7 @@ switch (guild.type) {
     break;
 
   case "artisans":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Creators", "Visionaries", "Artisans", "Artists"].random(),
       adjective: ["Creative", "Inspired", "Bohemian", "Unpaid", "God-Touched"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -83,7 +83,7 @@ switch (guild.type) {
     break;
 
   case "bards":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["Rehearsals", "Musicians", "Bards", "Harmonies", "Poems", "Ballads", "Arias", "Lutes", "Minstrels"].random(),
       adjective: ["Tuneful", "Melodious", "Inspired", "Twelve Tone", "Busking"].random(),
       group: ["Symphony", "Quartet", "Ensemble", "Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
@@ -91,7 +91,7 @@ switch (guild.type) {
     break;
 
   case "nobles":
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["People of Taste", "Land Owners", "Barons", "Tycoons", "Nobles", "Gentlemen"].random(),
       adjective: ["Sophisticated", "Intelligent", "Refined", "Cultured", "Wealthy", "Distinguished"].random(),
       group: ["Society", "Group", "Dinner Club", "League", "Club"].random(),
@@ -99,20 +99,20 @@ switch (guild.type) {
     break;
 
   default:
-    Object.assign(name, {
+    Object.assign(temp, {
       main: ["People", "Men", "Citizens"].random(),
       adjective: ["Watchful", "Careful"].random(),
       group: ["Society", "Group", "League", "Collective", "Brothers", "Brotherhood", "Order"].random(),
     });
   }
 
-  output = [
-    "The " + name.group + " of " + name.adjective + " " + name.main,
-    "The " + name.group + " of " + name.main,
-    "The " + name.adjective + " " + name.group,
-    "The " + name.main + " of " + State.town.name,
-    "The " + State.town.name + " " + name.main,
+  name = [
+    "The " + temp.group + " of " + temp.adjective + " " + temp.main,
+    "The " + temp.group + " of " + temp.main,
+    "The " + temp.adjective + " " + temp.group,
+    "The " + temp.main + " of " + State.town.name,
+    "The " + State.town.name + " " + temp.main,
   ].random();
 
- return output;
+ return name;
 };
