66330cda451961c7a6efaac0ff632309bbbc72f3
ryceg <rhysg3@gmail.com>
2018-07-05 13:37:59 +1000

    test

--- Essential Establishment Generator/genPDF.js
+++ Essential Establishment Generator/genPDF.js
@@ -1,7 +1,7 @@
 function genPDF() {
   var doc = new jsPDF();
 
-  doc.fromHTML($('#testdiv').get(0), 20, 20,
+  doc.fromHTML($('#testdiv').get(0), 20, 20,{
           'width': 500 });
 
   doc.save('Test.pdf');
