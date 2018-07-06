3c3176e3d228bcb87eeae4843dcf5c278e5e9ac7
ryceg <rhysg3@gmail.com>
2018-07-06 17:16:14 +1000

    test

--- Essential Establishment Generator/genPDF.js
+++ Essential Establishment Generator/genPDF.js
@@ -1,19 +1,30 @@
 
-// setup.genPDF = function() {
-//   var doc = new jsPDF();
-//
-//   doc.fromHTML($('#testdiv').get(0), 20, 20,{
-//           'width': 500 });
-//
-//   doc.save('Test.pdf');
-// }
 setup.genPDF = function() {
-  html2canvas(document.getElementById("testdiv"), {
-    onrendered: function (canvas) {
-      var img = canvas.toDataURL("image/png");
-      var doc = new jsPDF();
-      doc.addImage(img, 'JPEG', 20, 20);
-      doc.save('Test.pdf');
-    }
+  var doc = new jsPDF();
+
+  doc.fromHTML($('#testdiv').get(0), 20, 20,{
+          'width': 400 });
+
+  doc.setProperties({
+   title: "Eigengrau's Generator Output",
+   subject: 'Generator Output',
+   author: '/u/rcgy',
+   keywords: 'generated, javascript, dnd, dungeons and dragons, eigengrau, generator, random',
+   creator: '/u/rcgy'
   });
-};
+
+  doc.save('Test.pdf');
+}
+
+
+
+// setup.genPDF = function() {
+//   html2canvas(document.getElementById("testdiv"), {
+//     onrendered: function (canvas) {
+//       var img = canvas.toDataURL("image/png");
+//       var doc = new jsPDF();
+//       doc.addImage(img, 'JPEG', 20, 20);
+//       doc.save('Test.pdf');
+//     }
+//   });
+// };
