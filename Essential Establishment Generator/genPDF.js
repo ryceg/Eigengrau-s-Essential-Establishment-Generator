4127b04d4fc28f47e3d0aab964726113d298883d
ryceg <rhysg3@gmail.com>
2018-07-06 15:39:51 +1000

    test

--- Essential Establishment Generator/genPDF.js
+++ Essential Establishment Generator/genPDF.js
@@ -1,17 +1,17 @@
-// setup.genPDF = function() {
-//   var doc = new jsPDF();
-//
-//   doc.fromHTML($('#testdiv').get(0), 20, 20,{
-//           'width': 500 });
-//
-//   doc.save('Test.pdf');
-//
-//   // html2canvas(document.body, {
-//   //   onrendered: function (canvas) {
-//   //     var img = canvas.toDataURL("image/png");
-//   //     var doc = new jsPDF();
-//   //     doc.addImage(img, 'JPEG', 20, 20);
-//   //     doc.save('Test.pdf');
-//   //   }
-//   // })
-// }
+setup.genPDF = function() {
+  var doc = new jsPDF();
+
+  doc.fromHTML($('#testdiv').get(0), 20, 20,{
+          'width': 500 });
+
+  doc.save('Test.pdf');
+
+  // html2canvas(document.body, {
+  //   onrendered: function (canvas) {
+  //     var img = canvas.toDataURL("image/png");
+  //     var doc = new jsPDF();
+  //     doc.addImage(img, 'JPEG', 20, 20);
+  //     doc.save('Test.pdf');
+  //   }
+  // })
+}
