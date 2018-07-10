5aa508206d2f30d29a92e403cd0476eb022a477b
ryceg <rhysg3@gmail.com>
2018-07-10 14:31:29 +1000

    test

--- EssentialEstablishmentGenerator/tippy.js
+++ EssentialEstablishmentGenerator/tippy.js
@@ -1,8 +1,6 @@
-var tippyLoaded = new Promise(function(resolve, reject) {
-    importScripts("https://unpkg.com/tippy.js@2.5.3/dist/tippy.all.min.js")
-        .then(function() { resolve(); });
-});
-
-setup.tippy = function(selector) {
-    tippyLoaded.then(function() { tippy(selector); });
+var tippyLoaded = importScripts("https://unpkg.com/tippy.js@2.5.3/dist/tippy.all.min.js");
+setup.tippy = function (selector) {
+    tippyLoaded.then(function () {
+        tippy(selector);
+    });
 }
