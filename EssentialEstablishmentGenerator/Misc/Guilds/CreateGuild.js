62e5219c4169c611fab88e87ebb81d2d1561b634
ryceg <rhysg3@gmail.com>
2018-07-10 13:38:04 +1000

    test

--- EssentialEstablishmentGenerator/Misc/Guilds/CreateGuild.js
+++ EssentialEstablishmentGenerator/Misc/Guilds/CreateGuild.js
@@ -127,7 +127,7 @@ setup.createGuild = function(base) {
     //   guild.influenceRoll += Math.fm(guild.influenceRoll, 10);
     // }
 
-  State.variables.guilds.set(baseName + ++index, guild)};
+  // State.variables.guilds.set(baseName + ++index, guild)};
 
   return guild;
 };
