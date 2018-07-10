27f11e9f6e30eaff7c13b3fd51ea4faaff91e596
ryceg <rhysg3@gmail.com>
2018-07-10 16:32:28 +1000

    test

--- EssentialEstablishmentGenerator/Settings/profile.js
+++ EssentialEstablishmentGenerator/Settings/profile.js
@@ -1,3 +1,4 @@
 setup.profileTooltip = function() {
-document.getElementById("tip").title = State.temporary.char.descriptor.random() + " " + State.temporary.char.dndclass + " called " + State.temporary.char.name;
+// document.getElementById("tip").title = State.temporary.char.descriptor.random() + " " + State.temporary.char.dndclass + " called " + State.temporary.char.name;
+document.getElementById("tip").title = "Test";
 };
