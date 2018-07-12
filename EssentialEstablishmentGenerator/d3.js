a306b1efcb41c2d2a15588dd1c4f28d4a9c0f62c
ryceg <rhysg3@gmail.com>
2018-07-12 13:37:25 +1000

    test

--- EssentialEstablishmentGenerator/d3.js
+++ EssentialEstablishmentGenerator/d3.js
@@ -1,47 +1,24 @@
 setup.testd3 = function() {
-
-var width = 600, height = 400;
-// circle and ellipse attributes
-     var cx = 50,   cy = 80,  radius1 = 40,
-         ex = 250,  ey = 80,  radius2 = 80;
-     // rectangle attributes
-     var rectX = 20, rectY = 200;
-     var rWidth = 100, rHeight = 50;
-     // line segment attributes
-     var x1=150,y1=150,x2=300,y2=250,lineWidth=4;
-     var colors = ["red", "blue", "green"];
-     // create an SVG element
-     var svg = d3.select("body")
-                 .append("svg")
-                 .attr("width",  width)
-                 .attr("height", height);
-     // append a circle
-     svg.append("circle")
-        .attr("cx", cx)
-        .attr("cy", cy)
-        .attr("r",  radius1)
-        .attr("fill", colors[0]);
-     // append an ellipse
-     svg.append("ellipse")
-        .attr("cx", ex)
-        .attr("cy", ey)
-        .attr("rx", radius2)
-        .attr("ry", radius1)
-        .attr("fill", colors[1]);
-     // append a rectangle
-     svg.append("rect")
-        .attr("x", rectX)
-        .attr("y", rectY)
-        .attr("width",  rWidth)
-        .attr("height", rHeight)
-        .attr("fill", colors[2]);
-     // append a line segment
-     svg.append("line")
-        .attr("x1", x1)
-        .attr("y1", y1)
-        .attr("x2", x2)
-        .attr("y2", y2)
-        .attr("stroke-width", lineWidth)
-        .attr("stroke", colors[0]);
+          d3.select("body")
+            .append("svg")
+            .attr("width",  300)
+            .attr("height", 300)
+            .append("circle")
+            .attr("cx", 150)
+            .attr("cy", 150)
+            .attr("r",  30)
+            .attr("fill", "blue")
+            .on("mouseover", function() {
+               return d3.select(this)
+                        .attr("r", 80)
+                        .attr("fill", "red");
+            })
+            .on("mouseout", function() {
+              return d3.select(this)
+                       .attr("r", 50)
+                       .attr("fill", "green");
+            }).on("click", function(d, i) {
+              return console.log(d, i);
+            });
 
 }
