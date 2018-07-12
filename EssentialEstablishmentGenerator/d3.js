1002faa3a3bb1a2aa27499772d915ad55ef7e3d3
ryceg <rhysg3@gmail.com>
2018-07-12 13:38:55 +1000

    test

--- EssentialEstablishmentGenerator/d3.js
+++ EssentialEstablishmentGenerator/d3.js
@@ -1,24 +1,18 @@
 setup.testd3 = function() {
-          d3.select("body")
+var width = 800, height = 500;
+          var radius = 30, moveCount = 0, index = 0;
+          var circleColors = ["red", "yellow", "green", "blue"];
+          var svg = d3.select("body")
             .append("svg")
-            .attr("width",  300)
-            .attr("height", 300)
-            .append("circle")
-            .attr("cx", 150)
-            .attr("cy", 150)
-            .attr("r",  30)
-            .attr("fill", "blue")
-            .on("mouseover", function() {
-               return d3.select(this)
-                        .attr("r", 80)
-                        .attr("fill", "red");
-            })
-            .on("mouseout", function() {
-              return d3.select(this)
-                       .attr("r", 50)
-                       .attr("fill", "green");
-            }).on("click", function(d, i) {
-              return console.log(d, i);
-            });
+            .attr("width",  width)
+            .attr("height", height);
+          svg.on("mousemove", function() {
+            index = (++moveCount) % circleColors.length;
+            var circle = svg.append("circle")
+               .attr("cx", (width-100)*Math.random())
+               .attr("cy", (height-100)*Math.random())
+               .attr("r",  radius);
+            circle.attr("fill", circleColors[index]);
+          });
 
 }
