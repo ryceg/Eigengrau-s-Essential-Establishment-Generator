8e6e6b9696af4e80e9c344b7b97cc6340ad5f6b5
ryceg <rhysg3@gmail.com>
2018-07-12 13:41:48 +1000

    test

--- EssentialEstablishmentGenerator/d3.js
+++ EssentialEstablishmentGenerator/d3.js
@@ -1,18 +1,26 @@
 setup.testd3 = function() {
-var width = 800, height = 500;
-          var radius = 30, moveCount = 0, index = 0;
-          var circleColors = ["red", "yellow", "green", "blue"];
-          var svg = d3.select("body")
-            .append("svg")
-            .attr("width",  width)
-            .attr("height", height);
-          svg.on("mousemove", function() {
-            index = (++moveCount) % circleColors.length;
-            var circle = svg.append("circle")
-               .attr("cx", (width-100)*Math.random())
-               .attr("cy", (height-100)*Math.random())
-               .attr("r",  radius);
-            circle.attr("fill", circleColors[index]);
-          });
+var width=600, height=400, circle1;
+    var drag = d3.behavior.drag()
+                 .on(‘dragstart’, dragStart)
+                 .on(‘drag’,      dragElement)
+                 .on(‘dragend’,   dragEnd);
+    d3.select("#circle1").call(drag);
+    function dragStart(d, i) { }
+    function dragElement(d, i) {
+       circle1.attr("cx", d3.event.sourceEvent.pageX)
+              .attr("cy", d3.event.sourceEvent.pageY);
+    }
+    function dragEnd(d, i) { }
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("width",  width)
+                .attr("height", height);
+        circle1 = svg.append("circle")
+                     .attr("id", "circle1")
+                     .attr("cx", 50)
+                     .attr("cy", 50)
+                     .attr("r",  30)
+                     .attr("fill", "blue")
+                     .call(drag);
 
 }
