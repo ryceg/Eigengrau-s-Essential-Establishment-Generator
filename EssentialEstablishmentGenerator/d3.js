fa8ed3ac446ef824a1747101c8998f4855fd1a1d
ryceg <rhysg3@gmail.com>
2018-07-12 13:29:59 +1000

    test

--- EssentialEstablishmentGenerator/d3.js
+++ EssentialEstablishmentGenerator/d3.js
@@ -1,8 +1,8 @@
 setup.testd3 = function() {
 
-d3.select(“body”).append(“p”).text(“Hello1 D3”);
-          d3.select(“body”).append(“p”).text(“Hello2 D3”);
-          d3.select(“body”).append(“p”).text(“Hello3 D3”);
-          d3.select(“body”).append(“p”).text(“Hello4 D3”);
-          d3.selectAll(“p”).style(“color”, “red”);
+d3.select("body").append("p").text("Hello1 D3");
+          d3.select("body").append("p").text("Hello2 D3");
+          d3.select("body").append("p").text("Hello3 D3");
+          d3.select("body").append("p").text("Hello4 D3");
+          d3.selectAll("p").style("color", "red");
 }
