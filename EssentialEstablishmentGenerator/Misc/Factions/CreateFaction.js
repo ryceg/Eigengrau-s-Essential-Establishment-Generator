3f0ac30cc9185017de7b26ab51190bae6bfe4444
ryceg <rhysg3@gmail.com>
2018-07-14 13:43:41 +1000

    test

--- EssentialEstablishmentGenerator/Misc/Factions/CreateFaction.js
+++ EssentialEstablishmentGenerator/Misc/Factions/CreateFaction.js
@@ -161,7 +161,7 @@ setup.createFaction = function(base) {
 
   switch (faction.leadershipType) {
     case "individual":
-      State.setVar(factionLeader, setup.createNPC(leadershipGeneration));
+      State.variables.factionLeader = setup.createNPC(leadershipGeneration);
   }
 
 
