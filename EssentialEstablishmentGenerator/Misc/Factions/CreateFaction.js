5346141d0f1b7ebca35829cf5c1b23fbd67697df
ryceg <rhysg3@gmail.com>
2018-07-14 15:09:49 +1000

    Revert "test"
    This reverts commit 2fafc3deab27d179ff4c6fedc209eb154b7d255a.

--- EssentialEstablishmentGenerator/Misc/Factions/CreateFaction.js
+++ EssentialEstablishmentGenerator/Misc/Factions/CreateFaction.js
@@ -4,11 +4,11 @@ setup.createFaction = function(base) {
   var type = ["thieves", "merchants", "wizards", "rangers", "seers", "priests", "monks", "assassins", "artisans", "nobles", "bards", "mercenaries", "bandits"].random();
   var motivation = ["money", "fame", "power", "glory", "vengeance", "politics"];
   var leadershipType = ["individual", "individual", "individual", "group"];
-  var leadershipGeneration = {};
+  var leadershipGeneration;
 
-  var meetingAccessibility,
-      meetingRegularity,
-      meetingCooperation;
+  var meetingAccessibility;
+  var meetingRegularity;
+  var meetingCooperation;
 
   var leaderQualification = ["most skilled", "rose to power through nepotism", "completed an ordeal"];
   var leaderBribesRoll = random(1, 50) + random(1, 50);
@@ -99,10 +99,134 @@ setup.createFaction = function(base) {
     // default:
   }
 
+  if (faction.leaderBribesRoll > 95){
+    faction.leaderBribes = "will never, under any circumstances be accepted";
+  } else if (faction.leaderBribesRoll > 90){
+    faction.leaderBribes = "will never be accepted, and will be met with instant excommunication";
+  } else if (faction.leaderBribesRoll > 80){
+    faction.leaderBribes = "are treated as insults";
+  } else if (faction.leaderBribesRoll > 70){
+    faction.leaderBribes = "will be met with laughter";
+  } else if (faction.leaderBribesRoll > 60){
+    faction.leaderBribes = "are scorned";
+  } else if (faction.leaderBribesRoll > 55){
+    faction.leaderBribes = "are uncommon, and frowned upon";
+  } else if (faction.leaderBribesRoll > 50){
+    faction.leaderBribes = "will usually be rejected";
+  } else if (faction.leaderBribesRoll > 45){
+    faction.leaderBribes = "depend on circumstances";
+  } else if (faction.leaderBribesRoll > 40){
+    faction.leaderBribes = "are sometimes accepted";
+  } else if (faction.leaderBribesRoll > 30){
+    faction.leaderBribes = "will be bargained about";
+  } else if (faction.leaderBribesRoll > 20){
+    faction.leaderBribes = "will usually be accepted";
+  } else if (faction.leaderBribesRoll > 10){
+    faction.leaderBribes = "are a regular part of business";
+  } else if (faction.leaderBribesRoll <= 5){
+    faction.leaderBribes = "are expected";
+  } else {
+    faction.leaderBribes = "depend on circumstances";
+
+  if (faction.leaderCompetenceRoll > 95){
+    faction.leaderCompetence = "ruthlessly efficient";
+  } else if (faction.leaderCompetenceRoll > 90){
+    faction.leaderCompetence = "extremely efficient";
+  } else if (faction.leaderCompetenceRoll > 80){
+    faction.leaderCompetence = "very competent";
+  } else if (faction.leaderCompetenceRoll > 70){
+    faction.leaderCompetence = "quite competent";
+  } else if (faction.leaderCompetenceRoll > 60){
+    faction.leaderCompetence = "reasonably competent";
+  } else if (faction.leaderCompetenceRoll > 55){
+    faction.leaderCompetence = "usually competent";
+  } else if (faction.leaderCompetenceRoll > 50){
+    faction.leaderCompetence = "of mild competence";
+  } else if (faction.leaderCompetenceRoll > 45){
+    faction.leaderCompetence = "of mild incompetence";
+  } else if (faction.leaderCompetenceRoll > 40){
+    faction.leaderCompetence = "somewhat incompetent";
+  } else if (faction.leaderCompetenceRoll > 30){
+    faction.leaderCompetence = "quite incompetent";
+  } else if (faction.leaderCompetenceRoll > 20){
+    faction.leaderCompetence = "very incompetent";
+  } else if (faction.leaderCompetenceRoll > 10){
+    faction.leaderCompetence = "unbelievably incompetent";
+  } else if (faction.leaderCompetenceRoll <= 5){
+    faction.leaderCompetence = "incompetent to the point of being unable to pour water out of a boot with the instructions written on the heel";
+  } else {
+    faction.leaderCompetence = "of mild competence";
+  }
+
+
   switch (faction.leadershipType) {
     case "individual":
       State.variables.factionLeader = setup.createNPC(leadershipGeneration);
   }
+
+
+
+    //
+    // if (faction.placeholderRoll > 95){
+    //   faction.placeholder = "excellent";
+    // } else if (faction.placeholderRoll > 90){
+    //   faction.placeholder = "very good";
+    // } else if (faction.placeholderRoll > 80){
+    //   faction.placeholder = "quite good";
+    // } else if (faction.placeholderRoll > 70){
+    //   faction.placeholder = "good";
+    // } else if (faction.placeholderRoll > 60){
+    //   faction.placeholder = "above average";
+    // } else if (faction.placeholderRoll > 55){
+    //   faction.placeholder = "slightly above average";
+    // } else if (faction.placeholderRoll > 50){
+    //   faction.placeholder = "average";
+    // } else if (faction.placeholderRoll > 45){
+    //   faction.placeholder = "slightly below average";
+    // } else if (faction.placeholderRoll > 40){
+    //   faction.placeholder = "poor";
+    // } else if (faction.placeholderRoll > 30){
+    //   faction.placeholder = "quite poor";
+    // } else if (faction.placeholderRoll > 20){
+    //   faction.placeholder = "very poor";
+    // } else if (faction.placeholderRoll > 10){
+    //   faction.placeholder = "extremely poor";
+    // } else if (faction.placeholderRoll <= 5){
+    //   faction.placeholder = "abysmal";
+    // } else {
+    //   faction.placeholder = "average";
+    // }
+    //
+    //
+    // if (faction.placeholderRoll > 95){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 15);
+    // } else if (faction.placeholderRoll > 90){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 10);
+    // } else if (faction.placeholderRoll > 80){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 8);
+    // } else if (faction.placeholderRoll > 70){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 6);
+    // } else if (faction.placeholderRoll > 60){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 4);
+    // } else if (faction.placeholderRoll > 55){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 2);
+    // } else if (faction.placeholderRoll > 50){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 1);
+    // } else if (faction.placeholderRoll > 45){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -1);
+    // } else if (faction.placeholderRoll > 40){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -2);
+    // } else if (faction.placeholderRoll > 30){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -4);
+    // } else if (faction.placeholderRoll > 20){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -6);
+    // } else if (faction.placeholderRoll > 10){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -8);
+    // } else if (faction.placeholderRoll <= 5){
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, -10);
+    // } else {
+    //   faction.influenceRoll += Math.fm(faction.influenceRoll, 10);
+    // }
 
   // State.variables.factions.set(baseName + ++index, faction)};
 
