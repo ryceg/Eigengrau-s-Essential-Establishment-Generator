4c0e2b300fd3f4033aa68ad0a8abb4655b5507de
ryceg <rhysg3@gmail.com>
2018-07-14 16:23:26 +1000

    movin' stuff about

--- EssentialEstablishmentGenerator/Settings/Tippy/tippy.js
+++ EssentialEstablishmentGenerator/Settings/Tippy/tippy.js
@@ -5,12 +5,15 @@ setup.tippy = function (selector) {
     });
 }
 
+const tip = tippy('[title]')
+
 tippy.browser.onUserInputChange = type => {
   const method = type === 'touch' ? 'disable' : 'enable'
   for (const tooltip of tip.tooltips) {
     tooltip[method]()
   }
 }
+
 
 $(document).on(':passageend', function (ev) {
     tippy('.tip');
