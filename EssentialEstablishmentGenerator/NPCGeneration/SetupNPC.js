dcaee67e73a3f55943c3939b877a972a31ce85da
ryceg <rhysg3@gmail.com>
2018-07-29 12:08:47 +1000

    Fixed age

--- EssentialEstablishmentGenerator/NPCGeneration/SetupNPC.js
+++ EssentialEstablishmentGenerator/NPCGeneration/SetupNPC.js
@@ -25,6 +25,7 @@ setup.createNPC = function (base) {
   var owner
   var pubRumour = setup.createPubRumour()
   var race = npcJSON.race.random()
+  var age
 
   // Base random variables first - those that don't depend on others
   var npc = Object.assign({
@@ -32,7 +33,7 @@ setup.createNPC = function (base) {
     race: race,
     firstName: firstName,
     lastName: lastName,
-    age: ['childlike', 'rather young', 'eighteen year old', 'surprisingly young', 'relatively young', 'relatively young', 'middle aged', 'middle aged', 'middle aged', 'middle aged', 'middle aged', 'relatively old', 'sun wizened', 'quite old', 'ancient'].random(),
+    age: age,
     /* currentmood: ["annoyed", "scared", "relaxed", "concerned", "bemused", "stressed", "amused", "content", "distracted"], */
     demeanour: npcJSON.demeanour.random(),
     calmtrait: npcJSON.calmTrait.random(),
@@ -53,7 +54,7 @@ setup.createNPC = function (base) {
     idle: npcJSON.idle,
     eyes: npcJSON.raceTraits[race].eyes.random(),
     racePlural: npcJSON.raceTraits[race].racePlural,
-    raceAdjective: npcJSON.raceTraits[race].raceAdjective,
+    raceadjective: npcJSON.raceTraits[race].raceAdjective,
     raceLanguage: npcJSON.raceTraits[race].raceLanguage,
     currentproject: currentproject,
     mundane: mundane,
@@ -81,7 +82,6 @@ setup.createNPC = function (base) {
   if (random(1, 100) >= 60) {
     npc.vocalPattern = ['is incoherent except for a few key words', 'stutters', 'says ‘um’ a lot', 'says ‘like’ a lot', 'swears', "uses thee's and thou's", 'never stops to breathe', 'uses short, clipped sentences', 'talks in third person', "doesn't conjugate well (‘me make good soup’)", 'rolls R’s', 'never uses contractions', 'is whiny', 'obviously has a stuffy nose', 'tongue stuck to back of teeth', 'does so through clenched teeth', 'speaks in a sing-song voice', 'likes to rhyme', "spits on every 's' sound", 'makes all Th-sounds become Z-sounds', 'repeats the last few words of a sentence/thought (‘nice to meet you, meet you’)', 'uses full titles or descriptions of how he knows you (‘ellen-farmers-daughter is pretty’)', 'strings together adjectives/adverbs for more impact (‘wow, your dress is pretty-pretty!’ ‘I am short-short-short.’)', 'all non-proper nouns end with ‘en’/’sen’ (‘may I have some applesen?’ ‘I saw a big moosen!’)', 'all L-sounds become w-sounds', 'repeats the last word you say before responding', 'sings everything', 'does the wrong emphasis on the wrong syllables', 'pauses often', 'has a clipped pattern of speech', 'is rather monotonous', 'whistles on S-sounds', 'spits on Th-sounds and S-sounds (think Sylvester the cat from Looney toons)', 'has a light lisp', 'makes all r-sounds become w-sounds', 'has a severe underbite', 'has a severe overbite', 'speaks out of the corner of his mouth', 'is always pouting', 'makes ‘ar/er’ sounds become ‘aye’ sounds (fart will sound like fight, water will sound like watay)', 'makes soft letters elongated (‘ssssso, hhhhhhow are you?’)', 'slurs words', 'always has a full mouth while talking', 'sighs after each sentence', 'never uses am/is/are/was/were (‘I big.’ ‘She pretty.’)', 'responds in the form of questions', 'mutters'].random()
   }
-
 
   setup.createName(npc)
 
