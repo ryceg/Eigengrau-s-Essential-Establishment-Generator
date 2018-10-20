89f43aa7b1e6dd85542b5aaa565da88297a251c2
ryceg <rhysg3@gmail.com>
2018-10-20 12:34:44 +1100

    removing unnecessary stuff

--- EssentialEstablishmentGenerator/Factions/CreateFaction.js
+++ EssentialEstablishmentGenerator/Factions/CreateFaction.js
@@ -3,18 +3,14 @@ setup.createFaction = function (town, base) {
   var type = ['thieves', 'merchants', 'wizards', 'rangers', 'seers', 'priests', 'monks', 'assassins', 'artisans', 'nobles', 'bards', 'mercenaries', 'bandits', 'craftsmen', 'scholars'].random()
   var isPoliticalPower
   var isThrowaway
-  var leaders = []
-  var leadershipGeneration = {}
   // Rolls are defined immediately in case they're needed in the subroutines out of order (i.e. it makes no sense to initialise SizeRoll in the size.js function if it's being used in "reputation.js")
 
   var faction = Object.assign({
     associatedTown: town,
-    isPoliticalPower: isPoliticalPower,
-    isThrowaway: isThrowaway,
     type: type,
     factionNoun: setup.factionData.type[type].factionNoun,
     motivation: setup.factionData.type[type].motivation.random(),
-    name: setup.nameFaction(type),
+    name: setup.nameFaction(town.name, type),
     leadershipType: ['individual', 'individual', 'individual', 'group', 'group'].random(),
     influenceRoll: dice(2, 50).clamp(1, 100),
     reputationRoll: dice(2, 50).clamp(1, 100),
@@ -24,19 +20,17 @@ setup.createFaction = function (town, base) {
     resourcesRoll: dice(2, 50).clamp(1, 100)
   }, base)
 
-
-
   setup.ageFaction(faction)
   setup.reputationFaction(faction)
-  setup.sizeFaction(faction)
+  setup.sizeFaction(town, faction)
   setup.influenceFaction(faction)
   setup.resourcesFaction(faction)
   setup.stabilityFaction(faction)
 
-  setup.leaderFaction(town, action)
+  setup.leaderFaction(town, faction)
   setup.joinFaction(faction)
   // setup.membersFaction(faction)
-  faction.membersTrait = setup.factionData.type[type].membersTrait.random()
+  faction.membersTrait = setup.factionData.type[faction.type].membersTrait.random()
   setup.createAllies(faction)
   setup.createRivals(faction)
   setup.createMisc(faction)
