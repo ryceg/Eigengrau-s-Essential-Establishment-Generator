18aacfef768a319a62f3b194fd395d5a5d462a5f
ryceg <rhysg3@gmail.com>
2018-10-20 12:35:38 +1100

    misc

--- EssentialEstablishmentGenerator/Alchemist/js/createAlchemist.js
+++ EssentialEstablishmentGenerator/Alchemist/js/createAlchemist.js
@@ -2,7 +2,7 @@ setup.createAlchemist = function (town, opts) {
   opts = opts || {}
   var alchemist = (opts['newBuilding'] || setup.createBuilding)(town.name, 'alchemist')
   alchemist.chemist = (opts['newBartender'] || setup.createChemist)(town.name)
-  alchemist.associatedTown = town
+  alchemist.associatedTown = town || ''
   alchemist.passageName = 'AlchemistOutput'
   alchemist.name = setup.createAlchemistName(alchemist.chemist.firstName)
   setup.alchemistModifiers(alchemist)
--- EssentialEstablishmentGenerator/NPCGeneration/SetupNames.js
+++ EssentialEstablishmentGenerator/NPCGeneration/SetupNames.js
@@ -2,6 +2,6 @@ setup.createName = function (npc) {
   if (!npc) {
     return setup.npcData.raceTraits['human'].genderTraits['man'].firstName.random().toUpperFirst()
   }
-  npc.formalName = npc.formalName || npc.title + ' ' + npc.lastName
+  npc.formalName = npc.formalName || npc.title + ' ' + npc.lastName || npc.name
   return npc
 }
