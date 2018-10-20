ad069ef80a439ad24d51340c23569c4bc4d9ea0f
ryceg <rhysg3@gmail.com>
2018-10-20 14:22:28 +1100

    as

--- EssentialEstablishmentGenerator/Alchemist/js/createAlchemist.js
+++ EssentialEstablishmentGenerator/Alchemist/js/createAlchemist.js
@@ -1,12 +1,12 @@
 setup.createAlchemist = function (town, opts) {
-  opts = opts || {}
+  opts = opts || {};
   var alchemist = (opts['newBuilding'] || setup.createBuilding)(town.name, 'alchemist')
   alchemist.chemist = (opts['newBartender'] || setup.createChemist)(town.name)
-  alchemist.associatedTown = town || ''
+  alchemist.associatedTown = town
   alchemist.passageName = 'AlchemistOutput'
-  alchemist.name = setup.createAlchemistName(alchemist.chemist.firstName)
+  alchemist.name = setup.createAlchexmistName(alchemist.chemist.firstName)
   setup.alchemistModifiers(alchemist)
   setup.alchemistRenders(alchemist)
 
   return alchemist
-}
+};
