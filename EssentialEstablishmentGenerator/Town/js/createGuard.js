9c3f2e3bba6e1cb8f9d8298bd24ec8f2f70f3802
ryceg <rhysg3@gmail.com>
2018-10-24 18:11:33 +1100

    guard.js and guardData. Currently not working quite right.

--- EssentialEstablishmentGenerator/Town/js/createGuard.js
+++ EssentialEstablishmentGenerator/Town/js/createGuard.js
@@ -1,88 +1,28 @@
-setup.createGuard = function (town) {
+setup.createGuard = function (townName) {
+  // console.log(townName + ' is the townName passed to the guard.')
   var guard = {
     captain: setup.createNPC({
       dndClass: 'fighter',
-      background: 'soldier',
-      gender: ['man', 'man', 'man', 'woman'].random()
+      background: 'soldier'
     }),
     passageName: 'GuardOutput',
-    primaryColours: [
-      'black',
-      'red',
-      'scarlet',
-      'forest green',
-      'royal blue',
-      'light blue',
-      'magenta',
-      'pale yellow',
-      'brown',
-      'dark grey',
-      'gunmetal',
-      'maroon',
-      'navy blue',
-      'steel',
-      'olive green'].random(),
-    secondaryColours: [
-      'black',
-      'gold',
-      'silver',
-      'white',
-      'pearl white',
-      'purple'].random(),
-    insignia: [
-      'a skull',
-      'a bow & arrow',
-      'an eagle',
-      'a star',
-      'an axe',
-      'a set of crossed spears',
-      'a shield',
-      'a ghost',
-      'a clenched fist',
-      'a flame',
-      'an arrow',
-      'a dagger',
-      'a sword',
-      'a hammer',
-      'the sun',
-      'the moon',
-      'a bat',
-      'a bull',
-      'a dragon',
-      'a falcon',
-      'a lion',
-      'a raven',
-      'a scorpion',
-      'a snake',
-      'a vulture',
-      'a wolf'].random()
+    livery: setup.guardData.primaryColours.random() + ' and ' + setup.guardData.secondaryColours.random() + ' livery adorned with an image of ' + setup.guardData.insignia.random()
   }
-  guard.livery = guard.primaryColours + ' and ' + guard.secondaryColours + ' livery adorned with an image of ' + guard.insignia
 
-  var guardnameA = ['Regiment', 'Guard', 'Officers', 'Guardsmen', 'Protectors', 'Protectorate', 'Defenders', 'Watch', 'Watchers', 'Watchmen', 'Police', 'Peacemen', 'Axemen', 'Swordsmen', 'Pikesmen', 'Squad', 'Batallion', 'Battlesquad', 'Fighters', 'Keepers'].random()
-  var guardnameB = ['Justice', 'Righteousness', 'Law', 'Order', 'Safety', 'Strength', town.name, town.name, town.name].random()
-  var guardnameC = ['Black', 'White', 'Shining', 'Just', 'Tall', 'Impenetrable', 'Unbreakable', 'Brass', 'Bronze', 'Blue', 'Strong', 'Mighty', 'Lawful'].random()
-  var guardnameD = ['Hand', 'Fist', 'Gauntlet', 'Glove', 'Hammer', 'Shield', 'Cloak', 'Dagger', 'Mace', 'Sword'].random()
-
-  var guardnameRoll = random(1, 6)
-  switch (guardnameRoll) {
-    case 1:
-      guard.name = 'The ' + guardnameA + ' of ' + town.name
-      break
-    case 2:
-      guard.name = 'The ' + town.name + ' ' + guardnameA
-      break
-    case 3:
-      guard.name = 'The ' + guardnameA + ' of ' + guardnameB
-      break
-    case 4:
-      guard.name = 'The ' + guardnameC + ' ' + guardnameA
-      break
-    case 5:
-      guard.name = 'The ' + guardnameC + ' ' + guardnameD
-      break
-    default:
-      guard.name = 'The ' + guardnameC + ' ' + guardnameD + ' of ' + guardnameB
+  var nameRoll = random(1, 6)
+  // console.log('nameRoll is ' + nameRoll)
+  if (nameRoll === 1) {
+    guard.name = 'The ' + setup.guardData.group.random() + ' of ' + townName
+  } else if (nameRoll === 2) {
+    guard.name = 'The ' + townName + ' ' + setup.guardData.group.random()
+  } else if (nameRoll === 3) {
+    guard.name = 'The ' + setup.guardData.group.random() + ' of ' + [setup.guardData.alternateAdjective.random(), setup.guardData.alternateAdjective.random(), setup.guardData.alternateAdjective.random(), townName].random()
+  } else if (nameRoll === 4) {
+    guard.name = 'The ' + setup.guardData.adjective.random() + ' ' + setup.guardData.group.random()
+  } else if (nameRoll === 5) {
+    guard.name = 'The ' + setup.guardData.adjective.random() + ' ' + setup.guardData.noun.random()
+  } else {
+    guard.name = 'The ' + setup.guardData.adjective.random() + ' ' + setup.guardData.noun.random() + ' of ' + [setup.guardData.alternateAdjective.random(), setup.guardData.alternateAdjective.random(), setup.guardData.alternateAdjective.random(), townName].random()
   }
   return guard
 }
