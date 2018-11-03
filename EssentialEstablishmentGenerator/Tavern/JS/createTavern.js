f4b058b12f1ca8b7a31c5919f88b7192bc45910b
ryceg <rhysg3@gmail.com>
2018-11-03 19:13:07 +1100

    minor changes

--- EssentialEstablishmentGenerator/Tavern/JS/createTavern.js
+++ EssentialEstablishmentGenerator/Tavern/JS/createTavern.js
@@ -5,24 +5,44 @@ setup.createTavern = function (town, opts) {
 
   tavern.name = setup.createTavernName()
   console.groupCollapsed(tavern.name)
-  var bartender = (opts['newBartender'] || setup.createBartender)(town.name, tavern.name)
-  var barmaid = setup.createNPC({
+  tavern.bartender = (opts['newBartender'] || setup.createBartender)(town.name, tavern.name)
+  tavern.barmaid = setup.createNPC({
     isShallow: true,
     gender: 'woman',
     background: 'commoner',
     hasClass: false,
     profession: 'barmaid'
   })
-  tavern.barmaid = barmaid
-  tavern.bartender = bartender
-  var shortages = ['wine', 'booze', 'grog', 'whiskey', 'mutton', 'lamb', 'carrots', 'mugs', 'forks', 'frogs', 'bread', 'mushrooms', 'salt', 'silver pieces', 'chairs', 'eggs', 'potatoes']
 
   Object.assign(tavern, {
     passageName: 'TavernOutput',
     initPassage: 'InitTavern',
     wordnoun: ['tavern', 'tavern', 'tavern', 'tavern', 'pub', 'pub', 'pub', 'inn', 'inn', 'bar', 'bar', 'bar', 'watering hole', 'drinkery'].random(),
-    shortages: shortages,
+    shortages: ['wine', 'booze', 'grog', 'whiskey', 'mutton', 'lamb', 'carrots', 'mugs', 'forks', 'frogs', 'bread', 'mushrooms', 'salt', 'silver pieces', 'chairs', 'eggs', 'potatoes'],
     fun: setup.tavernData.fun.random(),
+    type: [
+      'quiet and low-key bar',
+      'regular',
+      'regular',
+      'regular',
+      'regular',
+      'raucous dive',
+      'raucous dive',
+      'raucous dive',
+      'raucous dive',
+      "thieves' guild hangout",
+      'gathering place for a secret society',
+      'high-end dining club',
+      'high-end dining club',
+      'gambling den',
+      'gambling den',
+      tavern.bartender.race + ' only club',
+      "guild-member's only club",
+      "guild-member's only club",
+      'members-only club',
+      'brothel',
+      'brothel'
+    ].random(),
     // entertainment: setup.tavernData.entertainment.random(),
     // patrons: setup.tavernData.patrons.random(),
     game: setup.tavernData.games.random()
@@ -43,29 +63,6 @@ setup.createTavern = function (town, opts) {
 
   Object.assign(tavern, setup.getTavernDraws(town, tavern))
   // console.log(tavern)
-  tavern.type = [
-    'quiet and low-key bar',
-    'regular',
-    'regular',
-    'regular',
-    'regular',
-    'raucous dive',
-    'raucous dive',
-    'raucous dive',
-    'raucous dive',
-    "thieves' guild hangout",
-    'gathering place for a secret society',
-    'high-end dining club',
-    'high-end dining club',
-    'gambling den',
-    'gambling den',
-    bartender.race + ' only club',
-    "guild-member's only club",
-    "guild-member's only club",
-    'members-only club',
-    'brothel',
-    'brothel'
-  ].random()
 
   if (tavern.draw === 'proximity to the church') {
     if (tavern.type.indexOf(['gambling den', 'proximity to the brothel', 'raucous dive']) !== -1) {
