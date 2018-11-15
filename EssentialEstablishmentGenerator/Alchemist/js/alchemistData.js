582af9d9d61a709f13195521507a89599fd1136f
ryceg <rhysg3@gmail.com>
2018-11-15 16:06:25 +1100

    siloing data

--- EssentialEstablishmentGenerator/Alchemist/js/alchemistData.js
+++ EssentialEstablishmentGenerator/Alchemist/js/alchemistData.js
@@ -110,3 +110,52 @@ setup.getAlchemistLookAround = function (alchemist) {
   ]
   return setup.alchemistLookAround
 }
+
+setup.alchemistData = {
+  'rollData': {
+    'wealth': [
+      [95, 'kingly'],
+      [80, 'aristocratic'],
+      [70, 'wealthy'],
+      [60, 'comfortable'],
+      [50, 'modest'],
+      [25, 'poor'],
+      [15, 'squalid'],
+      [0, 'destitute']
+    ],
+    'size': [
+      [95, 'cavernous'],
+      [80, 'huge'],
+      [70, 'quite large'],
+      [60, 'large'],
+      [50, 'spacious'],
+      [40, 'average sized'],
+      [30, 'somewhat cramped'],
+      [20, 'small'],
+      [10, 'tiny'],
+      [0, 'extremely cramped']
+    ],
+    'cleanliness': [
+      [80, 'fastidious'],
+      [70, 'very tidy'],
+      [60, 'tidy'],
+      [50, 'reasonably tidy'],
+      [40, 'somewhat messy'],
+      [30, 'rather messy'],
+      [20, 'very messy'],
+      [10, 'extremely messy'],
+      [0, 'dangerously messy']
+    ],
+    'expertise': [
+      [80, 'masterful'],
+      [70, 'exceptional'],
+      [60, 'superior quality'],
+      [50, 'finely crafted'],
+      [40, 'well crafted'],
+      [30, 'sloppily made'],
+      [20, 'somewhat amateur'],
+      [10, 'amateur'],
+      [0, 'blatantly amateur']
+    ]
+  }
+}
