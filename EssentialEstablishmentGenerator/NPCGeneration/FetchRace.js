33c50bc1d81f010f01c6f0b7e59680127787e607
ryceg <rhysg3@gmail.com>
2019-03-10 22:45:44 +1100

    refactored code.

--- EssentialEstablishmentGenerator/NPCGeneration/FetchRace.js
+++ EssentialEstablishmentGenerator/NPCGeneration/FetchRace.js
@@ -1,43 +1,28 @@
-/* global setup randomFloat State */
+/* global setup */
 setup.fetchRace = function (town) {
   console.log('Fetching race...')
-  var townDemographic = town.demographic || State.variables.town.demographic
-  var races = Object.keys(townDemographic)
-  // console.log('list of races:')
-  // console.log(races)
-  // console.log('list of percentages:')
-  // console.log(townDemographic)
-  // Calculate the sum of the raw demographic values.
-  var raceRoll = randomFloat(0, 100)
-  // console.log('raceRoll is: ' + raceRoll)
-  var sum = races
-    .map(function (race) {
-      return town.demographic[race]
-    }, town)
-  var cumulativeSum = 0
-  var selectedRace
+  var args = town.baseDemographics
+  console.log(args)
+  var pool = []
+  var namePool = Object.keys(args)
+  var totalWeight = 0
+  for (var arg in args) {
+    pool.push(args[arg])
+    totalWeight += args[arg]
+  }
 
-  for (let percent of sum) {
-    if (raceRoll <= cumulativeSum) {
-      selectedRace = sum.indexOf(percent) - 1
-      console.log('selected race is: ' + races[selectedRace])
-      return races[selectedRace]
-    } else if (raceRoll === 100) {
-      selectedRace = sum.length - 1
-      console.log('Hit 100! selected race is: ' + races[selectedRace])
-      return races[selectedRace]
-    } else if (raceRoll > cumulativeSum) {
-      // console.log('Add ' + percent)
-      cumulativeSum += percent
-      if (cumulativeSum === 100) {
-        selectedRace = sum.length - 1
-        console.log('Hit 100! selected race is: ' + races[selectedRace])
-        return races[selectedRace]
-      }
-      // console.log('Equals ' + cumulativeSum)
-    } else {
-      console.log('Somehow managed to not find a race for this person.')
-      return 'human'
+  var random = Math.floor(Math.random() * totalWeight)
+  console.log(random)
+  for (var i = 0; i < pool.length; i++) {
+    random -= pool[i]
+    if (random < 0) {
+      var index = i
+      break
     }
   }
+  // console.log(pool)
+  // console.log(namePool)
+  // console.log(selected)
+  // console.log(namePool[index])
+  return namePool[index]
 }
