8d026c3a28d29e498e4567cf710092a4a11bbd7d
ryceg <rhysg3@gmail.com>
2019-03-10 22:12:48 +1100

    Added assistants to shops

--- EssentialEstablishmentGenerator/Blacksmith/JS/createSmithyName.js
+++ EssentialEstablishmentGenerator/Blacksmith/JS/createSmithyName.js
@@ -1,19 +1,112 @@
+/* global setup */
 setup.createSmithyName = function (town, smithy) {
   var smithyNameRoll = random(1, 5)
   var adjective = ['Hard', 'Sharp', 'Pointy', 'Well-worn', 'Rusted', 'Shiny', 'Cold', 'Glowing', 'Heated', 'Golden', 'Silvered', 'Bronzed', 'Polished', 'Engraved', 'Jeweled', 'Plated', 'Eternal', 'Long-Lasting', 'Famed'].random()
   var noun = ['Iron', 'Metal', 'Gold', 'Silver', 'Bronze', 'Copper', 'Platinum', 'Electrum', 'Ingot', 'Tongs', 'Pliers', 'Anvil', 'Hammer', 'Forge', 'Bellows', 'Bucket', 'Steam', 'Smoke', 'Chimney', 'Flame', 'Fire', 'Magma', 'Coal', 'Crucible'].random()
-  var family = ['Son', 'Daughter', 'Brother', 'Sister', 'Uncle', 'Aunt', 'Father', 'Friends', 'Family', 'Fool'].random()
+  var family = ['son', 'daughter', 'brother', 'sister', 'uncle', 'aunt', 'father', 'friend', 'family', 'fool'].random()
   var rider = ['Shop', 'Blacksmith', 'Fabricator', 'Smith', 'Smithy', 'Farrier', 'Metalsmith', 'Swordsmith'].random()
+
+  var fam = {
+    'son': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.parentNoun
+      },
+      gender: 'man',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'young adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'daughter': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.parentNoun
+      },
+      gender: 'woman',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'young adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'brother': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.siblingNoun
+      },
+      gender: 'man',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: smithy.blacksmith.ageStage,
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'sister': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.siblingNoun
+      },
+      gender: 'woman',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: smithy.blacksmith.ageStage,
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'uncle': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.niblingNoun
+      },
+      gender: 'man',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'settled adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'aunt': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.niblingNoun
+      },
+      gender: 'woman',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'settled adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'father': {
+      relationships: {
+        [smithy.blacksmith.key]: smithy.blacksmith.childNoun
+      },
+      gender: 'man',
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'settled adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'friend': {
+      relationships: {
+        [smithy.blacksmith.key]: 'friend'
+      },
+      ageStage: 'settled adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'family': {
+      relationships: {
+        [smithy.blacksmith.key]: 'relative'
+      },
+      lastName: smithy.blacksmith.lastName,
+      ageStage: 'settled adult',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    },
+    'fool': {
+      relationships: {
+        [smithy.blacksmith.key]: 'employer'
+      },
+      gender: 'man',
+      profession: ['blacksmith', "blacksmith's assistant", "blacksmith's assistant", "blacksmith's assistant"].random()
+    }
+  }
 
   switch (smithyNameRoll) {
     case 1:
       smithy.name = 'The ' + adjective + ' ' + noun
       break
     case 2:
-      smithy.name = smithy.blacksmith.firstName + ' and ' + family
+      smithy.name = smithy.blacksmith.firstName + ' and ' + family.toUpperFirst()
+      smithy.assistant = setup.createNPC(town, fam[family])
+      setup.createRelationship(town, smithy.blacksmith, smithy.assistant, family, smithy.assistant.relationships[smithy.blacksmith.key])
       break
     case 3:
-      smithy.name = 'The ' + noun + ' and ' + family
+      smithy.name = 'The ' + noun + ' and ' + family.toUpperFirst()
+      smithy.assistant = setup.createNPC(town, fam[family])
+      setup.createRelationship(town, smithy.blacksmith, smithy.assistant, family, smithy.assistant.relationships[smithy.blacksmith.key])
       break
     case 4:
       smithy.name = 'The ' + adjective + ' ' + rider
--- EssentialEstablishmentGenerator/GeneralStore/JS/createGeneralStoreName.js
+++ EssentialEstablishmentGenerator/GeneralStore/JS/createGeneralStoreName.js
@@ -1,20 +1,111 @@
-setup.createGeneralStoreName = function (shopkeep) {
+setup.createGeneralStoreName = function (town, GeneralStore) {
   var name
   var roll = dice(1, 5)
   var adjective = ['Dependable', 'Reliable', 'Expendable', 'Indispensible', 'Incomparable', 'Incredible', 'Excellent', 'Important', 'Cheap', 'Affordable', 'Affable', 'Discount', 'Low-Cost'].random()
   var noun = ['Mount', 'Saddle', 'Guild', 'Fangs', 'Man', 'Pardon', 'Pleasure', 'Belt', 'Staff', 'Shield', 'Prince', 'Master', 'Servant', 'Meal', 'Prince', 'Favor', 'Love', 'Word', 'Scribe', 'Apprentice', 'Acolyte', 'Dress', 'Goddess', 'God', 'Gold', 'Purse', 'Trap', 'King', 'Son', 'Sister', 'Mother', 'Daughter', 'Cry', 'Shout', 'Cupboard', 'Pantry', 'Queen', 'Wealth', 'Star', 'Void', 'Woman', 'Man', 'Whore', 'Butcher', 'Anvil', 'Tome', 'Sacrifice', 'Armor', 'Cup', 'Pot', 'Stove', 'Stool', 'Princess', 'Chain', 'Sword', 'Pork', 'Grain', 'Tooth', 'Lance', 'Axe', 'Scabbard', 'Knife', 'Dagger', 'Spear', 'Bow', 'Crossbow', 'Quarterstaff', 'Staff', 'Fire', 'Ice', 'Wind', 'Earth', 'Water', 'Stone', 'Ladle', 'Monastery', 'Chalice', 'Goblet', 'Dungeon', 'Lust', 'Lantern', 'Bone', 'Life', 'Stone', 'Mistress', 'Mind', 'Treasure', 'Barter', 'Armorer', 'Butler', 'Page', 'Tome', 'Feather', 'Shadow', 'Friend', 'Labyrinth', 'Mountain', 'Hope', 'Boot', 'Gauntlet'].random()
-  var family = ['Son', 'Daughter', 'Brother', 'Sister', 'Uncle', 'Aunt', 'Father', 'Friends', 'Family', 'Fool'].random()
+  var family = ['son', 'daughter', 'brother', 'sister', 'uncle', 'aunt', 'father', 'friend', 'family', 'fool'].random()
   var rider = ['Shop', 'Bazaar', 'Convenience Store', 'Trading Post', 'Warehouse', 'Antiquerie', 'Adventure Supplier', 'Supplier', 'Goods', 'Goods and Bads', 'Stock Shop', 'Wares'].random()
+  var fam = {
+    'son': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.parentNoun
+      },
+      gender: 'man',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'young adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'daughter': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.parentNoun
+      },
+      gender: 'woman',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'young adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'brother': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.siblingNoun
+      },
+      gender: 'man',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: GeneralStore.shopkeep.ageStage,
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'sister': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.siblingNoun
+      },
+      gender: 'woman',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: GeneralStore.shopkeep.ageStage,
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'uncle': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.niblingNoun
+      },
+      gender: 'man',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'settled adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'aunt': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.niblingNoun
+      },
+      gender: 'woman',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'settled adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'father': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: GeneralStore.shopkeep.childNoun
+      },
+      gender: 'man',
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'settled adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'friend': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: 'friend'
+      },
+      ageStage: 'settled adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'family': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: 'relative'
+      },
+      lastName: GeneralStore.shopkeep.lastName,
+      ageStage: 'settled adult',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    },
+    'fool': {
+      relationships: {
+        [GeneralStore.shopkeep.key]: 'employer'
+      },
+      gender: 'man',
+      profession: ['shopkeep', "shopkeep's assistant", "shopkeep's assistant", "shopkeep's assistant"].random()
+    }
+  }
 
   switch (roll) {
     case 1:
       name = 'The ' + adjective + ' ' + noun
       break
     case 2:
-      name = shopkeep.firstName + ' and ' + family
+      GeneralStore.name = GeneralStore.shopkeep.firstName + ' and ' + family.toUpperFirst()
+      GeneralStore.assistant = setup.createNPC(town, fam[family])
+      setup.createRelationship(town, GeneralStore.shopkeep, GeneralStore.assistant, family, GeneralStore.assistant.relationships[GeneralStore.shopkeep.key])
       break
     case 3:
-      name = noun + ' and ' + family
+      GeneralStore.name = 'The ' + noun + ' and ' + family.toUpperFirst()
+      GeneralStore.assistant = setup.createNPC(town, fam[family])
+      setup.createRelationship(town, GeneralStore.shopkeep, GeneralStore.assistant, family, GeneralStore.assistant.relationships[GeneralStore.shopkeep.key])
       break
     case 4:
       name = 'The ' + adjective + ' ' + rider
