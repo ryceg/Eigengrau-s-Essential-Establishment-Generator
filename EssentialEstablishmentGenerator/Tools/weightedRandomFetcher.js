72e4d5d575cd67714e66536543e15c93bde32f15
ryceg <rhysg3@gmail.com>
2019-03-10 22:11:19 +1100

    added the weightedRandomFetcher! Very useful tool.

---
+++ EssentialEstablishmentGenerator/Tools/weightedRandomFetcher.js
@@ -0,0 +1,35 @@
+setup.weightedRandomFetcher = function (town, args, npc) {
+  // console.log(args)
+  if (!npc) {
+    npc = setup.createNPC(town)
+  }
+  var pool = []
+  var totalWeight = 0
+  // function addWeight (totalWeight, value) {
+  //   totalWeight += value
+  // }
+  for (var arg in args) {
+    // console.log(args[arg])
+    var isValid = args[arg].exclusions(town, npc)
+    if (isValid === true) {
+      pool.push(args[arg])
+      // console.log('probability: ' + args[arg].probability)
+      // addWeight.apply(totalWeight, [args[arg].probability])
+      totalWeight += args[arg].probability
+    }
+    // isValid = {}
+  }
+  // console.log('Starting the search.')
+  var random = Math.floor(Math.random() * totalWeight)
+  console.log(random)
+  for (var i = 0; i < pool.length; i++) {
+    random -= pool[i].probability
+    if (random < 0) {
+      // console.log('Less than zero! Found one.')
+      // console.log(pool[i])
+      var selected = pool[i]
+      break
+    }
+  }
+  return selected.function(town, npc)
+}
