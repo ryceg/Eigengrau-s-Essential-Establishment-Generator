9934b853be6704243b17d5878e7490308b312112
ryceg <rhysg3@gmail.com>
2019-03-30 15:40:10 +1100

    minor stability

--- EssentialEstablishmentGenerator/NPCGeneration/profile.js
+++ EssentialEstablishmentGenerator/NPCGeneration/profile.js
@@ -1,4 +1,4 @@
 setup.profile = function (npc, base) {
-  base = base || npc.name
+  base = base || npc.name || npc.race
   return '<<profile `$npcs[' + JSON.stringify(npc.key) + '] `' + JSON.stringify(base) + '>>'
 }
