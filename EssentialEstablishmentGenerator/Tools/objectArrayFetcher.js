1233f93f341ac5cad1f0fc8341b2d60b1fefaa90
ryceg <rhysg3@gmail.com>
2019-04-03 10:12:31 +1100

    comment

--- EssentialEstablishmentGenerator/Tools/objectArrayFetcher.js
+++ EssentialEstablishmentGenerator/Tools/objectArrayFetcher.js
@@ -1,4 +1,5 @@
 setup.objectArrayFetcher = function (target, args) {
+  // fetches a random key from an object
   var key = Object.keys(target)
   return target[key[key.length * Math.random() << 0]](args)
 }
