a289b5740edaade7c6599ffb8aefc76978bc14c9
ryceg <rhysg3@gmail.com>
2019-04-07 20:55:42 +1000

    adding comments

--- EssentialEstablishmentGenerator/Tools/factionsForType.js
+++ EssentialEstablishmentGenerator/Tools/factionsForType.js
@@ -1,15 +1,18 @@
 /* global setup clone */
 /* Returns a random of factions for a leadership type. */
-setup.factionsForType = function (town, variable, arg) {
+setup.factionsForType = function (town, variable, value) {
+  // town is the town object. Like most times it's passed as an argument, it's mandatory here.
+  // variable is the thing that we're searching for. This can be leadershipType, type, etc.
+  // value is the value that is expected for variable.
   var found = []
   for (var faction in town.factions) {
-    if (faction[variable] === arg) {
+    if (faction[variable] === value) {
       found.push(clone(faction))
     }
   }
   if (found.length === 0) {
     var tempFaction = setup.createFaction(town, {
-      [variable]: arg,
+      [variable]: value,
       isThrowaway: true
     })
     town.factions[tempFaction.key] = tempFaction
--- EssentialEstablishmentGenerator/Tools/objectArrayFetcher.js
+++ EssentialEstablishmentGenerator/Tools/objectArrayFetcher.js
@@ -1,5 +1,7 @@
 setup.objectArrayFetcher = function (target, args) {
   // fetches a random key from an object
   var key = Object.keys(target)
-  return target[key[key.length * Math.random() << 0]](args)
+  var index = random(0, key.length)
+  return target[key[index]]
+  // (args) || target[key[index]]
 }
