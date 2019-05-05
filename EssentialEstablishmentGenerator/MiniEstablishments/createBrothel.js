9d178d41a5ccdf6490a93483120c9b01f3e0c0ab
ryceg <rhysg3@gmail.com>
2019-05-05 11:46:36 +1000

    pimp is now stored in brothelData

--- EssentialEstablishmentGenerator/MiniEstablishments/createBrothel.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/createBrothel.js
@@ -39,39 +39,15 @@ setup.createBrothel = function (town, tavern) {
   rollDataVariables.forEach(function (propName) {
     setup.defineRollDataGetter(brothel, setup.brothel.rollData, propName)
   })
-//as title is already a value in npcData.js the title for the brothel owner appears as altTitle
-  switch (brothel.owner) {
-    case 'a mean old madam':
-      brothel.pimp = setup.createNPC(town, { age: 'venerable', gender: 'woman', altTitle: 'Mistress', demeanour: 'mean', profession: 'pimp' })
-      break
-    case 'a large madam with a no-nonsense attitude':
-      brothel.pimp = setup.createNPC(town, { gender: 'woman', altTitle: 'Mistress', weight: 'plump', demeanour: 'sensible', profession: 'pimp' })
-      break
-    case 'a warm motherly figure':
-      brothel.pimp = setup.createNPC(town, { gender: 'woman', altTitle: 'Mistress', weight: 'plump', demeanour: 'kind', profession: 'pimp' })
-      break
-    case 'a surly brute':
-      brothel.pimp = setup.createNPC(town, { gender: 'man', altTitle: 'Master', weight: 'muscular', demeanour: 'mean', profession: 'pimp' })
-      break
-    case 'a tyrannical and cruel fellow':
-      brothel.pimp = setup.createNPC(town, { gender: 'man', altTitle: 'Master', age: 'middle-aged', demeanour: 'mean', profession: 'pimp' })
-      break
-    case 'a sultry seductress':
-      brothel.pimp = setup.createNPC(town, { gender: 'woman', altTitle: 'Mistress', age: 'relatively young', demeanour: 'flirtatious', profession: 'pimp' })
-      break
-    case 'a charming witch':
-      brothel.pimp = setup.createNPC(town, { gender: 'woman', altTitle: 'Mistress', age: 'relatively young', dndClass: 'sorcerer', profession: 'pimp' })
-      break
-    case 'an incredibly well endowed woman':
-      brothel.pimp = setup.createNPC(town, { gender: 'woman', altTitle: 'Mistress', weight: 'plump', age: 'relatively young', demeanour: 'flirtatious', profession: 'pimp' })
-      break
-      case 'a rugged and grizzled rogue':
-        brothel.pimp = setup.createNPC(town, { gender: 'man', altTitle: 'Master', demeanour: 'mean', profession: 'pimp' })
+  // as title is already a value in npcData.js the title for the brothel owner appears as altTitle
+  brothel.pimp = setup.createNPC(town, Object.assign(setup.objectArrayFetcher(setup.brothel.pimp), {
+    isShallow: true,
+    hasClass: false
+  }))
 
-  }
-
-  brothel.pimp.greeting = ['nods at you', 'welcomes you warmly', 'smiles, greets you', 'raises a hand with a wave', 'sizes you up, before $pimp.heshe nods at you', 'checks you out for just a moment before smiling at you', 'waves slightly in your direction',
-'gives you you a slight nod', 'ignores you at first before finally turning your way', 'frowns, but greets you just the same']
+  brothel.pimp.greeting = [
+    'nods at you', 'welcomes you warmly', 'smiles, greets you', 'raises a hand with a wave', 'sizes you up, before $pimp.heshe nods at you', 'checks you out for just a moment before smiling at you', 'waves slightly in your direction', 'gives you you a slight nod', 'ignores you at first before finally turning your way', 'frowns, but greets you just the same'
+  ]
   console.log(brothel)
   // setup.townBinder(town, brothel, 'brothel')
   return brothel
