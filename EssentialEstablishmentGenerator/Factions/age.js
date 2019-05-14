dfc4fedfcbd3944052ada201bca715d4d512c922
ryceg <rhysg3@gmail.com>
2019-05-14 10:52:43 +1000

    formatting & updated var to const.

--- EssentialEstablishmentGenerator/Factions/age.js
+++ EssentialEstablishmentGenerator/Factions/age.js
@@ -1,21 +1,22 @@
-setup.ageFaction = function(faction) {
-  console.log("ageing...");
-  var ranks = [
-    [95, "ancient"],
-    [90, "extremely old"],
-    [80, "very old"],
-    [70, "quite old"],
-    [60, "well established"],
-    [55, "somewhat old"],
-    [50, "relatively new"],
-    [45, "recently established"],
-    [40, "new"],
-    [30, "quite new"],
-    [20, "very new"],
-    [10, "brand new"],
-    [5, "unknown"],
-    [0, "well established"]
-  ];
-  for (let [num, description] of ranks)
-    if (faction.roll.age > num) return (faction.age = description);
-};
+setup.ageFaction = function (faction) {
+  console.log('ageing...')
+  const ranks = [
+    [95, 'ancient'],
+    [90, 'extremely old'],
+    [80, 'very old'],
+    [70, 'quite old'],
+    [60, 'well established'],
+    [55, 'somewhat old'],
+    [50, 'relatively new'],
+    [45, 'recently established'],
+    [40, 'new'],
+    [30, 'quite new'],
+    [20, 'very new'],
+    [10, 'brand new'],
+    [5, 'unknown'],
+    [0, 'well established']
+  ]
+  for (let [num, description] of ranks) {
+    if (faction.roll.age > num) return (faction.age = description)
+  }
+}
