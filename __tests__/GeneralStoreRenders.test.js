7c9b84c93fccd4cb921042863eef7d38242ad7d3
cktang88 <arctangent20@yahoo.com>
2019-05-16 11:26:58 -0500

    Fix test with mocking random()

--- __tests__/GeneralStoreRenders.test.js
+++ __tests__/GeneralStoreRenders.test.js
@@ -1,8 +1,6 @@
-console.log("Testing general store rendering...");
-
 // Attaches itself to the *global* node object.
 global.setup = {}
-
+global.random = (min, max) => { return (min+max)/2} ; // mock random global func to be deterministic
 require('../EssentialEstablishmentGenerator/GeneralStore/JS/GeneralStoreRenders')
 
 const store = {
@@ -13,4 +11,20 @@ const store = {
   },
   size: 40
 };
-console.log(setup.GeneralStoreRenders(store));
+
+test("Testing general store rendering...", () => {
+  const expected = {
+    roll: {
+      cleanliness: 50,
+      expertise: 50,
+      activity: 50,
+      warmth: 65.5,
+    },
+    size: 'slightly cramped',
+    "warmth": "uncomfortably warm",
+    cleanliness: 'somewhat messy',
+    expertise: 'well-crafted',
+    activity: 'not terribly busy'
+  }
+  expect(setup.GeneralStoreRenders(store)).toEqual(expected); // use toEqual to test object value equality
+})
