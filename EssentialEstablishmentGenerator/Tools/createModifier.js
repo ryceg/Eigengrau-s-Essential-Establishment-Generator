beddbd2ea1719cc8d05b3084ed59667a58372ff6
Victor Glindås <glinkis@gmail.com>
2019-05-22 20:07:17 +0200

    Add jsdoc documentation

--- EssentialEstablishmentGenerator/Tools/createModifier.js
+++ EssentialEstablishmentGenerator/Tools/createModifier.js
@@ -1,8 +1,16 @@
 setup.createModifier = function (callback) {
-  return function modifier (value, map, defaultValue) {
-    for (const key of Object.keys(map)) {
-      if (callback(value, key)) return map[key]
+  /**
+   * @template T
+   * @param {T} value
+   * @param {{}} map
+   * @param {T} [defaultValue]
+   */
+  function modifier (value, map, defaultValue) {
+    for (const key of Object.keys(map).reverse()) {
+      if (callback(value, Number(key))) return map[key]
     }
-    return defaultValue || value
+    return defaultValue
   }
+
+  return modifier
 }
