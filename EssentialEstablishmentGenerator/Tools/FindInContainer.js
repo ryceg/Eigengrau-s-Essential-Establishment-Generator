c9d8819c689beb2dd069811b470c6a1211da7a4e
ryceg <rhysg3@gmail.com>
2019-05-23 19:09:04 +1000

    adding console log stuff

--- EssentialEstablishmentGenerator/Tools/FindInContainer.js
+++ EssentialEstablishmentGenerator/Tools/FindInContainer.js
@@ -1,8 +1,16 @@
 setup.findInContainer = function (container, key, value) {
   // searches with a depth of one; i.e. this will search the object setup.professions for a specific
-  Object.keys(container).forEach(function (object) {
+  console.log('running setup.findInContainer...')
+  console.log({
+    container,
+    key,
+    value
+  })
+  var found = Object.keys(container).forEach(function (object) {
     if (container[object][key].includes(value)) {
+      console.log('Found ' + value + ' in ' + object)
       return container[object]
     }
   })
+  return found
 }
