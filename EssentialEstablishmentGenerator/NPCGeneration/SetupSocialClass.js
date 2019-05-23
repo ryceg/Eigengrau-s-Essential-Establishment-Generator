6e1e287f8e166e1210664cbbba217f187f570226
ryceg <rhysg3@gmail.com>
2019-05-23 19:08:21 +1000

    more changes to how social class is determined

--- EssentialEstablishmentGenerator/NPCGeneration/SetupSocialClass.js
+++ EssentialEstablishmentGenerator/NPCGeneration/SetupSocialClass.js
@@ -2,33 +2,34 @@
 setup.createSocialClass = function (town, npc) {
   console.log('Creating social class...')
   setup.socialClasses = [
-    [195, 'aristocracy'],
-    [95, 'aristocracy'],
-    [80, 'nobility'],
+    [195, 'aristocracy', 5],
+    [95, 'aristocracy', 5],
+    [80, 'nobility', 4],
     // [75, 'high class'],
     // [65, 'upper-middle class'],
-    [60, 'commoner'],
+    [60, 'commoner', 3],
     // [40, 'lower-middle class'],
-    [20, 'peasantry'],
-    [10, 'paupery'],
-    [0, 'indentured servitude']
+    [20, 'peasantry', 2],
+    [10, 'paupery', 1],
+    [0, 'indentured servitude', 0]
   ]
-  if (!npc.roll.socialClass) {
-    npc.roll.socialClass = setup.townData.professions[(npc.dndClass || npc.profession)].socialClassRoll(town, npc) || 20 + dice(4, 10)
+  if (!npc.roll) {
+    npc.roll = {}
   }
+  let profession = setup.findProfession(town, npc)
+
+  npc.roll.socialClass = npc.roll.socialClass || profession.socialClassRoll || 40 + dice(8, 6)
+
+  console.log({ npc })
   if (!npc.socialClass) {
     console.log('Social class not predefined. Searching for the social class of a ' + npc.profession + '...')
     // if .socialClass is defined in the professions.js, then that's all dandy.
-    if (setup.townData.professions[(npc.dndClass || npc.profession)].socialClass) {
-      npc.socialClass = setup.townData.professions[(npc.dndClass || npc.profession)].socialClass
-      return npc
-    // ...but if not, try looking for synonyms.
-    } else if (typeof setup.findInContainer(setup.townData.professions, 'synonyms', npc.dndClass) !== 'undefined') {
-      console.log('Synonym for ' + npc.dndClass + ' found!')
-      npc.socialClass = setup.findInContainer(setup.townData.professions, 'synonyms', npc.dndClass)
+    if (profession.socialClass) {
+      npc.socialClass = profession.socialClass
       return npc
     // otherwise, just roll some dice.
     } else {
+      console.log('No synonyms found for ' + npc.dndClass)
       var array = setup.socialClasses.find(function (desc) {
         return desc[0] <= npc.roll.socialClass
       })
