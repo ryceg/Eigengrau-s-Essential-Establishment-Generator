992ea5df0b8c36eb2664022f4bb8468286ee5039
Victor Glindås <glinkis@gmail.com>
2019-05-23 21:37:41 +0200

    Refactor postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,52 +6,55 @@ const utils = require('./utils')
 
 fs.mkdirSync(utils.twineFolder, { recursive: true })
 
-console.clear()
-
 const tweegoLink = getTweegoLink()
+
+utils.logClear()
 utils.logAction('Downloading the tweego binary file from', tweegoLink)
 utils.logInfo('Copyright(c) 2014 - 2018 Thomas Michael Edwards <thomasmedwards@gmail.com>.\nAll rights reserved.')
-downloadAndExtract(tweegoLink, utils.tweegoZip)
 
-utils.logAction('Downloading the story formats from', utils.links.storyFormats)
-downloadAndExtract(utils.links.storyFormats, utils.formatsZip)
+downloadAndExtract(tweegoLink, utils.tweegoZip).then(() => {
+  utils.logAction('Downloading the story formats from', utils.links.storyFormats)
+
+  downloadAndExtract(utils.links.storyFormats, utils.formatsZip).then(() => {
+    utils.logSuccess('All done!')
+  })
+})
 
 function downloadAndExtract (link, filePath) {
-  const writeStream = fs.createWriteStream(filePath)
+  return new Promise((resolve, reject) => {
+    const writeStream = fs.createWriteStream(filePath)
 
-  http
-    .get(link, response => response.pipe(writeStream))
-    .on('error', utils.logError)
-    .on('close', onDownloaded)
+    http
+      .get(link, response => response.pipe(writeStream))
+      .on('error', utils.logError)
+      .on('close', unzip)
 
-  function onDownloaded () {
-    utils.logSuccess('Downloaded', filePath)
-
-    // Unzip
-    yauzl.open(filePath, { lazyEntries: true }, (error, zip) => {
-      if (error) {
-        throw error
-      }
-      utils.logAction('Unzipping...')
-      zip.readEntry()
-      zip.on('entry', entry => {
-        if (isFolder(entry)) {
-          zip.readEntry()
-          return
+    function unzip () {
+      yauzl.open(filePath, { lazyEntries: true }, (error, zip) => {
+        if (error) {
+          throw error
         }
-        zip.openReadStream(entry, (error, stream) => {
-          if (error) {
-            throw error
+        utils.logAction('Unzipping...\n')
+        zip.readEntry()
+        zip.on('entry', entry => {
+          if (isFolder(entry)) {
+            zip.readEntry()
+            return
           }
-          const folder = path.resolve(utils.twineFolder, getFileDirectory(entry.fileName))
-          fs.mkdirSync(folder, { recursive: true })
-          const filePath = path.resolve(utils.twineFolder, entry.fileName)
-          const writeStream = fs.createWriteStream(filePath)
-          stream.on('end', () => zip.readEntry()).pipe(writeStream)
-        })
+          zip.openReadStream(entry, (error, stream) => {
+            if (error) {
+              throw error
+            }
+            const fileFolder = path.resolve(utils.twineFolder, getFileDirectory(entry.fileName))
+            fs.mkdirSync(fileFolder, { recursive: true })
+            const filePath = path.resolve(utils.twineFolder, entry.fileName)
+            const writeStream = fs.createWriteStream(filePath)
+            stream.on('end', () => zip.readEntry()).pipe(writeStream)
+          })
+        }).on('end', resolve)
       })
-    })
-  }
+    }
+  })
 }
 
 function getTweegoLink () {
