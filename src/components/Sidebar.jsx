fabf5eeec6afd36c31db5914ade4e0ac30f9f439
Victor Glindås <glinkis@gmail.com>
2019-05-31 22:11:11 +0200

    More styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,13 +13,17 @@ const Sidebar = () => {
         <h1>Eigengrau's Generator</h1>
         <section className="author">
           <span>Created by /u/rcgy</span>
-          <a href="https://discord.gg/A543VC5">
+          <a href="https://www.patreon.com/bePatron?u=399997">
             <img src={patreon} alt="patreon button" />
           </a>
           <a href="https://ko-fi.com/Q5Q8O5AA">
             <img src={kofi} alt="ko-fi button" />
           </a>
         </section>
+        <nav className="menu" role="navigation">
+          <ul className="menu-story" />
+          <ul className="menu-core" />
+        </nav>
       </header>
     </aside>
   )
