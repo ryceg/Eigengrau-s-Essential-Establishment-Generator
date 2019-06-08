7523f05724dd7728534a94839ba3a643aefad4d7
Victor Glindås <glinkis@gmail.com>
2019-06-08 18:09:49 +0200

    Improve factionsForType documentation header

--- EssentialEstablishmentGenerator/Tools/factionsForType.js
+++ EssentialEstablishmentGenerator/Tools/factionsForType.js
@@ -1,9 +1,16 @@
-/* Returns a random of factions for a leadership type. */
+/**
+ * Returns a random of factions for a leadership type.
+ *
+ * @param town The town object. Like most times it's passed as an argument, it's mandatory here.
+ *
+ * @param variable The thing that we're searching for.
+ * This can be leadershipType, type, etc.
+ *
+ * @param value The value that is expected for variable.
+ * if it doesn't find any, it creates a matching faction.
+ * This is for plot hooks that require a wizard's college, etc.
+ */
 setup.factionsForType = function (town, variable, value) {
-  // town is the town object. Like most times it's passed as an argument, it's mandatory here.
-  // variable is the thing that we're searching for. This can be leadershipType, type, etc.
-  // value is the value that is expected for variable.
-  // if it doesn't find any, it creates a matching faction. This is for plot hooks that require a wizard's college, etc.
   const found = []
   for (const faction in town.factions) {
     if (faction[variable] === value) {
