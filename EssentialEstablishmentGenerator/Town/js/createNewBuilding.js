0aaf885beb2786ea339be389e206a56ee3712564
ryceg <rhysg3@gmail.com>
2019-06-12 12:00:26 +1000

    more elegant way of creating new buildings.

--- EssentialEstablishmentGenerator/Town/js/createNewBuilding.js
+++ EssentialEstablishmentGenerator/Town/js/createNewBuilding.js
@@ -1,8 +1,27 @@
+setup.buildingTypes = {
+  'tavern': 'setup.createTavern',
+  'smithy': 'setup.createSmithy',
+  'docks': 'setup.createDocks',
+  'alchemist': 'setup.createAlchemist',
+  'general store': 'setup.createGeneralStore',
+  'brothel': 'setup.createBrothel',
+  'town square': 'setup.createTownSquare',
+  'temple': 'setup.createTemple',
+  'market': 'setup.createMarket',
+  'bakery': 'setup.goodsAndServices.bakery.create',
+  'florist': 'setup.goodsAndServices.florist.create',
+  'tailor': 'setup.goodsAndServices.tailor.create',
+  'butcher': 'setup.goodsAndServices.butcher.create',
+  'cobbler': 'setup.goodsAndServices.cobbler.create'
+}
 setup.createNewBuilding = function (town, buildingType, opts) {
+  // this is necessary to point the function towards where the building creation function is kept.
+  // unfortunately, it currently needs to be updated manually with each new building.
+
   if (!town.buildings[buildingType]) {
     town.buildings[buildingType] = {}
   }
-  var building = setup['create' + buildingType.toUpperFirst()](town, opts)
+  const building = setup.buildingTypes[buildingType](town, opts)
   town.buildings[buildingType][building.key] = building
   return town
 }
