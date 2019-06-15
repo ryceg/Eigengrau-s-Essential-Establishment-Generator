4f5f39b30ac9b7a9f7f58f5325ee5820dd24b54a
ryceg <rhysg3@gmail.com>
2019-06-15 20:08:27 +1000

    balancing

--- EssentialEstablishmentGenerator/Town/lifestyleStandards.js
+++ EssentialEstablishmentGenerator/Town/lifestyleStandards.js
@@ -2,7 +2,7 @@ setup.lifestyleStandards = [
   // index 0 is the amount of money necessary for the lifestyle
   // index 1 is the description
   // index 2 is the percentage of their money taken up by lifestyle expenses (used by npc.finances.livingExpenses())
-  [1000, 'aristocratic', 45],
+  [1000, 'aristocratic', 40],
   [400, 'wealthy', 35],
   [200, 'comfortable', 30],
   [100, 'modest', 25],
