c11abe3d78bce00ee5881f9df6fbfec44a227e35
ryceg <rhysg3@gmail.com>
2019-06-15 12:00:23 +1000

    remove dead code

--- EssentialEstablishmentGenerator/NPCGeneration/fetchProfessionChance.js
+++ EssentialEstablishmentGenerator/NPCGeneration/fetchProfessionChance.js
@@ -1,4 +1,5 @@
 setup.fetchProfessionChance = function (town, npc) {
+  // This gets the starting profession.
   console.log('Fetching profession...')
   town = town || State.variables.town
   const professions = Object.keys(town.professions)
@@ -9,56 +10,22 @@ setup.fetchProfessionChance = function (town, npc) {
       return town.professions[profession].socialClass === npc.socialClass
     })
   }
-  // console.log('list of professions:')
-  // console.log(professions)
-  // console.log('list of percentages:')
-  // console.log(town.professions)
-  // Calculate the sum of the raw demographic values.
-
-  // console.log('professionRoll is: ' + professionRoll)
   const sum = professions
     .map(function (profession) {
       return town.professions[profession].population
     }, town)
-  // console.log('sum is: ')
-  // console.log(sum)
   let totalWeight = 0
   sum.forEach(function (single) {
     totalWeight += single
   })
   let random = Math.floor(randomFloat(1) * totalWeight)
-  // console.log(random)
+  let index
   for (let i = 0; i < sum.length; i++) {
     random -= sum[i]
     if (random < 0) {
-      // eslint-disable-next-line no-var
-      var index = i
+      index = i
       break
     }
   }
-  // console.log(pool)
-  // console.log(namePool)
-  // console.log(selected)
-  // console.log(namePool[index])
   return professions[index]
-  //
-  // for (let percent of sum) {
-  //   if (professionRoll <= cumulativeSum) {
-  //     var selectedProfession = sum.indexOf(percent)
-  //     // console.log('selected profession is: ' + professions[selectedProfession])
-  //     return professions[selectedProfession]
-  //   } else if (professionRoll === 100) {
-  //     selectedProfession = sum.length - 1
-  //     console.log('Hit 100! selected profession is: ' + professions[selectedProfession])
-  //     return professions[selectedProfession]
-  //   } else if (professionRoll > cumulativeSum) {
-  //     cumulativeSum += percent
-  //     if (cumulativeSum === 100) {
-  //       selectedProfession = sum.length - 1
-  //       console.log('Hit 100! selected profession is: ' + professions[selectedProfession])
-  //       return professions[selectedProfession]
-  //     }
-  //     // console.log(cumulativeSum)
-  //   }
-  // }
 }
