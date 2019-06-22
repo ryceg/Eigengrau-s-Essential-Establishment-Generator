81395944262b3e80e30d948b80b66604b1899539
ryceg <rhysg3@gmail.com>
2019-06-22 14:11:52 +1000

    Added error handling.

--- EssentialEstablishmentGenerator/NPCGeneration/profile.js
+++ EssentialEstablishmentGenerator/NPCGeneration/profile.js
@@ -1,17 +1,17 @@
 setup.profile = function (obj, base, type) {
   // this is a function that returns the profile widget for the provided object.
   // obj is the object. It is mandatory.
+  // type is the type of object it is- it points towards npcs as a default.
+  // For buildings, point towards town.buildings.tavern
+  // For factions, point towards town.factions
+  type = type || 'npcs'
   if (typeof obj === 'string') {
     console.error('Profile function for ' + obj + ' called with a string.')
-    obj = State.variables.npcs[obj]
+    obj = State.variables[type][obj]
   }
 
   // base is the user-facing text
   base = base || obj.name || obj.race
 
-  // type is the type of object it is- it points towards npcs as a default.
-  // For buildings, point towards town.buildings.tavern
-  // For factions, point towards town.factions
-  type = type || 'npcs'
   return '<<profile `$' + type + '[' + JSON.stringify(obj.key) + '] `' + JSON.stringify(base) + '>>'
 }
