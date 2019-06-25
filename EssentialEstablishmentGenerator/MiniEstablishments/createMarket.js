1b49da21221deb81263bc3ceb7053442fabc7117
Wendy <41271335+WishingOnAWendy@users.noreply.github.com>
2019-06-25 08:23:33 -0400

    Divide and Add Market crowds
    Divided the market description from the merchants themselves with a subheader
    
    Added market crowd descriptors just for more flavor.

--- EssentialEstablishmentGenerator/MiniEstablishments/createMarket.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/createMarket.js
@@ -15,7 +15,8 @@ setup.createMarket = function (town, opts) {
     size: setup.market.size.seededrandom(),
     cleanliness: setup.market.cleanliness.seededrandom(),
     draw: setup.market.draw.seededrandom(),
-    organisation: setup.market.organisation.seededrandom()
+    organisation: setup.market.organisation.seededrandom(),
+    crowd: setup.market.crowd.seededrandom()
   })
   market.notableFeature = market.draw
   market.tippyDescription = 'A ' + (market.size || market._size) + ' ' + market.wordNoun + " that's " + (market.cleanliness || market._cleanliness) + ', and is known for ' + market.notableFeature + '.'
