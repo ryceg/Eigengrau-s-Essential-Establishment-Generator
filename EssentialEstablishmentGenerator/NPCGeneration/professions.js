505755221b76e205eacfeb1252f3315a0e332d5a
ryceg <rhysg3@gmail.com>
2019-06-25 19:32:18 +1000

    another synonym for child

--- EssentialEstablishmentGenerator/NPCGeneration/professions.js
+++ EssentialEstablishmentGenerator/NPCGeneration/professions.js
@@ -4,7 +4,7 @@ setup.townData.professions = {
     sv: 10000,
     type: 'family',
     sector: 'family',
-    synonyms: ['prepubescent'],
+    synonyms: ['prepubescent', 'kid'],
     description: 'a child, specifically one that is not working.',
     dailyWage: 4,
     socialClass: 'commoner',
