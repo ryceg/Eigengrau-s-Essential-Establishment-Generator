55b85b286f66e8b817daf7fab46ff74fce0c933b
Wendy <41271335+WishingOnAWendy@users.noreply.github.com>
2019-07-07 21:39:29 -0400

    Graveyard Name start and More Options

--- EssentialEstablishmentGenerator/MiniEstablishments/createGraveyard.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/createGraveyard.js
@@ -7,13 +7,15 @@ setup.createGraveyard = function (town, base) {
     initPassage: 'GraveyardOutput',
     passageName: 'GraveyardOutput',
     buildingType: 'graveyard',
-    wordNoun: ['graveyard', 'cemetary', 'necropolis', 'mausoleum'].seededrandom(),
+    wordNoun: ['graveyard', 'cemetary', 'necropolis', 'burial ground', 'boneyard', `potter's field`].seededrandom(),
     needsWordNoun: false,
     size: setup.graveyard.size.seededrandom(),
     location: setup.graveyard.location.seededrandom(),
     cleanliness: setup.graveyard.cleanliness.seededrandom()
   })
-
+  graveyard.name = [
+    `${town.name} ${graveyard.wordNoun.toUpperFirst()}`
+  ].seededrandom()
   graveyard.tippyDescription = `A ${graveyard.wordNoun} that is ${graveyard.size} and is ${graveyard.location}.`
   return graveyard
 }
--- EssentialEstablishmentGenerator/MiniEstablishments/graveyardData.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/graveyardData.js
@@ -2,17 +2,22 @@ setup.graveyard = {
   size: [
     // the graveyard is ______
     'extremely large, occupying several acres of land',
+    'fairly large, with some very ornate looking tombstones',
+    'decently sized, with several rows of plots',
+    'fairly small, with only a few rows of plots',
     'quite small, with only a couple plots'
   ],
   location: [
     // it is _____
     'located just outside of the town',
     'situated near a beautiful tree',
-    'connected via a long path'
+    'connected via a long path',
+    `directly behind the town's major temple`
   ],
   cleanliness: [
     // it is _____
     'well-kempt, with the occassional bouquet of flowers laid on a gravestone',
-    'in need of some maintenance, with moss creeping onto several gravestones'
+    'in need of some maintenance, with moss creeping onto several gravestones',
+    'nearly forgotten, with many headstones having fallen over or crumbled away'
   ]
 }
