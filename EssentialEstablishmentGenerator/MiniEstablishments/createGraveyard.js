c732e6402c04b988620c4ab1d1d615f48f09d99b
Wendy <41271335+WishingOnAWendy@users.noreply.github.com>
2019-07-07 23:54:07 -0400

    Cemetary Names

--- EssentialEstablishmentGenerator/MiniEstablishments/createGraveyard.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/createGraveyard.js
@@ -7,14 +7,23 @@ setup.createGraveyard = function (town, base) {
     initPassage: 'GraveyardOutput',
     passageName: 'GraveyardOutput',
     buildingType: 'graveyard',
-    wordNoun: ['graveyard', 'cemetary', 'necropolis', 'burial ground', 'boneyard', `potter's field`].seededrandom(),
+    wordNoun: ['graveyard', 'cemetery', 'necropolis', 'burial ground', 'boneyard', `potter's field`, 'churchyard'].seededrandom(),
     needsWordNoun: false,
     size: setup.graveyard.size.seededrandom(),
     location: setup.graveyard.location.seededrandom(),
-    cleanliness: setup.graveyard.cleanliness.seededrandom()
+    cleanliness: setup.graveyard.cleanliness.seededrandom(),
+    namePrefix: setup.graveyard.namePrefix.seededrandom(),
+    nameSuffix: setup.graveyard.nameSuffix.seededrandom(),
+    priest: setup.createNPC(town, {
+      dndClass: ['cleric', 'cleric', 'cleric', 'cleric', 'druid'].seededrandom(),
+      background: ['acolyte', 'acolyte', 'acolyte', 'acolyte', 'sage', 'sage', 'sage'].seededrandom(),
+      profession: 'priest'
+    })
   })
   graveyard.name = [
-    `${town.name} ${graveyard.wordNoun.toUpperFirst()}`
+    `${town.name} ${graveyard.wordNoun.toUpperFirst()}`,
+    `${graveyard.nameSuffix} ${graveyard.namePrefix} ${graveyard.wordNoun.toUpperFirst()}`,
+    `St. ` + setup.createName({ race: graveyard.priest.race }) + `'s ${graveyard.wordNoun.toUpperFirst()}`
   ].seededrandom()
   graveyard.tippyDescription = `A ${graveyard.wordNoun} that is ${graveyard.size} and is ${graveyard.location}.`
   return graveyard
--- EssentialEstablishmentGenerator/MiniEstablishments/graveyardData.js
+++ EssentialEstablishmentGenerator/MiniEstablishments/graveyardData.js
@@ -1,6 +1,7 @@
 setup.graveyard = {
   size: [
     // the graveyard is ______
+    'unbelievably large, the rows of headstones stretch out farther than the eye can see',
     'extremely large, occupying several acres of land',
     'fairly large, with some very ornate looking tombstones',
     'decently sized, with several rows of plots',
@@ -12,12 +13,23 @@ setup.graveyard = {
     'located just outside of the town',
     'situated near a beautiful tree',
     'connected via a long path',
-    `directly behind the town's major temple`
+    `directly behind the town's major temple`,
+    'located on the site of a battle from long ago',
+    'located at the end of a long tunnel'
   ],
   cleanliness: [
     // it is _____
     'well-kempt, with the occassional bouquet of flowers laid on a gravestone',
+    'overgrown with weeds, but the graves themselves are well upkept',
+    'very well-kemp, with beatiful murals painted on many of the tombstones',
     'in need of some maintenance, with moss creeping onto several gravestones',
     'nearly forgotten, with many headstones having fallen over or crumbled away'
+  ],
+  nameSuffix: [
+    'Spring', 'Gentle', 'Crescent', 'Burning', 'Autumn', 'Winter', 'Crystal', 'Rotten', 'Devils', 'Summer', 'Eternal', 'Guilty', 'Crown', 'Black', 'Glory', 'Hollow', 'Shadow',
+    'Light', 'Sapphire', 'Dragon', `Lion's`, 'Forgotten'
+  ],
+  namePrefix: [
+    'Oak', 'Hill', 'Lake', 'Mound', 'Springs', 'Memory', 'Gardens', 'Forest', 'River', 'Glory', 'Spirit', 'End', 'Park', 'Pine', 'Soul', 'Town', 'Woods', 'Grove'
   ]
 }
