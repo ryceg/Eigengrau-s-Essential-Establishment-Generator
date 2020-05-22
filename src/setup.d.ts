51176ba2463d55d7ca4d55a0eac46e126eb8e5a6
Victor Glindas <glinkis@gmail.com>
2020-05-22 14:15:38 +0200

    Add more function signatures

--- src/setup.d.ts
+++ src/setup.d.ts
@@ -23,4 +23,19 @@ interface Setup {
   createAlchemist(alchemist: any, opts:any): any
   createAlchemistName(alchemistFirstName: string): string
   createChemist(town: any): any
+
+  getMoralsData(npc: any): MoralsData[]
+  npcTaxRate(town: any, npc: any): number
+  profile(obj: any, base: string, type?: string): string
+  setAsPartners(npc1: any, npc2: any): void
+  createAge(npc: any): any
+  isOfAge(ageStage, race, ageYears): boolean
+  createBackground(npc: any): any
+  createClass(town: any, npc: any): any
+}
+
+
+interface MoralsData {
+  name: string
+  note: string
 }
