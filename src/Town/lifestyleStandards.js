e78e7e1994e73b84aca39fc01c89100622b8c18b
Rhys <36913739+ryceg@users.noreply.github.com>
2020-05-24 22:38:26 +1000

    wording

--- src/Town/lifestyleStandards.js
+++ src/Town/lifestyleStandards.js
@@ -5,7 +5,7 @@ setup.lifestyleStandards = [
   [1000, 'aristocratic', 40, 'lives large, with little care to how much money is spent, splashing out on expensive things on a whim'],
   [400, 'wealthy', 35, 'lives lavishly, with fine foods, wines, clothes, and all the comforts money can buy'],
   [200, 'comfortable', 30, 'lives a comfortable life, filled with good food and wine'],
-  [100, 'modest', 25, 'lives modestly, but not without creature comforts of wine and decent food thanks to frugality'],
+  [100, 'modest', 25, 'lives modestly- not without creature comforts of wine and decent food thanks to frugality, though'],
   [20, 'poor', 10, 'lives a poor life, having to count copper and save for purchases'],
   [10, 'squalid', 5, 'lives a squalid existence, eating terrible food with no alcohol, and few creature comforts'],
   [0, 'wretched', 3, 'lives a wretched life of constant filth and misery. There is no respite to this poverty']
