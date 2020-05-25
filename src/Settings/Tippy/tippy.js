6a54dd51080a3bff9db39b742fb29957d704323c
Rhys <36913739+ryceg@users.noreply.github.com>
2020-05-25 16:11:39 +1000

    added some documentation

--- src/Settings/Tippy/tippy.js
+++ src/Settings/Tippy/tippy.js
@@ -7,14 +7,19 @@ setup.tippy = function (selector) {
 }
 
 setup.createTippy = readout => {
+  // to be used when you want to wrap a tippy around something i.e. you know what you're doing
+  // note the lack of a closing span
   return `<span class="tip" title=${JSON.stringify(readout)}><<run setup.tippy("span.tip")>>`
 }
 
 setup.createTippyWord = (tippy, word) => {
+  // assumes that the first argument was created using the createTippy function
+  // note the two closing spans to accomodate this
   return `${tippy}<span class="dotted">${word}</span></span>`
 }
 
 setup.createTippyFull = (readout, word) => {
+  // the function that should be used most of the time.
   return `<span class="tip dotted" title=${JSON.stringify(readout)}>${word}<<run setup.tippy("span.tip")>></span>`
 }
 
