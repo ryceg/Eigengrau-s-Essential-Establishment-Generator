cbf7e71306d72753c54dfb9b6fb279cf3112fa00
Victor Glindas <glinkis@gmail.com>
2020-05-27 21:25:19 +0200

    Refactor createDescriptors

--- src/NPCGeneration/createDescriptors.js
+++ src/NPCGeneration/createDescriptors.js
@@ -1,21 +1,21 @@
 
-setup.createDescriptors = function (npc) {
-  console.log('assigning descriptors to ' + npc.name + '...')
+setup.createDescriptors = npc => {
+  console.log(`assigning descriptors to ${npc.name}...`)
   // remember adjectival precedence!
   // opinion  size    age   shape       colour  origin  material  purpose     noun
   // lovely   little  old   rectangular green   French  silver    whittling   knife
   npc.descriptors = [
-    (npc.age || npc.ageStage) + ' ' + npc.raceName,
-    (npc.height || npc.skinColour + ' skinned') + ' ' + npc.raceName,
-    (npc.weight || npc.height) + ' ' + npc.raceName,
-    (npc.height || npc.age) + ' ' + npc.gender + ' with ' + npc.physicalTrait
+    `${npc.age || npc.ageStage} ${npc.raceName}`,
+    `${npc.height || `${npc.skinColour} skinned`} ${npc.raceName}`,
+    `${npc.weight || npc.height} ${npc.raceName}`,
+    `${npc.height || npc.age} ${npc.gender} with ${npc.physicalTrait}`
   ]
 
-  if (typeof beard !== 'undefined') {
-    npc.descriptorsAdd = npc.raceName + ' with ' + setup.articles.output(npc.beard)
+  if (npc.beard) {
+    npc.descriptorsAdd = `${npc.raceName} with ${setup.articles.output(npc.beard)}`
   }
 
-  if (npc.hasClass === true) {
+  if (npc.hasClass) {
     npc.descriptorsAdd = npc.dndClass
   }
 
