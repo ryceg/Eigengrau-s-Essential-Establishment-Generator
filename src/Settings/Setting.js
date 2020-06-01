8cc70bbf41c3a59ccd67020030e402869a6c5da5
Rhys <36913739+ryceg@users.noreply.github.com>
2020-06-01 15:11:19 +1000

    Added descriptions to settings

--- src/Settings/Setting.js
+++ src/Settings/Setting.js
@@ -1,11 +1,6 @@
 /* global Setting State */
 Setting.addHeader('Content Settings')
 
-Setting.addToggle('showSliders', {
-  label: 'Show sliders?',
-  onInit: false
-})
-
 const settingShowTutorial = function () {
   const showTutorial = State.metadata.get('showTutorial')
   if (settings.showTutorial !== showTutorial) {
@@ -14,12 +9,10 @@ const settingShowTutorial = function () {
 }
 
 const settingHideAds = function () {
-  if (!settings.hideAds) {
-    State.variables.data.hideAds = false
-    $.wiki('<<remember $data.hideAds = false>>')
+  if (settings.hideAds === true) {
+    settings.hideAds = true
   } else {
-    State.variables.data.hideAds = true
-    $.wiki('<<remember $data.hideAds = true>>')
+    settings.hideAds = false
   }
 }
 
@@ -36,19 +29,24 @@ Setting.addToggle('showMetric', {
   label: 'Show metric?'
 })
 
-Setting.addToggle('silverStandard', {
-  label: 'Silver Standard?'
+Setting.addToggle('showSliders', {
+  label: '<span id="sliders" class="tip dotted" title="If you would like to change the output of buildings, enable this. Warning: feature is in beta.">Show sliders?</span>',
+  onInit: false
 })
 
-Setting.addToggle('hideAds', {
-  label: 'Hide ads?',
-  onInit: false,
-  onChange: settingHideAds
+Setting.addToggle('silverStandard', {
+  label: '<span id="silver" class="tip dotted" title="This is based off the popular homebrew rule where money is divided by ten, so the silver is the standard, reserving gold for kings, making it feel truly like a treasure.">Silver Standard?</span>'
 })
 
 Setting.addToggle('ignoreGender', {
-  label: 'Ignore gender inequality?',
+  label: '<span id="gender" class="tip dotted" title="If you would rather NPCs not be limited in the professions that they take due to sexism, enable this.">Ignore gender inequality?</span>',
   onInit: false
+})
+
+Setting.addToggle('hideAds', {
+  label: '<span id="ads" class="tip dotted" title="This is free, open-source software. Please consider supporting us- this option is available to give people a cleaner interface (for streaming, etc.).">Hide ads?</span>',
+  onInit: false,
+  onChange: settingHideAds
 })
 
 Setting.save()
