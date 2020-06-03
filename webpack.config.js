fe244e8bbab137053ffacc9654d9f2fd98926666
Victor Glindas <glinkis@gmail.com>
2020-06-03 21:59:26 +0200

    Add typescript build process

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+/**
+ * @type {import("webpack").Configuration}
+ */
+module.exports = {
+  entry: './lib/index.ts',
+  output: {
+    library: 'lib',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, 'gh-pages')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+}
