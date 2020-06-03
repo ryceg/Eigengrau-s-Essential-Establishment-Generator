0d47609ae546f396ad8665aeeaa2ead3b51cf8e6
Victor Glindas <glinkis@gmail.com>
2020-06-03 19:02:02 +0200

    Convert to template literals

--- src/Town/Weather/js/toCelsius.js
+++ src/Town/Weather/js/toCelsius.js
@@ -1,9 +1,9 @@
 setup.toCelsius = function (temp) {
   if (settings.showCelsius) {
-    console.log(temp + ' in Fahrenheit')
+    console.log(`${temp} in Fahrenheit`)
     temp -= 32
-    temp = Math.trunc(temp *= 0.5556) + ' degrees Celsius'
-    console.log(temp + ' in Celsius')
+    temp = `${Math.trunc(temp *= 0.5556)} degrees Celsius`
+    console.log(`${temp} in Celsius`)
   } else {
     temp += ' degrees Fahrenheit'
   }
