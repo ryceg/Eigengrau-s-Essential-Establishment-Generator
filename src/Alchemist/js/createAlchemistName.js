c418009be5f0c497b056861c40f1d5e726976f31
Rhys <36913739+ryceg@users.noreply.github.com>
2020-06-05 12:30:36 +1000

    refactoring

--- src/Alchemist/js/createAlchemistName.js
+++ src/Alchemist/js/createAlchemistName.js
@@ -1,25 +1,10 @@
 setup.createAlchemistName = chemistFirstName => {
-  const alchemistNameRoll = dice(1, 5)
-  let name
-  switch (alchemistNameRoll) {
-    case 1:
-      name = `The ${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.noun.random()}`
-      break
-    case 2:
-      name = `${chemistFirstName} and ${setup.alchemist.name.adjective.random()}`
-      // name = `The ${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.noun.random()}`
-      break
-    case 3:
-      name = `The ${setup.alchemist.name.noun.random()} and ${setup.alchemist.name.adjective.random()}`
-      break
-    case 4:
-      name = `The ${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.rider.random()}`
-      break
-    case 5:
-      name = `${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.noun.random()}`
-      break
-    default:
-      name = `The ${setup.alchemist.name.adjective.random()} Alchemist`
-  }
-  return name
+  return [
+      `The ${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.noun.random()}`,
+      `${chemistFirstName} and ${setup.alchemist.name.adjective.random()}`,
+      `The ${setup.alchemist.name.noun.random()} and ${setup.alchemist.name.adjective.random()}`,
+      `The ${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.rider.random()}`,
+      `${setup.alchemist.name.adjective.random()} ${setup.alchemist.name.noun.random()}`,
+      `The ${setup.alchemist.name.adjective.random()} Alchemist`
+  ].random()
 }
