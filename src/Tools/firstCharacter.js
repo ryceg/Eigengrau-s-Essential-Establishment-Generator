73ddf5f541d0cb8d3109dda771bdd46a5b01f204
ryceg <rhysg3@gmail.com>
2020-06-07 16:22:36 +1000

    simplify firstCharacter

--- src/Tools/firstCharacter.js
+++ src/Tools/firstCharacter.js
@@ -1,7 +1,4 @@
 setup.firstCharacter = (word) => {
   /** @param word */
-  const character = word.slice(0, 1).toUpperFirst()
-  const restOfWord = word.slice(1)
-  return `<span class="firstcharacter">${character}</span>${restOfWord}`
-  // return `<span class="firstcharacter">${word.slice(0, 1)}</span>${word.slice(1)}`
+  return `<span class="firstcharacter">${word.slice(0, 1)}</span>${word.slice(1)}`
 }
