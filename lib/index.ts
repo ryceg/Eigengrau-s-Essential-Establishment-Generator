c6cb2bd02a976fbce166a3dbc12402b6926be1ba
Victor Glindas <glinkis@gmail.com>
2020-06-09 23:04:35 +0200

    Move clampRolls into lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { calcPercentage } from './src/calcPercentage'
+export { clampRolls } from './src/clampRolls'
 export { colours } from './src/colours'
 export { setRandom } from './src/random'
--- lib/src/clampRolls.ts
+++ lib/src/clampRolls.ts
@@ -1,11 +1,11 @@
+import { keys } from './utils'
+
 /**
  * Clamps every value presented to the 1-100 range.
  * Most useful for rolls.
- *
- * @param {Record<number, number>} rolls
  */
-setup.clampRolls = rolls => {
-  for (const roll of Object.keys(rolls)) {
+export const clampRolls = (rolls: Record<number, number>) => {
+  for (const roll of keys(rolls)) {
     if (rolls[roll] > 100) {
       console.log(`${rolls[roll]} was over 100.`)
       rolls[roll] = 100
@@ -16,5 +16,4 @@ setup.clampRolls = rolls => {
       rolls[roll] = 1
     }
   }
-  return rolls
 }
