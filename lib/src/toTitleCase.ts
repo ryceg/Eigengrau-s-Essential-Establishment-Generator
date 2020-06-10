e45063521658a474414fe5e1fbbedb7914a8f5ae
Rhys <36913739+ryceg@users.noreply.github.com>
2020-06-10 13:29:12 +1000

    shift toTitleCase to .lib

--- lib/src/toTitleCase.ts
+++ lib/src/toTitleCase.ts
@@ -1,6 +1,5 @@
-window.toTitleCase = function (input) {
-  let i, j, str
-  str = input.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
+export function toTitleCase (input: string) {
+  let str = input.replace(/([^\W_]+[^\s-]*) */g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   })
 
@@ -8,18 +7,18 @@ window.toTitleCase = function (input) {
   // they are the first or last words in the string
   const lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At',
     'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With']
-  for (i = 0, j = lowers.length; i < j; i++) {
-    str = str.replace(new RegExp(`\\s${lowers[i]}\\s`, 'g'),
-      function (txt) {
+  for (const lower of lowers) {
+    str = str.replace(new RegExp(`\\s${lowers[lower]}\\s`, 'g'),
+      (txt) => {
         return txt.toLowerCase()
       })
   }
 
   // Certain words such as initialisms or acronyms should be left uppercase
   const uppers = ['Id', 'Tv']
-  for (i = 0, j = uppers.length; i < j; i++) {
-    str = str.replace(new RegExp(`\\b${uppers[i]}\\b`, 'g'),
-      uppers[i].toUpperCase())
+  for (const upper of uppers) {
+    str = str.replace(new RegExp(`\\b${uppers[upper]}\\b`, 'g'),
+      upper.toUpperCase())
   }
 
   return str
