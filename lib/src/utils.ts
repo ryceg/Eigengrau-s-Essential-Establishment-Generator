6b156bbcf30e923df7df2daaa1bbfe8aad663366
Victor Glindas <glinkis@gmail.com>
2020-06-10 09:31:42 +0200

    Add assign utility

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -6,6 +6,18 @@
  * Object.keys(obj) // string[]
  * keys(obj) // "a" | "b"
  */
-export const keys = <T>(object: T) => {
+export function keys <T> (object: T) {
   return Object.keys(object) as (keyof T)[]
+}
+
+/**
+ * An alternative to `Object.assign`,
+ * which asserts that the properties are added to the type.
+ *
+ * @example
+ * const obj = { a: 0, b: 1}
+ * assign(obj, { c: 2 }) // Typescript now knows that c is available.
+ */
+export function assign <T, S> (target: T, source: S): asserts target is T & S {
+  Object.assign(target, source)
 }
