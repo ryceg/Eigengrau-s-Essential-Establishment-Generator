fc78fc76a13fafb89c838c77d408d7ee3f720fe0
Victor Glindas <glinkis@gmail.com>
2020-06-11 16:59:18 +0200

    Move languages into lib

--- lib/src/languages.ts
+++ lib/src/languages.ts
@@ -1,4 +1,4 @@
-setup.languages = {
+export const languages = {
   elvish: {
     description: 'Elvish is fluid, with subtle intonations and intricate grammar. Elven literature is rich and varied, and their songs and poems are famous among other races. Many bards learn their language so they can add Elvish ballads to their repertoires.'
   },
