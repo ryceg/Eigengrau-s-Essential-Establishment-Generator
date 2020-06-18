b47a422e8ffc6905d41ad207160d9aedab2a0a98
Victor Glindas <glinkis@gmail.com>
2020-06-18 21:27:09 +0200

    Move createMagic to lib

---
+++ lib/src/createMagic.ts
@@ -0,0 +1,37 @@
+import { createRing } from './createRing'
+import { magicData } from './magicData'
+import { random } from './random'
+import { assign } from './utils'
+
+type MagicType = keyof typeof magicData
+
+export function createMagic <T extends MagicType> (type: T | 'ring') {
+  console.log(`Type: ${type}`)
+
+  if (type === 'ring') {
+    const magic = createRing()
+    console.log('Ring:', magic)
+    return magic
+  }
+
+  const data = magicData[type]
+
+  const prefixRoll = random(data.property.length - 1)
+  const suffixRoll = random(data.property.length - 1)
+
+  const magic = {
+    type: random(data.type),
+    prefix: data.prefix[prefixRoll],
+    suffix: data.suffix[suffixRoll],
+    prefixProperty: data.property[prefixRoll],
+    suffixProperty: data.property[suffixRoll]
+  }
+
+  assign(magic, {
+    description: `${magic.prefixProperty}. ${magic.suffixProperty}`,
+    name: `${magic.prefix} ${magic.type} ${magic.suffix}`
+  })
+
+  console.log(magic)
+  return magic
+}
