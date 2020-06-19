3a522d2fc666e1cc0f711912e42dbd4073c113e3
Victor Glindas <glinkis@gmail.com>
2020-06-19 21:34:34 +0200

    Update types

--- lib/constructs/_common.ts
+++ lib/constructs/_common.ts
@@ -1,9 +1,9 @@
 export interface Construct<T extends string = string> {
-  $type: T
-  $uuid: string
+  readonly $type: T
+  readonly $uuid: string
 }
 
 export interface ConstructUtils<C extends Construct = Construct> {
   create: (base?: Partial<C>) => C
-  readout: (object: C) => string
+  readout: (object: Readonly<C>) => string
 }
