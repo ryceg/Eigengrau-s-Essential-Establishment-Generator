b7dd60f80bc2aed0cdaf217502dba7df6a22e762
ryceg <rhysg3@gmail.com>
2020-06-21 13:22:26 +1000

    fix personality

--- src/NPCGeneration/createPersonality.js
+++ src/NPCGeneration/createPersonality.js
@@ -1,13 +1,13 @@
 setup.createPersonality = (npc) => {
   const data = setup.npcData
-  npc = {
+  Object.assign(npc, {
     calmTrait: npc.calmTrait || data.calmTrait.random(),
     stressTrait: npc.stressTrait || data.stressTrait.random(),
     trait: npc.trait || data.trait.random()
     // value: npc.value || data.value.random(),
     // drive: npc.drive || data.drive.random(),
     // belief: npc.belief || data.belief.random(),
-  }
+  })
 
   if (!npc.vocalPattern) {
     if (dice(2, 50) >= 75) {
