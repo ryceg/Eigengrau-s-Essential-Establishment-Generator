8e76850605581f697b8b6d3f1cf5c115b7338a67
Victor Glindas <glinkis@gmail.com>
2020-06-23 21:51:33 +0200

    Move createAlchemy to lib

--- src/PlotHook/Missions/alchemistMissions.js
+++ src/PlotHook/Missions/alchemistMissions.js
@@ -4,28 +4,28 @@ const missions = {
     return `${'I have had some issues with potions disappearing, and suspect that somebody is stealing from me. Would you be able to catch the thief?' + '<blockquote>The thief is <<profile `$npcs['}${JSON.stringify(npc.key)}]\`>></blockquote>`
   },
   testSubject () {
-    const missionPotion = setup.createAlchemy({ type: 'potion' })
+    const missionPotion = lib.createAlchemy({ type: 'potion' })
     return `${"I'm in need of a test subject for a new potion that I've been brewing; I'm not quite sure what the effects will be. Would you be interested in trying this?" + "<div class='descriptive'>" + '<h4>'}${missionPotion.titleReadout}</h4>${missionPotion.descriptionReadout}</div>` + `<blockquote>${missionPotion.effectReadout}</blockquote>`
   },
   bandits (town) {
     const npc = setup.createNPC(town, { dndClass: ['rogue', 'fighter'].random(), background: 'criminal' })
-    const missionIngredient = setup.createAlchemy({ type: 'preserved herb' })
+    const missionIngredient = lib.createAlchemy({ type: 'preserved herb' })
     return `I had a shipment of ${missionIngredient.herb}, but it was attacked by bandits. They probably don't know what they got; they're hiding in the hills. Would you retrieve it for me?` + `<blockquote>The bandit captain responsible is <<profile \`$npcs[${JSON.stringify(npc.key)}]\`>></blockquote>`
   },
   collectHerb () {
-    const missionIngredient = setup.createAlchemy({ type: 'preserved herb' })
+    const missionIngredient = lib.createAlchemy({ type: 'preserved herb' })
     return `I am in need of some capable adventurers to go and collect some ${missionIngredient.herb}. Are you up to the task?`
   },
   disappearingShipment () {
-    const missionIngredient = setup.createAlchemy({ type: 'preserved herb' })
+    const missionIngredient = lib.createAlchemy({ type: 'preserved herb' })
     return `I had a shipment of ${missionIngredient.herb}, but it never arrived... I suspect that something happened to it.`
   },
   bodyParts () {
-    const missionBodyPart = setup.createAlchemy({ type: 'body part' })
+    const missionBodyPart = lib.createAlchemy({ type: 'body part' })
     return `This may be somewhat unsavoury, but I need the ${missionBodyPart.bodyPart} from ${setup.articles.output(missionBodyPart.bodyPartOrigin)}. Can you find me one?`
   },
   badIngredients (town) {
-    const missionIngredient = setup.createAlchemy({ type: 'preserved herb' })
+    const missionIngredient = lib.createAlchemy({ type: 'preserved herb' })
     const npc = setup.createNPC(town, { hasClass: false, profession: 'alchemist' })
     return `I received a shipment of ${missionIngredient.herb}, but it was completely unusable- my friend <<profile \`$npcs[${JSON.stringify(npc.key)}]\`>> would never send me bad goods, so I suspect that something is amiss!`
   }
