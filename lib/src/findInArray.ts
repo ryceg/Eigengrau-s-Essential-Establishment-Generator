305d033acee4cec9beea6c3c851445b6e0567d35
Victor Glindas <glinkis@gmail.com>
2020-07-06 20:12:15 +0200

    Refactor findInArray

--- lib/src/findInArray.ts
+++ lib/src/findInArray.ts
@@ -1,14 +1,13 @@
-/*
-* for returning an object from an array by key value
-* i.e. town.buildings keys
+/**
+* For returning an object from an array by key value
+* i.e. town.buildings keys.
 */
-
 export function findInArray<T, K extends keyof T> (array: T[], key: K, value: T[K]) {
-  return array.find(element => {
+  for (const element of array) {
     if (element[key] === value) {
       console.log(`Found matching key value of ${key}: ${value}!`)
       console.log(element)
       return element
     }
-  })
+  }
 }
