a919eb481f1a911028c106cf0bf9e55f37bdab18
Victor Glindas <glinkis@gmail.com>
2020-07-10 18:26:46 +0200

    Extract getProfessionOrigin utility

--- src/NPCGeneration/SetupClass.js
+++ src/NPCGeneration/SetupClass.js
@@ -29,34 +29,8 @@ setup.createClass = (town, npc) => {
     classWeapon = 'a dagger'
   }
 
-  const profession = lib.findProfession(town, npc)
-
-  const originWage = [
-    [-25, `I've tried to do a good job as ${lib.articles.output(npc.profession)} but am just rubbish at it. I don't think I'm good at anything, really.`],
-    [-20, `I've been trying to make it as ${lib.articles.output(npc.profession)} but suck at it. I'm beginning to think I was never meant to be ${lib.articles.output(npc.profession)}.`],
-    [-15, `I've been trying to make it as ${lib.articles.output(npc.profession)} but just can't seem to hack it. I think I'll quit.`],
-    [-10, `I've had trouble as ${lib.articles.output(npc.profession)}. I guess some people are born with it- I'm sure as hell not.`],
-    [-5, `I've had a bit of a downturn as ${lib.articles.output(npc.profession)}. If it keeps up for much longer, I'm going to begin losing hope.`],
-    [0, `I'm working as ${lib.articles.output(npc.profession)}. The work is alright, ${['and I enjoy it', 'though it can be a bit tedious', 'I\'ve certainly had worse jobs', 'if a little dull', 'if a little dull at times'].random()}`],
-    [5, `I'm on the upswing as ${lib.articles.output(npc.profession)}. Things are looking better.`],
-    [10, `I'm doing really well as ${lib.articles.output(npc.profession)}! Maybe it's luck, maybe a natural talent, I don't know.`],
-    [15, `It turns out that I'm pretty good at being ${lib.articles.output(npc.profession)}! I enjoy the work.`],
-    [20, `It turns out that I'm really good at being ${lib.articles.output(npc.profession)}. It's actually kinda easy.`],
-    [25, `Not to brag, but I'm a born natural at being ${lib.articles.output(npc.profession)}. It's fun, very rewarding work.`]
-  ]
-
   if (!npc.professionOrigin) {
-    if (Array.isArray(profession.professionOrigin)) {
-      npc.professionOrigin = profession.professionOrigin.random()
-    } else if (typeof setup.npcData.professionTraits[npc.profession] !== 'undefined') {
-      npc.professionOrigin = setup.npcData.professionTraits[npc.profession].professionOrigin.random()
-    } else {
-      const wageVariation = npc.roll.wageVariation(town)
-      const wageArray = originWage.find(desc => {
-        return desc[0] >= wageVariation
-      })
-      npc.professionOrigin = wageArray[1]
-    }
+    npc.professionOrigin = getProfessionOrigin(npc, town)
   }
 
   npc.professionOrigin = npc.professionOrigin || professionOrigin
@@ -64,4 +38,41 @@ setup.createClass = (town, npc) => {
   npc.weapon = npc.weapon || classWeapon
 
   return npc
+}
+
+function getProfessionOrigin (npc, town) {
+  const profession = lib.findProfession(town, npc)
+
+  if (Array.isArray(profession.professionOrigin)) {
+    return lib.random(profession.professionOrigin)
+  }
+
+  const professionWithArticle = lib.articles.output(npc.profession)
+
+  /** @type {[number, string][]} */
+  const originWage = [
+    [-25, `I've tried to do a good job as ${professionWithArticle} but am just rubbish at it. I don't think I'm good at anything, really.`],
+    [-20, `I've been trying to make it as ${professionWithArticle} but suck at it. I'm beginning to think I was never meant to be ${professionWithArticle}.`],
+    [-15, `I've been trying to make it as ${professionWithArticle} but just can't seem to hack it. I think I'll quit.`],
+    [-10, `I've had trouble as ${professionWithArticle}. I guess some people are born with it- I'm sure as hell not.`],
+    [-5, `I've had a bit of a downturn as ${professionWithArticle}. If it keeps up for much longer, I'm going to begin losing hope.`],
+    [0, `I'm working as ${professionWithArticle}. The work is alright, ${['and I enjoy it', 'though it can be a bit tedious', 'I\'ve certainly had worse jobs', 'if a little dull', 'if a little dull at times'].random()}`],
+    [5, `I'm on the upswing as ${professionWithArticle}. Things are looking better.`],
+    [10, `I'm doing really well as ${professionWithArticle}! Maybe it's luck, maybe a natural talent, I don't know.`],
+    [15, `It turns out that I'm pretty good at being ${professionWithArticle}! I enjoy the work.`],
+    [20, `It turns out that I'm really good at being ${professionWithArticle}. It's actually kinda easy.`],
+    [25, `Not to brag, but I'm a born natural at being ${professionWithArticle}. It's fun, very rewarding work.`]
+  ]
+
+  const professionTrait = setup.npcData.professionTraits[npc.profession]
+  if (typeof professionTrait !== 'undefined') {
+    return lib.random(professionTrait.professionOrigin)
+  }
+
+  const wageVariation = npc.roll.wageVariation(town)
+  for (const [amount, origin] of originWage) {
+    if (amount >= wageVariation) return origin
+  }
+
+  throw new Error('Could not get profession origin.')
 }
