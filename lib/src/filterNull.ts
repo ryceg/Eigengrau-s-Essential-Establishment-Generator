4e5cff2e326a07cec42bbd97c5b75144b23ac2f6
ryceg <rhysg3@gmail.com>
2020-08-24 20:11:47 +1000

    fixed filterNull

--- lib/src/filterNull.ts
+++ lib/src/filterNull.ts
@@ -1,9 +1,10 @@
 /* for removing null entries */
 export function filterNull<T> (obj: Record<string, T>) {
-  return Object.keys(obj)
-    .filter(e => obj[e] !== null)
-    .reduce((o, e) => {
-      o[e] = obj[e]
-      return o
-    }, {})
+  const propNames = Object.getOwnPropertyNames(obj)
+  for (let i = 0; i < propNames.length; i++) {
+    const propName = propNames[i]
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName]
+    }
+  }
 }
