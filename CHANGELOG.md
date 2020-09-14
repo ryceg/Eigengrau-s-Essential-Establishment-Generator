a74b6c4efb2ec792dd871e442cfab1e54d957a8b
ryceg <rhysg3@gmail.com>
2020-09-14 22:17:05 +1000

    Changed start building logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,10 +13,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 - Ability to edit dominant gender
 - Customers and expanded relationships
 - Relationships between buildings and NPCs
+- Added race in dropdown NPC creation tool in the toolbox.
 
 ### Changed
 - Equality slider has changed; previously 0 was a completely matriarchal society, 50 was perfect egalitarianism, and 100 was patriarchal. Now, there is a dropdown for which gender is the 'dominant' gender, and 0 is sexist with 100 being perfect egalitarianism.
-- Added race in dropdown NPC creation tool in the toolbox.
+- Changed the function that generates initial buildings to test for whether a person with the corresponding profession lives in the town (don't flip a coin to see if there's a bakery when you've already got a concrete answer of whether there's a baker!)
 - Fixed some bugs introduced in the last update.
 - Bug fixes for Tippy on profiles
 - Fix tavern material being [object Object]
--- src/Town/js/createStartBuildings.js
+++ src/Town/js/createStartBuildings.js
@@ -1,32 +1,49 @@
 setup.createStartBuildings = function (town) {
   const buildingType = [
     'Town Square',
-    'Tavern',
-    'General Store',
-    'Smithy',
-    'Market',
-    'Temple'
+    // 'Tavern',
+    // 'General Store',
+    // 'Smithy',
+    'Market'
+    // 'Temple'
   ]
-
+  console.log('Creating starting buildings...')
+  console.log(town)
   if (town.location === 'seashore' || town.location === 'river coast') { buildingType.push('Docks') }
-
+  if (town.population > 500 || town.roll.wealth > 60) { buildingType.push('Graveyard') }
   if (town.hasBrothel) { buildingType.push('Brothel') }
 
-  if (town.population > 100 || town.roll.wealth > 40) { buildingType.push('Alchemist') }
-
-  if (town.population > 100 || town.roll.wealth > 40) { buildingType.push('Bakery') }
-
-  if (town.population > 1000 || town.roll.wealth > 70) { buildingType.push('Florist') }
-
-  if (town.population > 600 || town.roll.wealth > 60) { buildingType.push('Tailor') }
-
-  if (town.population > 400 || town.roll.wealth > 40) { buildingType.push('Butcher') }
-
-  if (town.population > 500 || town.roll.wealth > 60) { buildingType.push('Graveyard') }
-
-  if (town.population > 700 || town.roll.wealth > 60) { buildingType.push('Cobbler') }
-
-  if ((town.population > 350 && town.roll.wealth > 40) || town.roll.wealth > 60) { buildingType.push('Barber') }
+  const professions = {
+    'Tavern': 'bartender',
+    'Alchemist': ['alchemist', 'wizard'],
+    'Bakery': ['pastry chef', 'baker'],
+    'General Store': ['shopkeep'],
+    'Smithy': ['goldsmith', 'weaponsmith', 'silversmith', 'blacksmith', 'farrier', 'armorer'],
+    'Florist': ['florist', 'botanist'],
+    'Tailor': 'tailor',
+    'Butcher': 'butcher',
+    'Cobbler': 'cobbler',
+    'Brothel': 'pimp',
+    'Barber': ['barber', 'surgeon'],
+    'Temple': ['high priest', 'archbishop', 'cardinal', 'bishop', 'priest', 'deacon']
+  }
+  for (const building in professions) {
+    console.log(building)
+    if (Array.isArray(professions[building])) {
+      for (const profession in professions[building]) {
+        console.log(professions[building])
+        if (town.professions[profession] && town.professions[profession].population > 0) {
+          console.log(profession, 'exists')
+          buildingType.push(building)
+        }
+      }
+    } else {
+      if (town.professions[professions[building]] && town.professions[professions[building]].population > 0) {
+        console.log(professions[building], 'exists')
+        buildingType.push(building)
+      }
+    }
+  }
 
   for (const type of buildingType) {
     console.log(`Creating ${lib.articles.output(type)}...`)
