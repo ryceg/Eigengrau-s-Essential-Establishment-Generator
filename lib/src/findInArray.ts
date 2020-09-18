15aa633ab8fbc2e386f637d82c71b90528f1b35c
ryceg <rhysg3@gmail.com>
2020-09-18 21:37:20 +1000

    add some keywords so i can ctrl f

--- lib/src/findInArray.ts
+++ lib/src/findInArray.ts
@@ -1,6 +1,7 @@
 /**
 * For returning an object from an array by key value
 * i.e. town.buildings keys.
+* find building find array
 */
 export function findInArray<T, K extends keyof T> (array: T[], key: K, value: T[K]) {
   for (const element of array) {
