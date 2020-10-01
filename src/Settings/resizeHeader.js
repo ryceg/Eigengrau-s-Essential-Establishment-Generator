ecbf85254fb6ecba2c4ac9b792917707e9d248af
ryceg <rhysg3@gmail.com>
2020-10-01 16:33:59 +1000

    refactor

--- src/Settings/resizeHeader.js
+++ src/Settings/resizeHeader.js
@@ -1,5 +1,5 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function () { scrollFunction() }
+window.onscroll = scrollFunction
 
 function scrollFunction () {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
