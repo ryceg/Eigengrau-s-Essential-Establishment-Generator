bbb97d3d0d35a91a09ea4060013b45645f0e704d
Victor Glindås <glinkis@gmail.com>
2020-10-11 23:21:31 +0200

    Use building type from lib

--- src/Buildings/createBuilding.d.ts
+++ src/Buildings/createBuilding.d.ts
@@ -1,11 +1,5 @@
+type Building = import('../lib/building/_common').Building
+
 interface Setup {
   createBuilding(town: Town, type?: string, base?: Partial<Building>): Building
-}
-
-interface Building {
-  type: string
-  lighting: string
-  outside: string
-  material: any
-  [key: string]: any
 }
