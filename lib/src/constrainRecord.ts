e8ce5beb52c136338a51d376136a6b16faf3bf29
Victor Glindås <glinkis@gmail.com>
2020-10-12 19:28:47 +0200

    Add documentation comment

--- lib/src/constrainRecord.ts
+++ lib/src/constrainRecord.ts
@@ -1,7 +1,18 @@
-
 /**
  * Constrains every property of a record to be a certain type,
  * while still allowing for the keys to be unique values.
+ *
+ * @example
+ * interface Person {
+ *   name: string
+ * }
+ *
+ * const people = constrainRecord<Person>()({
+ *   dwight: { name: "Dwight" },
+ *   angela: { name: "Angela" }
+ * })
+ *
+ * people.dwight // Infers the type correctly.
  */
 export function constrainRecord<T> () {
   return function constrainer<R extends Record<string, T>> (record: R) {
