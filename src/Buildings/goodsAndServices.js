5475801a328d02a1770e120a307bb790a49cec28
Sammy D <scove003@plattsburgh.edu>
2020-10-11 23:34:03 -0400

    Created the Jewelers

--- src/Buildings/goodsAndServices.js
+++ src/Buildings/goodsAndServices.js
@@ -2106,6 +2106,211 @@ setup.initGoodsAndServices = () => {
         'making powerful arrows that have a side effect of breaking more often.'
       ]
     },
+
+    jeweler: {
+      // the bakery can be used as an example of how to add more features to a building.
+      create (town, building, opts = {}) {
+        if (!building) {
+          console.error('A building was not passed!')
+          return
+        }
+        building.associatedNPC = setup.createNPC(town, opts.professionOpts || setup.goodsAndServices[building.type].profession.opts)
+        lib.createBuildingRelationship(town, building, building.associatedNPC, { relationship: 'owner', reciprocalRelationship: 'business' })
+        building.name = building.name || opts.name || setup.goodsAndServices[building.type].name.function(town, building)
+        building.notableFeature = setup.goodsAndServices[building.type].notableFeature.random()
+        building.specialty = setup.goodsAndServices[building.type].specialty.random()
+
+        building.tippyDescription = `${lib.articles.output(building.type).toUpperFirst()} on ${building.road}. Their specialty is ${building.specialty}`
+        return building
+      },
+      name: {
+        function (town, building) {
+          const name = setup.goodsAndServices[building.type].name
+          const unique = name.unique.random() || `The ${town.name} ${name.wordNoun.random().toUpperFirst()}`
+          return [
+            `The ${name.adjective.random().toUpperFirst()} ${[name.noun.random().toUpperFirst(), name.wordNoun.random().toUpperFirst()].random()}`,
+            `The ${name.jewelryAdjective.random().toUpperFirst()} ${name.noun.random().toUpperFirst()}`,
+            `The ${town.name} ${name.wordNoun.random().toUpperFirst()}`,
+            `The ${building.road} ${name.wordNoun.random().toUpperFirst()}`,
+            `${building.associatedNPC.firstName}'s ${name.wordNoun.random().toUpperFirst()}`,
+            `${building.associatedNPC.lastName}'s ${name.noun.random().toUpperFirst()}`,
+            unique
+          ].random()
+        },
+        unique: [
+          'Emerald City Gems',
+          'Carat King',
+          'Jewel Factory',
+          'Golden Age',
+          'Silver Lining',
+          'The Gold Medal',
+          'Ruby Slippers',
+          'The Esteemed Citrine',
+          'Sapphire Blues',
+          'A Diamond A Dozen',
+          'Cracker Beryl',
+          'Garnet Guaranteed',
+          'Here Comes the Sunstone',
+          'Moonstones and More',
+          'The Opal of my Eye'
+        ],
+        noun: [
+          'emerald',
+          'sapphire',
+          'diamond',
+          'ruby',
+          'garnet',
+          'beryl',
+          'gold bar',
+          'silver bar',
+          'necklace',
+          'trinket',
+          'amulet',
+          'opal',
+          'lense',
+          'hammer',
+          'chisel',
+          'citrine',
+          'moonstone',
+          'sunstone'
+        ],
+        nounJeweledGood: [
+          'gemstone',
+          'amulet',
+          'necklace',
+          'ring',
+          'earring',
+          'trinket'
+        ],
+        adjective: [
+          'old',
+          'wrinkly',
+          'inquisitive',
+          'intelligent',
+          'ancient',
+          'magical',
+          'wise',
+          'artisian',
+          'friendly'
+        ],
+        jeweleryAdjective: [
+          'shiny',
+          'expensive',
+          'mesmerizing',
+          'bougie',
+          'breathtaking',
+          'radiant',
+          'iridescent',
+          'noble',
+          'deep',
+          'fine cut'
+        ],
+        wordNoun: [
+          'jewelery',
+          'jewelery shop',
+          'gem smith',
+          'gold smith',
+          'silver smith',
+          'gem shop',
+          'jeweler'
+        ]
+      },
+      PassageFormat: () => [
+        // each array string will be a new line.
+        // this will be evaluated by SugarCube; use *SugarCube syntax* for functions.
+        `<<print lib.firstCharacter("You")>> ${['enter', 'walk into', 'open the door to', 'come inside', 'step through the door of', 'come off the street into'].random()} ${[
+          '$building.name, $building.structure.descriptor.',
+          '$building.structure.descriptor called $building.name.'
+        ].random()} You notice $building.notableFeature`,
+        '',
+        'This $building.wordNoun is known for $building.specialty There is a <<profile $building.associatedNPC $building.associatedNPC.descriptor>> currently <<print $building.associatedNPC.idle.random()>>. <<print $building.associatedNPC.heshe.toUpperFirst()>> welcomes you, and asks what you are after.',
+        '<<goods $building setup.goodsAndServices[$building.type].goods>>'
+      ],
+      profession: {
+        name: 'jeweler',
+        opts: {
+          profession: 'jeweler',
+          hasClass: false,
+          idle: [
+            // name is currently _______
+            'cutting a gemstone',
+            'serving a customer',
+            'inspecting a gemstone',
+            'delicately repairing an ancient ring',
+            'adding gemstones to a fine gold necklace',
+            'weighing a brilliant gemstone',
+            'assisting a man browsing wedding rings.'
+          ]
+        }
+      },
+      goods: {
+        'silver trinket': {
+          // cost: in copper pieces. The <<money>> macro handles currency conversion.
+          cost: 300,
+          // description: used in tooltip.
+          description: 'A lucky charm made of out fine silver.',
+          // exclusions for testing whether it is available. Can be ommitted if it is always available. Return truthiness.
+          exclusions (town, building) {
+            return building.roll.wealth > 20
+          }
+        },
+        'brass ring': {
+          cost: random(1, 10),
+          description: "A discolored old ring. Don't propose to a lovely lady with this one."
+        },
+        'gold ring': {
+          cost: random(200, 400),
+          description: 'A ring made out of gold. Suitable for a noble perhaps.',
+          exclusions (town, building) {
+            return building.roll.wealth > 25
+          }
+        },
+        'gemstone ring': {
+          cost: 850,
+          description: 'A captivating ring made with fine gems cast in gold.',
+          exclusions (town, building) {
+            return town.population > 1500 && building.roll.wealth > 25
+          }
+        },
+        'gemstone amulet': {
+          cost: 1000,
+          description: 'Fine diamonds hang from a silver string. A wearable chandelier.',
+          exclusions (town, building) {
+            return town.population > 2000 && building.roll.wealth > 50
+          }
+        }
+      },
+      type: 'jewelers',
+      notableFeature: [
+        // you notice _______
+        'a stack of finely cut gems behind the counter.',
+        'the jeweler is assisting a royal member.',
+        'a collection of rare minerials on the front counter.',
+        'a trophy with "Finest Gemstones awarded to $building.name" etched into it sitting on a shelf near the entry.',
+        'silver trinkets for sale in a display case.',
+        'necklaces for sale in a display case.',
+        'sapphires, emeralds, diamonds and more along a workbench behind the counter.',
+        'a jeweled trinket halfway finished.',
+        'a large golden bell is sitting on the shop counter.',
+        'a dusty old bookshelf full of geological texts.',
+        'the distinct smell of metal.',
+        'the shop counter is made of lavish mahagony wood.'
+      ],
+      specialty: [
+        // the jeweler is known for _______
+        'charging an arm and a leg.',
+        'the fine gemstones cut every day.',
+        'being the best place to fix an earring.',
+        'their gemstone collection.',
+        'the brass rings they sell.',
+        'always finding the diamond in the dirt.',
+        'being mostly too expensive for common folks.',
+        'working closely with royals.',
+        'being execptional gem cutters.',
+        'their collection of odd trinkets.'
+      ]
+    },
+
     barber: {
       create (town, building, opts = {}) {
         if (!building) {
