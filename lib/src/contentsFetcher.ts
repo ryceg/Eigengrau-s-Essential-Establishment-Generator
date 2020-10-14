d87f014503b23709ebd61cd9c7e58e794fd9c4c9
Victor Glindås <glinkis@gmail.com>
2020-10-14 21:27:01 +0200

    Update contentsFetcher

--- lib/src/contentsFetcher.ts
+++ lib/src/contentsFetcher.ts
@@ -1,18 +1,26 @@
+import { Town } from '../town/_common'
 import { random } from './random'
+
+interface Encounter<T> {
+  summary: T
+  function?(town: Town, biome: string): string
+}
 
 /**
  * This is the function used to handle random encounters.
  * It needs a lot of work, and will change.
  */
-export function contentsFetcher<T> (keyTarget: (keyof T)[], contentsTarget: T) {
+export function contentsFetcher<T> (keyTarget: T[], contentsTarget: Encounter<T>[]) {
   const key = random(keyTarget)
-  const value = contentsTarget[key]
+  const value = contentsTarget.find(({ summary }) => summary === key)
 
-  if (typeof value === 'function') {
-    return value
+  if (value == null) {
+    throw new TypeError('Not a valid result!')
   }
 
-  console.error('key:', key)
-  console.error('value:', value)
-  throw new TypeError('Not a function!')
+  if (typeof value.function === 'function') {
+    return value.function
+  }
+
+  return () => value.summary
 }
