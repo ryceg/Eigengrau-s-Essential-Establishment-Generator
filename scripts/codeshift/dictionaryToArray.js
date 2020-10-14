3330513c463cba5130bc9cf584fe08593deed15a
Victor Glindås <glinkis@gmail.com>
2020-10-14 20:39:56 +0200

    Add codeshift

---
+++ scripts/codeshift/dictionaryToArray.js
@@ -0,0 +1,45 @@
+// @ts-check
+// Line to convert
+const LINE = 62
+
+/**
+ * Converts an object to an array of objects.
+ *
+ * @param {import("jscodeshift").FileInfo} fileInfo
+ * @param {import("jscodeshift").API} api
+ */
+module.exports = (fileInfo, api) => {
+  const jsc = api.jscodeshift
+  const doc = jsc(fileInfo.source)
+
+  doc.find(jsc.ObjectExpression, obj => obj.start === LINE).replaceWith(({ node }) => {
+    return jsc.arrayExpression(node.properties.map(property => {
+      if (property.type !== 'Property') {
+        throw new Error('Not a property')
+      }
+      if (property.value.type !== 'FunctionExpression' && property.value.type !== 'ArrowFunctionExpression') {
+        throw new Error('Not a function expression.')
+      }
+      if (property.key.type !== 'Literal') {
+        throw new Error('Key not a literal')
+      }
+
+      const properties = []
+      properties.push(jsc.property('init', jsc.identifier('summary'), jsc.literal(property.key.value)))
+      properties.push(jsc.property('init', jsc.identifier('function'), property.value))
+
+      // Remove function if it's an arrow function with same same return value as the key.
+      if (property.value.type === 'ArrowFunctionExpression') {
+        if (property.value.body.type === 'Literal') {
+          if (property.value.body.value === property.key.value) {
+            properties.splice(1, 1)
+          }
+        }
+      }
+
+      return jsc.objectExpression(properties)
+    })
+    )
+  })
+  return doc.toSource()
+}
