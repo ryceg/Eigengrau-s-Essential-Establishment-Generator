a190a1c2652f56e00591ef44ab21a274cef173e5
ryceg <rhysg3@gmail.com>
2020-10-14 23:57:44 +1100

    remove unnecessary console log

--- src/Buildings/Components/buildingRelationshipNpc.js
+++ src/Buildings/Components/buildingRelationshipNpc.js
@@ -18,24 +18,19 @@ setup.createBuildingRelationshipNpc = (town, building, associatedNPC, relationsh
   if (!args.objectKey) args.objectKey = 'building'
   if (!args.relationshipKey) args.relationshipKey = 'relationship'
   // const relationship = lib.findInArray(relationshipTable, 'relationship', args.selectedRelationship)
-  console.log('1')
   const index = relationshipTable.findIndex(obj => {
     return obj.relationships[args.objectKey][args.relationshipKey] === args.targetKey
   })
-  console.log('2')
   const relationship = relationshipTable[index]
   if (relationship.base) Object.assign(args.base, relationship.base)
   const npc = setup.createNPC(town, args.base)
-  console.log('3')
   if (relationship.relationships.associatedNPC) {
     setup.createRelationship(town, associatedNPC, npc, relationship.relationships.associatedNPC.relationship, relationship.relationships.associatedNPC.reciprocalRelationship)
   }
-  console.log('4')
   lib.createBuildingRelationship(town, building, npc, {
     description: relationship.description(building, npc),
     relationship: relationship.relationships.building.relationship,
     reciprocalRelationship: relationship.relationships.building.reciprocalRelationship || relationship.relationships.building.relationship
   })
-  console.log('5')
   return npc
 }
