8592846d45e0095389bf562ed7ed05e47f9ed715
Victor Glindås <glinkis@gmail.com>
2020-10-15 22:18:16 +0200

    Update building type

--- lib/buildings/_common.ts
+++ lib/buildings/_common.ts
@@ -1,12 +1,27 @@
 export interface Building {
   key: string
+  type: string
+  name: string
+  roll: {
+    magic: string
+    size: string
+    diversity: string
+    wealth: string
+    population: string
+    reputation: string
+    sin: string
+    roughness: string
+    cleanliness: string
+    expertise: string
+    activity: string
+  }
 }
 
 export interface BuildingRelationship {
   key: string
   buildingKey: string
-  npcKey: string,
-  relationship: string,
+  npcKey: string
+  relationship: string
   reciprocalRelationship: string
   description: string | null
 }
