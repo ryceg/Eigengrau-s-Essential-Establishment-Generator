f5aee9a3e09f4e9c947e9ca7e6270e8b0b6c272c
Victor Glindås <glinkis@gmail.com>
2020-10-15 22:53:32 +0200

    Refactor createTemple

--- src/MiniEstablishments/Temple/CreateTemple.js
+++ src/MiniEstablishments/Temple/CreateTemple.js
@@ -1,16 +1,19 @@
 setup.createTemple = (town, opts = {}) => {
   console.log('Creating a temple...')
+
   const temple = (opts.newBuilding || setup.createBuilding)(town, 'temple')
   const data = setup.temple
-  Object.assign(temple, {
+
+  lib.assign(temple, {
     passageName: 'TempleOutput',
     initPassage: 'TempleOutput',
     buildingType: 'temple',
     wordNoun: data.name.wordNoun.random(),
-    associatedNPC: setup.createNPC(town, Object.assign({
+    associatedNPC: setup.createNPC(town, {
       profession: ['priest', 'priest', 'priest', 'priest', 'priest', 'cleric', 'cleric', 'cleric', 'cleric', 'druid'].random(),
-      background: ['acolyte', 'acolyte', 'acolyte', 'acolyte', 'sage', 'sage', 'sage'].random()
-    }, opts.npc)),
+      background: ['acolyte', 'acolyte', 'acolyte', 'acolyte', 'sage', 'sage', 'sage'].random(),
+      ...opts.npc
+    }),
     prayerSubject: data.prayerSubject.random(),
     dedicated: [setup.misc.religion.namedGod.random(), setup.misc.religion.abstractGod.random(), setup.misc.religion.saint.random(), data.dedicated.random()].random(),
     knownFor: data.knownFor.random(),
@@ -27,28 +30,42 @@ setup.createTemple = (town, opts = {}) => {
     blessingConvey: data.blessingConvey.random(),
     blessingGift: data.blessingGift.random()
   })
-  setup.createStructure(town, temple)
-  temple.structure.templeDescriptor = `${lib.articles.output(temple.structure.material.wealth).toUpperFirst()} ${temple.structure.material.noun} ${temple.wordNoun} with a ${temple.structure.roof.verb} roof`
-  temple.name = [
-    `The ${data.name.adjective.random().toUpperFirst()} ${temple.wordNoun.toUpperFirst()} of ${data.name.plural.random().toUpperFirst()}`,
-    `The ${temple.wordNoun.toUpperFirst()} of ${data.name.soleNoun.random().toUpperFirst()}`,
-    `The ${temple.wordNoun.toUpperFirst()} of ${data.name.adjective.random().toUpperFirst()} ${data.name.plural.random().toUpperFirst()}`,
-    `The ${data.name.colour.random().toUpperFirst()} ${temple.wordNoun.toUpperFirst()}${['', ` of ${data.name.plural.random().toUpperFirst()}`, ` of ${data.name.soleNoun.random().toUpperFirst()}`].random()}`,
-    `${['', 'St. '].random() + lib.createName({ race: temple.associatedNPC.race })}'s ${temple.wordNoun.toUpperFirst()}`,
-    `${['', 'St. '].random() + lib.createName({ race: temple.associatedNPC.race })}'s ${data.name.soleNoun.random().toUpperFirst()}`
-  ].random()
-  lib.createBuildingRelationship(town, temple, temple.associatedNPC, { relationship: 'caretaker', reciprocalRelationship: 'temple' })
 
-  temple.blessing = `${temple.blessingConvey}. ${temple.blessingGift}.`
+  setup.createStructure(town, temple)
+
+  lib.assign(temple.structure, {
+    templeDescriptor: `${lib.articles.output(temple.structure.material.wealth).toUpperFirst()} ${temple.structure.material.noun} ${temple.wordNoun} with a ${temple.structure.roof.verb} roof`
+  })
+
+  lib.assign(temple, {
+    name: lib.random([
+      `The ${data.name.adjective.random().toUpperFirst()} ${temple.wordNoun.toUpperFirst()} of ${data.name.plural.random().toUpperFirst()}`,
+      `The ${temple.wordNoun.toUpperFirst()} of ${data.name.soleNoun.random().toUpperFirst()}`,
+      `The ${temple.wordNoun.toUpperFirst()} of ${data.name.adjective.random().toUpperFirst()} ${data.name.plural.random().toUpperFirst()}`,
+      `The ${data.name.colour.random().toUpperFirst()} ${temple.wordNoun.toUpperFirst()}${['', ` of ${data.name.plural.random().toUpperFirst()}`, ` of ${data.name.soleNoun.random().toUpperFirst()}`].random()}`,
+      `${['', 'St.'].random()} ${lib.createName({ race: temple.associatedNPC.race })}'s ${temple.wordNoun.toUpperFirst()}`,
+      `${['', 'St.'].random()} ${lib.createName({ race: temple.associatedNPC.race })}'s ${data.name.soleNoun.random().toUpperFirst()}`
+    ]),
+    blessing: `${temple.blessingConvey}. ${temple.blessingGift}.`
+  })
+
+  lib.createBuildingRelationship(town, temple, temple.associatedNPC, {
+    relationship: 'caretaker',
+    reciprocalRelationship: 'temple'
+  })
+
   const rollDataVariables = ['wealth', 'size', 'cleanliness']
   for (const propName of rollDataVariables) {
     lib.defineRollDataGetter(temple, data.rollData, propName)
   }
 
   // These are the full sentence printouts referenced within TempleOutput.twee
-  temple.guardReadout = `This ${temple.wordNoun} is protected by ${temple.guardedBy}.`
-  temple.aboutReadout = `Within this holy place they pray to ${temple.prayerSubject}. The temple itself was originally dedicated to ${temple.dedicated} and is known for ${temple.knownFor}.`
-  temple.interiorReadout = `You enter the ${temple.size}, ${temple.cleanliness} ${temple.wordNoun} and notice ${temple.features}. The main room is ${temple.floorPlan} in shape and is decorated with mostly ${temple.wealth} looking ${['furniture', 'relics', 'holy statues', 'holy symbols', 'stained glass windows', 'holy art'].random()}. The interior walls of the ${temple.wordNoun} are ${temple.walls} and the the ceiling is ${temple.ceiling}. The ${temple.wordNoun} was designed by ${temple.architect} and it is ${temple.complex}.`
-  temple.tippyDescription = `${lib.articles.output(temple.size).toUpperFirst()} and ${temple.cleanliness} ${temple.wordNoun} that is dedicated to ${temple.dedicated}`
+  lib.assign(temple, {
+    guardReadout: `This ${temple.wordNoun} is protected by ${temple.guardedBy}.`,
+    aboutReadout: `Within this holy place they pray to ${temple.prayerSubject}. The temple itself was originally dedicated to ${temple.dedicated} and is known for ${temple.knownFor}.`,
+    interiorReadout: `You enter the ${temple.size}, ${temple.cleanliness} ${temple.wordNoun} and notice ${temple.features}. The main room is ${temple.floorPlan} in shape and is decorated with mostly ${temple.wealth} looking ${['furniture', 'relics', 'holy statues', 'holy symbols', 'stained glass windows', 'holy art'].random()}. The interior walls of the ${temple.wordNoun} are ${temple.walls} and the the ceiling is ${temple.ceiling}. The ${temple.wordNoun} was designed by ${temple.architect} and it is ${temple.complex}.`,
+    tippyDescription: `${lib.articles.output(temple.size).toUpperFirst()} and ${temple.cleanliness} ${temple.wordNoun} that is dedicated to ${temple.dedicated}`
+  })
+
   return temple
 }
