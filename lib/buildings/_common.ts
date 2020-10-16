504c63ce20346fa5408185fc19fb176160b4cc62
Victor Glindås <glinkis@gmail.com>
2020-10-17 00:33:11 +0200

    Consolidate building types

--- lib/buildings/_common.ts
+++ lib/buildings/_common.ts
@@ -2,19 +2,33 @@ export interface Building {
   key: string
   type: string
   name: string
-  roll: {
-    magic: string
-    size: string
-    diversity: string
-    wealth: string
-    population: string
-    reputation: string
-    sin: string
-    roughness: string
-    cleanliness: string
-    expertise: string
-    activity: string
+  roll: BuildingRolls
+  wealth: string
+  sin: string
+  size: string
+  activity: string
+  roughness: string
+  cleanliness: string
+  diversity: string
+  reputation: string
+  material: {
+    noun: string
+    probability: number
   }
+}
+
+export interface BuildingRolls {
+  activity: number
+  cleanliness: number
+  diversity: number
+  expertise: number
+  magic: number
+  population: number
+  reputation: number
+  roughness: number
+  sin: number
+  size: number
+  wealth: number
 }
 
 export interface BuildingRelationship {
