a4a6bc8d9563fcf1bfb78e1512ba364f43352c3e
Rhys <36913739+ryceg@users.noreply.github.com>
2020-10-18 20:10:30 +1100

    add error testing for if it's undefined

--- lib/src/filterNull.ts
+++ lib/src/filterNull.ts
@@ -2,6 +2,7 @@
  * Removes all null entries.
  */
 export function filterNull<T> (obj: Record<string, T>) {
+  if (typeof obj === 'undefined') return
   const propNames = Object.getOwnPropertyNames(obj)
   for (let i = 0; i < propNames.length; i++) {
     const propName = propNames[i]
