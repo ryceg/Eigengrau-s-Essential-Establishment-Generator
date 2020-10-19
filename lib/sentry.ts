830a17c2b0318fe99fe36a705695a5c34cfa9cd6
Victor Glindås <glinkis@gmail.com>
2020-10-19 16:48:35 +0200

    Disable sentry locally

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -3,7 +3,12 @@ import { Integrations } from '@sentry/tracing'
 
 if (process.env.NODE_ENV === 'production') {
   console.log('Initializing sentry.')
+
+  // Disable sentry if we're running locally, even if production.
+  const enabled = location.origin !== 'file://'
+
   Sentry.init({
+    enabled,
     dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
     integrations: [
       new Integrations.BrowserTracing()
