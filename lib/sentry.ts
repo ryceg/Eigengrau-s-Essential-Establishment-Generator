690f82c4d7c2c174d8bb28235554b1288dd7df33
Victor Glindås <glinkis@gmail.com>
2020-10-19 16:41:40 +0200

    Only enable sentry in production

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,10 +1,13 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
 
-Sentry.init({
-  dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
-  integrations: [
-    new Integrations.BrowserTracing()
-  ],
-  tracesSampleRate: 0.2
-})
+if (process.env.NODE_ENV === 'production') {
+  console.log('Initializing sentry.')
+  Sentry.init({
+    dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
+    integrations: [
+      new Integrations.BrowserTracing()
+    ],
+    tracesSampleRate: 0.2
+  })
+}
