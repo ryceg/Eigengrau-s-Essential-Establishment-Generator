92b542c8b19dd07f5176ef422447ea0594e7c79f
Victor Glindås <glinkis@gmail.com>
2020-10-19 18:11:10 +0200

    Remove obsolete comment

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -4,8 +4,6 @@ import { Integrations } from '@sentry/tracing'
 if (process.env.NODE_ENV === 'production') {
   console.log('Initializing sentry.')
 
-  // We need to wait for all the sugarcube stuff to load first,
-  // as it overwrites the `window.onerror` event handler.
   Sentry.init({
     dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
     // Disable sentry if we're running locally, even if production.
