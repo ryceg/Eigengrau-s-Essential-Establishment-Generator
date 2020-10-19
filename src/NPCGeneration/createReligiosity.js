4402a4256ed477004ec970d550db7bf57b71d04d
Victor Glindås <glinkis@gmail.com>
2020-10-19 20:06:51 +0200

    Remove duplicate console log

--- src/NPCGeneration/createReligiosity.js
+++ src/NPCGeneration/createReligiosity.js
@@ -2,9 +2,8 @@ setup.createReligiosity = (town, npc) => {
   console.log(`Creating religion strength for ${npc.name}`)
   npc.roll.religiosity = lib.fm(lib.dice(2, 40) + 10, town.roll.religiosity - 50)
   npc.roll.religiosity = Math.clamp(npc.roll.religiosity, 1, 100)
-
-  console.log(`Creating religion strength for ${npc.name}`)
   console.log(npc)
+
   // TODO: transfer religiosity to the religion object; for some reason the defineRollDataGetter isn't playing nicely with it.
   // lib.defineRollDataGetter(npc, setup.npcData.religion, 'religiosity', 'religiosity')
   if (npc.religion.strength) {
