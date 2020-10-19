13c8107b43887d3ecab761d4ec8eb5fc0ee99c4d
Victor Glindås <glinkis@gmail.com>
2020-10-19 18:27:27 +0200

    Add reporting for console errors

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -1,15 +1,18 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
+import { CaptureConsole } from '@sentry/integrations'
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' && location.origin !== 'file://') {
   console.log('Initializing sentry.')
 
   Sentry.init({
     dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
     // Disable sentry if we're running locally, even if production.
-    enabled: location.origin !== 'file://',
     integrations: [
-      new Integrations.BrowserTracing()
+      new Integrations.BrowserTracing(),
+      new CaptureConsole({
+        levels: ['error']
+      })
     ],
     tracesSampleRate: 0.2
   })
