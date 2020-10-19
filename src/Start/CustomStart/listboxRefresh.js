59c17300e8d71706fed42026c5f369bec78bf3d8
ryceg <rhysg3@gmail.com>
2020-10-19 22:13:41 +1100

    minor edit

--- src/Start/CustomStart/listboxRefresh.js
+++ src/Start/CustomStart/listboxRefresh.js
@@ -1,20 +1,22 @@
-/**
- * TODO: Describe why we do this.
- */
 $('body').on('change', '.auto-update select', () => {
   const { terrain, location } = State.variables.tempTown
-
-  State.variables.listboxes = {
-    terrain: Object.keys(lib.terrain),
-    location: Object.keys(lib.terrain[terrain].location),
-    vegetation: ['thick', 'lush', 'sparse', 'desolate'],
-    origin: lib.terrain[terrain].location[location].origin,
-    primaryCrop: lib.townData.misc.primaryCrop,
-    primaryExport: lib.townData.misc.primaryExport,
-    season: Object.keys(lib.terrain.temperate.weather.season),
-    economicIdeology: lib.economicPairs,
-    politicalIdeology: lib.politicalIdeologyPairs,
-    politicalSource: ['absolute monarchy', 'constitutional monarchy', 'republic', 'anarchy']
+  switch (passage()) {
+    case 'BiomeGeneration':
+    case 'BiomeGenerationRefresh':
+      State.variables.listboxes = {
+        terrain: Object.keys(lib.terrain),
+        location: Object.keys(lib.terrain[terrain].location),
+        vegetation: ['thick', 'lush', 'sparse', 'desolate'],
+        origin: lib.terrain[terrain].location[location].origin,
+        primaryCrop: lib.townData.misc.primaryCrop,
+        primaryExport: lib.townData.misc.primaryExport,
+        season: Object.keys(lib.terrain.temperate.weather.season),
+        economicIdeology: lib.economicPairs,
+        politicalIdeology: lib.politicalIdeologyPairs,
+        politicalSource: ['absolute monarchy', 'constitutional monarchy', 'republic', 'anarchy']
+      }
+      break
+    case 'NPCProfileEdit':
   }
 
   $(document).trigger(':liveupdate')
