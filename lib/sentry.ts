a723d7d6d5628be3ded57a0cc7618ec680d9686f
Victor Glindås <glinkis@gmail.com>
2020-10-19 18:09:26 +0200

    Update sentry configuration

--- lib/sentry.ts
+++ lib/sentry.ts
@@ -4,12 +4,12 @@ import { Integrations } from '@sentry/tracing'
 if (process.env.NODE_ENV === 'production') {
   console.log('Initializing sentry.')
 
-  // Disable sentry if we're running locally, even if production.
-  const enabled = location.origin !== 'file://'
-
+  // We need to wait for all the sugarcube stuff to load first,
+  // as it overwrites the `window.onerror` event handler.
   Sentry.init({
-    enabled,
     dsn: 'https://92c3fb4ac9054e9987d39969cc55969a@o365643.ingest.sentry.io/5469192',
+    // Disable sentry if we're running locally, even if production.
+    enabled: location.origin !== 'file://',
     integrations: [
       new Integrations.BrowserTracing()
     ],
