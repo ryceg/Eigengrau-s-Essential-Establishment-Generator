97ebc527486488999db680365b87d704c9f74f33
ryceg <rhysg3@gmail.com>
2020-10-23 19:07:44 +1100

    guards have been absorbed into the factions

--- lib/town/createGuardLivery.ts
+++
@@ -1,9 +0,0 @@
-import { random } from '../src/random'
-import { guardData } from '../src/guardData'
-
-export function createGuardLivery () {
-  const insignia = random(guardData.insignia)
-  const primaryColour = random(guardData.primaryColours)
-  const secondaryColour = random(guardData.secondaryColours)
-  return `${primaryColour} and ${secondaryColour} livery adorned with an image of ${insignia}`
-}
