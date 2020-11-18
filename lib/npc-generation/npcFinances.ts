0a337e056c5e0ee742f15ceb9951ffb2525a9205
ryceg <rhysg3@gmail.com>
2020-11-18 23:57:17 +1100

    add their output

--- lib/npc-generation/npcFinances.ts
+++ lib/npc-generation/npcFinances.ts
@@ -2,29 +2,29 @@ import { NPC } from './_common'
 import { calcPercentage } from '../src/calcPercentage'
 import { Town } from '../town/_common'
 import { findProfession } from '../src/findProfession'
-import { lifestyleStandards } from './lifestyleStandards'
+import { LifestyleStandard, lifestyleStandards } from './lifestyleStandards'
 import { socialClass } from './socialClass'
 import { keys } from '../src/utils'
 
-export function wageVariation (town: Town, npc: NPC) {
+export function wageVariation (town: Town, npc: NPC): number {
   // town.roll.wealth increases or decreases npc.professionLuck by up to 10%, reflecting the strength of the economy.
   // expected range should be between -25 and 25.
   return calcPercentage(npc.roll.professionLuck, (town.roll.wealth - 50) / 5)
 }
 
-export function npcGrossIncome (town: Town, npc: NPC) {
+export function npcGrossIncome (town: Town, npc: NPC): number {
   // TODO add hobbies
   console.log(`Returning ${npc.name}'s gross income...`)
   const profession = findProfession(town, npc)
   return Math.round(calcPercentage(profession.dailyWage, (wageVariation(town, npc), (town.roll.wealth - 50) / 3)))
 }
 
-export function npcNetIncome (town: Town, npc: NPC) {
+export function npcNetIncome (town: Town, npc: NPC): number {
   console.log(`Returning ${npc.name}'s net income...`)
   return Math.round(calcPercentage(npcGrossIncome(town, npc), -npcTaxRate(town, npc)))
 }
 
-export function npcLifestyleStandard (town: Town, npc: NPC) {
+export function npcLifestyleStandard (town: Town, npc: NPC): LifestyleStandard {
   console.log(`Returning ${npc.name}'s lifestyle standard...`)
   const income = npcNetIncome(town, npc)
   for (const lifestyleStandard of Object.values(lifestyleStandards)) {
@@ -35,22 +35,22 @@ export function npcLifestyleStandard (town: Town, npc: NPC) {
   return lifestyleStandards.modest
 }
 
-export function npcLifestyleExpenses (town: Town, npc: NPC) {
+export function npcLifestyleExpenses (town: Town, npc: NPC): number {
   console.log(`Returning ${npc.name}'s lifestyle expenses...`)
   const income = npcGrossIncome(town, npc)
   return Math.round(income * (npcLifestyleStandard(town, npc).dailyWagePercentage / 100))
 }
 
-export function npcTotalLifestyleExpenses (town: Town, npc: NPC) {
+export function npcTotalLifestyleExpenses (town: Town, npc: NPC): number {
   return Math.round(npcLifestyleExpenses(town, npc) + npcLifestyleStandard(town, npc).incomeThreshold)
 }
 
-export function npcProfit (town: Town, npc: NPC) {
+export function npcProfit (town: Town, npc: NPC): number {
   console.log(`Returning ${npc.name}'s profit...`)
   return Math.round(npcNetIncome(town, npc) - npcTotalLifestyleExpenses(town, npc))
 }
 
-export function npcTaxRate (town: Town, npc: NPC) {
+export function npcTaxRate (town: Town, npc: NPC): number {
   let totalTax = 0
 
   for (const tax of keys(town.taxes)) {
