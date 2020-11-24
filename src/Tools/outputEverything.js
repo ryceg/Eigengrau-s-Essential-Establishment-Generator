b8b41fbf27c3892816dfef1bff85eff0ba6e93b3
ryceg <rhysg3@gmail.com>
2020-11-24 23:31:23 +1100

    add npcs and buildings

--- src/Tools/outputEverything.js
+++ src/Tools/outputEverything.js
@@ -1,7 +1,20 @@
 setup.outputEverything = () => {
   const output = {
     start: setup.textify('Start'),
-    town: setup.textify('TownOutput')
+    town: setup.textify('TownOutput'),
+    buildings: {},
+    factions: {},
+    npcs: {}
+  }
+  for (const building of State.variables.town.buildings) {
+    output.buildings[building.key] = setup.textify(building.passageName, building)
+  }
+  for (const faction of Object.keys(State.variables.town.factions)) {
+    output.factions[State.variables.town.factions[faction].key] = setup.textify(State.variables.town.factions[faction].passageName, State.variables.town.factions[faction])
+  }
+
+  for (const npc of Object.keys(State.variables.npcs)) {
+    output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc])
   }
   return JSON.stringify(output)
 }
