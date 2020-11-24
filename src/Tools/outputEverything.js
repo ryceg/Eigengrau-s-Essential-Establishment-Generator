715cd74e87734d4f55371045f61fab0965fca201
ryceg <rhysg3@gmail.com>
2020-11-25 00:32:40 +1100

    remove buttons

--- src/Tools/outputEverything.js
+++ src/Tools/outputEverything.js
@@ -1,3 +1,14 @@
+
+setup.textify = function (passageName, currentPassage) {
+  if (currentPassage) State.variables.currentPassage = currentPassage
+  State.temporary.isTextOutput = true
+  const raw = Story.get(passageName).processText()
+  const $offshore = $('<div />')
+  $offshore.wiki(raw)
+  $offshore.find('button').remove()
+  return Util.escape($offshore.html())
+}
+
 setup.outputEverything = () => {
   const output = {
     start: setup.textify('Start'),
@@ -9,19 +20,13 @@ setup.outputEverything = () => {
   for (const building of State.variables.town.buildings) {
     output.buildings[building.key] = setup.textify(building.passageName, building)
   }
-  for (const faction of Object.keys(State.variables.town.factions)) {
-    output.factions[State.variables.town.factions[faction].key] = setup.textify(State.variables.town.factions[faction].passageName, State.variables.town.factions[faction])
-  }
+  // for (const faction of Object.keys(State.variables.town.factions)) {
+  //   output.factions[State.variables.town.factions[faction].key] = setup.textify(State.variables.town.factions[faction].passageName, State.variables.town.factions[faction]);
+  // }
 
-  for (const npc of Object.keys(State.variables.npcs)) {
-    output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc])
-  }
+  // for (const npc of Object.keys(State.variables.npcs)) {
+  //   // eslint-disable-next-line
+  //   output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc]);
+  // }
   return JSON.stringify(output)
-}
-
-setup.textify = (passageTitle, currentPassage) => {
-  if (currentPassage) State.variables.currentPassage = currentPassage
-  State.temporary.isTextOutput = true
-  const temp = Util.escape($('<div />').wiki(Story.get(passageTitle).processText()).html())
-  return temp
 }
