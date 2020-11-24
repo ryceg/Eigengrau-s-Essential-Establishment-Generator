abaad0db4c056a16610f34481561c0a32b5438ad
ryceg <rhysg3@gmail.com>
2020-11-25 00:35:23 +1100

    Fix escaping issue

--- src/Tools/outputEverything.js
+++ src/Tools/outputEverything.js
@@ -20,13 +20,12 @@ setup.outputEverything = () => {
   for (const building of State.variables.town.buildings) {
     output.buildings[building.key] = setup.textify(building.passageName, building)
   }
-  // for (const faction of Object.keys(State.variables.town.factions)) {
-  //   output.factions[State.variables.town.factions[faction].key] = setup.textify(State.variables.town.factions[faction].passageName, State.variables.town.factions[faction]);
-  // }
+  for (const faction of Object.keys(State.variables.town.factions)) {
+    output.factions[State.variables.town.factions[faction].key] = setup.textify(State.variables.town.factions[faction].passageName, State.variables.town.factions[faction])
+  }
 
-  // for (const npc of Object.keys(State.variables.npcs)) {
-  //   // eslint-disable-next-line
-  //   output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc]);
-  // }
+  for (const npc of Object.keys(State.variables.npcs)) {
+    output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc])
+  }
   return JSON.stringify(output)
 }
