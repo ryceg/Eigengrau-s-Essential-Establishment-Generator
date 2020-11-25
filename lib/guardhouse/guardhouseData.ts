bf40ad4d1c4e95d44802a1ed0addd11fafba6c47
Victor Glindås <glinkis@gmail.com>
2020-11-25 14:33:10 +0100

    Fix guardhouse data

--- lib/guardhouse/guardhouseData.ts
+++ lib/guardhouse/guardhouseData.ts
@@ -1,46 +1,35 @@
-import { constrainRecord } from '../src/constrainRecord'
-import { ThresholdTable } from '../src/rollFromTable'
+import { NPC } from '../npc-generation/_common'
 import { Town } from '../town/_common'
 
 interface GuardhouseData {
-  rollData: {
-    wealth: ThresholdTable
-    size: ThresholdTable
-    cleanliness: ThresholdTable
-    militarisation: ThresholdTable
-  }
   name: {
     wordNoun: string[]
-    noun: string[]
     adjective: string[]
-    rider: string[]
   }
-  notableFeature: any[]
+  notableFeature: {
+    exclusions?(town: Town): boolean
+    function(): string
+  }[]
   evidenceLocker: {
-    items: any[]
+    items: {
+      function(): string
+    }[]
   }
   get: {
-    officeItems: any[]
-    officeDescription: any[]
-    holdingCell: any[]
+    officeItems: string[]
+    officeDescription: {
+      wealth: number
+      size: number
+      description: string
+    }[]
+    holdingCell: {
+      reason: string
+      base?: Partial<NPC>
+    }[]
   }
 }
 
-export const guardhouseData = constrainRecord<GuardhouseData>()({
-  rollData: {
-    wealth: [
-
-    ],
-    size: [
-
-    ],
-    cleanliness: [
-
-    ],
-    militarisation: [
-
-    ]
-  },
+export const guardhouseData: GuardhouseData = {
   name: {
     wordNoun: [
       'watchtower',
@@ -54,9 +43,6 @@ export const guardhouseData = constrainRecord<GuardhouseData>()({
       'command post',
       'office',
       'command'
-    ],
-    noun: [
-
     ],
     adjective: [
       'yellow',
@@ -76,45 +62,57 @@ export const guardhouseData = constrainRecord<GuardhouseData>()({
       'short',
       'tall',
       'towering'
-    ],
-    rider: [
-
-    ],
-    unique: [
-
     ]
   },
   notableFeature: [
     // the guardhouse is known for
     {
-      function () { return 'housing the entire list of laws of the town, codified into stone tablets available for anyone to view in the foyer, signifying that the law is for everyone.' }
+      function () {
+        return 'housing the entire list of laws of the town, codified into stone tablets available for anyone to view in the foyer, signifying that the law is for everyone.'
+      }
     },
     {
-      function () { return 'a large broken stone statue which is displayed by the foyer. It is kept there for its historical and cultural significance.' }
+      function () {
+        return 'a large broken stone statue which is displayed by the foyer. It is kept there for its historical and cultural significance.'
+      }
     },
     {
-      function () { return 'a small complex by the side is a cooperative of the town, a popular place to shop and chat. Guards and townsfolk are seen operating it together.' }
+      function () {
+        return 'a small complex by the side is a cooperative of the town, a popular place to shop and chat. Guards and townsfolk are seen operating it together.'
+      }
     },
     {
-      function () { return 'a bathing pool in the courtyard which is free for citizens to use, safe in the knowledge that their possessions are kept in the guardhouse, away from sticky fingers.' }
+      function () {
+        return 'a bathing pool in the courtyard which is free for citizens to use, safe in the knowledge that their possessions are kept in the guardhouse, away from sticky fingers.'
+      }
     },
     {
-      function () { return 'The entire list of laws of the town, codified into stone tablets available for anyone to view in the foyer, signifying that the law is for everyone.' }
+      function () {
+        return 'The entire list of laws of the town, codified into stone tablets available for anyone to view in the foyer, signifying that the law is for everyone.'
+      }
     },
     {
-      function () { return 'being incredibly fortified; after protests, the guards fortified the building, which made the citizenry protest even more, with the situation soon spiraling out of control.' }
+      function () {
+        return 'being incredibly fortified; after protests, the guards fortified the building, which made the citizenry protest even more, with the situation soon spiraling out of control.'
+      }
     },
     {
-      function () { return 'housing a huge vault that wealthy or high ranking visitors to the town can store valuable items in during their stay, knowing it is under constant guard.' }
+      function () {
+        return 'housing a huge vault that wealthy or high ranking visitors to the town can store valuable items in during their stay, knowing it is under constant guard.'
+      }
     },
     {
-      function () { return 'also housing the civil and criminal court building.' }
+      function () {
+        return 'also housing the civil and criminal court building.'
+      }
     },
     {
       exclusions (town: Town) {
-        if (town.economicIdeology !== 'primitivism' && town.roll.sin > 50) return true
+        return town.economicIdeology !== 'primitivism' && town.roll.sin > 50
       },
-      function () { return "the town's moneylenders also occupy the same building. The townsfolk often look to it with disgust as moneylender and guard are often a pair." }
+      function () {
+        return "the town's moneylenders also occupy the same building. The townsfolk often look to it with disgust as moneylender and guard are often a pair."
+      }
     }
 
   ],
@@ -122,64 +120,104 @@ export const guardhouseData = constrainRecord<GuardhouseData>()({
     // Inside the evidence locker, there is ___
     items: [
       {
-        function () { return 'some confiscated weaponry from captured bandits. One of the weapons is highly personalised and decorated.' }
+        function () {
+          return 'some confiscated weaponry from captured bandits. One of the weapons is highly personalised and decorated.'
+        }
       },
       {
-        function () { return 'an object given by a concerned townsperson. The object was discovered by the townsperson recently, and they were reportedly immensely disturbed as they dropped it off.' }
+        function () {
+          return 'an object given by a concerned townsperson. The object was discovered by the townsperson recently, and they were reportedly immensely disturbed as they dropped it off.'
+        }
       },
       {
-        function () { return 'a caged animal that was taken from a swindling vendor. One of the guards has acquired a fondness for it.' }
+        function () {
+          return 'a caged animal that was taken from a swindling vendor. One of the guards has acquired a fondness for it.'
+        }
       },
       {
-        function () { return "an excellently forged letter from someone important. It's location suggests it was forgotten or misplaced before it could be destroyed." }
+        function () {
+          return "an excellently forged letter from someone important. It's location suggests it was forgotten or misplaced before it could be destroyed."
+        }
       },
       {
-        function () { return 'a barrel that hides contraband underneath a thin layer of cured meat.' }
+        function () {
+          return 'a barrel that hides contraband underneath a thin layer of cured meat.'
+        }
       },
       {
-        function () { return 'a lone sending stone. Its pair is nowhere to be seen.' }
+        function () {
+          return 'a lone sending stone. Its pair is nowhere to be seen.'
+        }
       },
       {
-        function () { return 'a couple of improperly made potions or draughts. It was taken from a illicit alchemist working for a syndicate.' }
+        function () {
+          return 'a couple of improperly made potions or draughts. It was taken from a illicit alchemist working for a syndicate.'
+        }
       },
       {
-        function () { return "a broken cloak which still has vague traces of magic. It's decorated with religious symbols." }
+        function () {
+          return "a broken cloak which still has vague traces of magic. It's decorated with religious symbols."
+        }
       },
       {
-        function () { return 'forfeited precious goods from a merchant. The goods were stolen to be resold at a black market' }
+        function () {
+          return 'forfeited precious goods from a merchant. The goods were stolen to be resold at a black market'
+        }
       },
       {
-        function () { return "an exotic plant, confiscated from a traveller. It's apparently dear to them, and they were last seen shouting expletives at the building." }
+        function () {
+          return "an exotic plant, confiscated from a traveller. It's apparently dear to them, and they were last seen shouting expletives at the building."
+        }
       },
       {
-        function () { return 'some bloodstained and ripped clothing that was found in an alleyway. The owner has yet to be found and noone has come forward with clues.' }
+        function () {
+          return 'some bloodstained and ripped clothing that was found in an alleyway. The owner has yet to be found and noone has come forward with clues.'
+        }
       },
       {
-        function () { return 'a scroll for a dangerous spell that was found being carried by a novice spellcaster' }
+        function () {
+          return 'a scroll for a dangerous spell that was found being carried by a novice spellcaster'
+        }
       },
       {
-        function () { return 'a small bag of gold coins stamped with the seal of a city that fell to ruin centuries ago, discovered in the rags of a man who starved to death in an alleyway.' }
+        function () {
+          return 'a small bag of gold coins stamped with the seal of a city that fell to ruin centuries ago, discovered in the rags of a man who starved to death in an alleyway.'
+        }
       },
       {
-        function () { return 'a list of persons that a corrupt guard might have been given to create or plant incriminating evidence on.' }
+        function () {
+          return 'a list of persons that a corrupt guard might have been given to create or plant incriminating evidence on.'
+        }
       },
       {
-        function () { return 'some confiscated weaponry from captured bandits. One of the weapons is highly personalised and decorated.' }
+        function () {
+          return 'some confiscated weaponry from captured bandits. One of the weapons is highly personalised and decorated.'
+        }
       },
       {
-        function () { return 'some documents in foreign writing or language. Kept in a small scroll case.' }
+        function () {
+          return 'some documents in foreign writing or language. Kept in a small scroll case.'
+        }
       },
       {
-        function () { return 'some used bindings, showing a lot of damage. Different from those used by the guards.' }
+        function () {
+          return 'some used bindings, showing a lot of damage. Different from those used by the guards.'
+        }
       },
       {
-        function () { return 'some gaudy outfit, the style and make of which are rare in the general area.' }
+        function () {
+          return 'some gaudy outfit, the style and make of which are rare in the general area.'
+        }
       },
       {
-        function () { return 'some pendant. The symbols relate to an important organisation.' }
+        function () {
+          return 'some pendant. The symbols relate to an important organisation.'
+        }
       },
       {
-        function () { return 'a cane with a hidden compartment. It belonged to a noble of ill repute.' }
+        function () {
+          return 'a cane with a hidden compartment. It belonged to a noble of ill repute.'
+        }
       }
     ]
   },
@@ -442,4 +480,4 @@ export const guardhouseData = constrainRecord<GuardhouseData>()({
       }
     ]
   }
-})
+}
