918339a1af9036f9c339b6efb6ba3cffeb7d6f7d
ryceg <rhysg3@gmail.com>
2020-11-25 12:22:21 +1100

    automatically copy to clipboard

--- src/Tools/outputEverything.js
+++ src/Tools/outputEverything.js
@@ -6,7 +6,7 @@ setup.textify = function (passageName, currentPassage) {
   const $offshore = $('<div />')
   $offshore.wiki(raw)
   $offshore.find('button').remove()
-  return Util.escape($offshore.html())
+  return `${Util.escape($offshore.html())}`
 }
 
 setup.outputEverything = () => {
@@ -27,5 +27,34 @@ setup.outputEverything = () => {
   for (const npc of Object.keys(State.variables.npcs)) {
     output.npcs[State.variables.npcs[npc].key] = setup.textify(State.variables.npcs[npc].passageName, State.variables.npcs[npc])
   }
-  return JSON.stringify(output)
+  const json = JSON.stringify(output)
+  updateClipboard(json)
+  return json
+}
+
+$(document).on(':passageinit', function () {
+  const params = new URL(document.location).searchParams
+  const key = params.get('export')
+  if (key) {
+    // const obj = setup.findViaKey(key)
+    Engine.play('BlankOutput', true)
+    // $('#everything').text(setup.textify(obj.passageName, obj))
+  }
+})
+
+setup.copyText = () => {
+  const copyText = document.querySelector('#everything')
+  navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
+    if (result.state === 'granted' || result.state === 'prompt') {
+      updateClipboard(copyText)
+    }
+  })
+}
+
+function updateClipboard (copyText) {
+  navigator.clipboard.writeText(copyText).then(function () {
+    /* clipboard successfully set */
+  }, function () {
+    /* clipboard write failed */
+  })
 }
