9a084e8ad0242694f5851a20563ed45fa2d07eb2
ryceg <rhysg3@gmail.com>
2020-12-20 11:42:02 +1100

    Add more stuff

--- src/MiniEstablishments/Guardhouse/createGuardhouse.js
+++ src/MiniEstablishments/Guardhouse/createGuardhouse.js
@@ -14,7 +14,41 @@ setup.createGuardhouse = (town, opts) => {
     needsWordNoun: false,
     associatedNPC: setup.createNPC(town, { profession: 'guard', ...opts.associatedNPC })
   })
-
-  lib.assign(guardhouse, lib.weightedRandomFetcher(town, lib.guardhouseData.notableFeature, guardhouse, undefined))
+  lib.createBuildingRelationship(town, guardhouse, guardhouse.associatedNPC, { relationship: 'worker', reciprocalRelationship: 'place of employment' })
+  guardhouse.notableFeature = lib.weightedRandomFetcher(town, lib.guardhouseData.notableFeature, guardhouse, undefined, 'function')
+  guardhouse.name = setup.createGuardhouseName(town)
+  lib.createStructure(town, guardhouse)
+  const props = ['cleanliness', 'expertise']
+  for (const propName of props) {
+    lib.defineRollDataGetter(guardhouse, lib.guardhouseData.rollData[propName].rolls, propName)
+  }
   return guardhouse
+}
+
+/**
+ * Returns a guardhouse name.
+ * @param {import("../../../lib/town/_common").Town} town
+ */
+setup.createGuardhouseName = (town) => {
+  const potentialUniqueNames = [
+    'Emberhead Garrison',
+    'Nightwatch',
+    'Bulwark',
+    'The Compass',
+    'The Watchtower',
+    'The Veil',
+    'Watch Headquarters',
+    'Bastion',
+    'Hillside Watchtower'
+  ]
+  if (lib.findInArray(town.buildings, 'buildingType', 'docks')) potentialUniqueNames.push('The Dockside Guardhouse')
+  if (random(3) > 2) {
+    return lib.toTitleCase(potentialUniqueNames.random())
+  } else {
+    return lib.toTitleCase([
+      `The ${town.type} ${lib.guardhouseData.name.wordNoun.random()}`,
+      `The ${lib.guardhouseData.name.adjective.random()} ${lib.guardhouseData.name.wordNoun.random()}`,
+      `The ${lib.guardhouseData.name.adjective.random()} ${town.type} ${lib.guardhouseData.name.wordNoun.random()}`
+    ].random())
+  }
 }
