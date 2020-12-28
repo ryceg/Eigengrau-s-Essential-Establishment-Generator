742e92cbef95ec630291ee166e5b8c29b620b8ba
Glinkis <glinkis@gmail.com>
2020-12-28 22:44:47 +0100

    Refactor

--- src/NPCGeneration/getLifeEvents.ts
+++ src/NPCGeneration/getLifeEvents.ts
@@ -8,19 +8,23 @@ export const getLifeEvents = (npc: NPC) => {
 
   if (npc.lifeEvents.length === 1) {
     const [lifeEvent] = npc.lifeEvents
-    const other = ['Nothing else really would stand out', "I'm afraid that's all that would impress the likes of you", "I'm afraid that I've still got quite a ways to go in the ways of impressive tales, that's about it.", "And that's really honestly about it...", "Uh, that's really the only interesting story I have, to be honest."].random()
-    return `This one time ${lifeEvent} ${[other]}`
+    const other = lib.random([
+      'Nothing else really would stand out',
+      "I'm afraid that's all that would impress the likes of you",
+      "I'm afraid that I've still got quite a ways to go in the ways of impressive tales, that's about it.",
+      "And that's really honestly about it...",
+      "Uh, that's really the only interesting story I have, to be honest."
+    ])
+    return `This one time ${lifeEvent} ${other}`
   }
 
   const first = lib.first(npc.lifeEvents)
-  const misc = npc.lifeEvents.slice(1, -1)
   const last = lib.last(npc.lifeEvents)
 
-  const getMisc = () => {
-    return misc.reduce((total, lifeEvent) => {
-      return total + lib.random([
-        lifeEvent.toUpperFirst(),
-        lifeEvent.toUpperFirst(),
+  const misc = npc.lifeEvents.slice(1, -1).reduce((total, lifeEvent) => {
+    return total + lib.random([
+      lifeEvent.toUpperFirst(),
+      lifeEvent.toUpperFirst(),
         `How could I ever forget the time ${lifeEvent} `,
         `After that, ${lifeEvent}`,
         `Not to mention there was the time ${lifeEvent}`,
@@ -32,9 +36,8 @@ export const getLifeEvents = (npc: NPC) => {
         `I forget exactly when, but at one point, ${lifeEvent} `,
         `At one point ${lifeEvent} `,
         `I'll always remember the time ${lifeEvent} `
-      ])
-    }, '')
-  }
+    ])
+  }, '')
 
-  return `This one time ${first} ${getMisc()} At one point, ${last}`
+  return `This one time ${first} ${misc} At one point, ${last}`
 }
