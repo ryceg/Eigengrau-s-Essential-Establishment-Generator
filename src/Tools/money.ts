413fdb2ed7037edabc1615cae503297a6d30fde7
ryceg <rhysg3@gmail.com>
2020-12-29 17:34:47 +1100

    Add money

--- src/Tools/money.ts
+++ src/Tools/money.ts
@@ -1,6 +1,8 @@
+import { getPriceMod } from 'src/Tools/getPriceMod'
+
 // uses settings.silverStandard, setup.getPriceMod
-setup.money = (copper, priceMod) => {
-  if (priceMod) copper = Math.round(copper * setup.getPriceMod(priceMod))
+export const money = (copper: number, priceMod: number) => {
+  if (priceMod) copper = Math.round(copper * getPriceMod(priceMod))
   let silver
   let gold
   let isNegative = false
