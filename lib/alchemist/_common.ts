dd10ea4b31a8dc028777979abf1f31d265e302d6
Glinkis <glinkis@gmail.com>
2021-01-15 17:29:42 +0100

    Update alchemist type

--- lib/alchemist/_common.ts
+++ lib/alchemist/_common.ts
@@ -1,6 +1,8 @@
 import { Building, BuildingRolls } from '../buildings/_common'
 
 export interface Alchemist extends Building {
+  buildingType: string
+  initPassage: string
   priceModifier: number
   roll: BuildingRolls & {
     activity: number
