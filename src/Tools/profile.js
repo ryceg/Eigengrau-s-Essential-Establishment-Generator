719af7f26aa9d4b73a0ad3016e3cf1b4fa409cce
ryceg <rhysg3@gmail.com>
2021-01-19 22:12:02 +1100

    Extract profile from widget to macro

---
+++ src/Tools/profile.js
@@ -0,0 +1,25 @@
+Macro.add('profile', {
+  handler () {
+    if (!this.args[0]) return this.error('No arguments provided for profile.')
+    const obj = this.args[0]
+    const readout = this.args[1] || obj.name
+    const id = Util.slugify(obj.key || obj.name || obj.description || obj.wordNoun || 'profile')
+    const tip = $('<span class="tip"/>').addClass(id)
+    $(`<a class="link-internal macro-link">${readout}</a>`)
+      .ariaClick(() => {
+        State.variables.currentPassage = obj
+        setup.history(obj, obj.passageName, readout)
+
+        if (settings.showSliders && obj.initPassage) {
+          Engine.play(obj.initPassage)
+        } else {
+          Engine.play(obj.passageName)
+        }
+      })
+      .appendTo(tip)
+    /* do any other title addition and stuff here */
+    setup.makeTippyTitle($(tip)[0], obj)
+    tippy(tip.get(0))
+    $(this.output).append(tip)
+  }
+})
