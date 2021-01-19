a7799589ceb1d2479f3d7a603c3e9cc3e9df206c
ryceg <rhysg3@gmail.com>
2021-01-19 22:11:13 +1100

    shifted addTippyAccessibility

--- src/Settings/resizeHeader.js
+++ src/Settings/resizeHeader.js
@@ -22,8 +22,3 @@ function isScrolledOver (amount) {
 window.addEventListener('scroll', scrollFunction)
 
 $(document.documentElement).attr('lang', 'en')
-
-// Adds tippyAccessibility on passage finish to ensure that no tip is left behind.
-$(document).on(':passagedisplay', function (ev) {
-  lib.addTippyAccessibility()
-})
