4a9d8f2669e5f8a7bd6bc758caf361d33ea0fdbd
Glinkis <glinkis@gmail.com>
2021-01-19 13:25:48 +0100

    Remove redundant cast

--- src/Tools/getPriceMod.ts
+++ src/Tools/getPriceMod.ts
@@ -8,10 +8,10 @@ interface Modifiable {
 export const getPriceMod = (priceModSource: number | Modifiable): number => {
   console.log(priceModSource)
   if (typeof priceModSource === 'object' && typeof priceModSource.priceModifier === 'number') {
-    return Number(1 - (priceModSource.priceModifier / 100))
+    return 1 - (priceModSource.priceModifier / 100)
   }
   if (typeof priceModSource === 'number') {
-    return Number(1 - (priceModSource / 100))
+    return 1 - (priceModSource / 100)
   }
   return 1
 }
