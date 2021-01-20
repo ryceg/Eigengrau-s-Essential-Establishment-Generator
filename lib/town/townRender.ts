6fcae2bcf0fe4a3f70d311fc4a70bcb7e7561814
Glinkis <glinkis@gmail.com>
2021-01-20 14:35:18 +0100

    Evaluate town law description at runtime

--- lib/town/townRender.ts
+++ lib/town/townRender.ts
@@ -55,53 +55,24 @@ export function townRender (town: Town) {
 
   switch (town.politicalIdeology) {
     case 'kleptocracy':
-      if (town.roll.law > 90) {
-        town.law = `Law in ${town.name} is completely one sided. Those that breach the complex and codified list of laws and do not have the protection of the Thieves' Guild can expect hard labor, incarceration, or public execution. The Thieves' Guild controls the law, and have free reign as a result.`
-      } else if (town.roll.law > 80) {
-        town.law = `Law in ${town.name} is totally one sided. Those that breach the complex list of laws and do not have the protection of the Thieves' Guild can expect hard labor, incarceration, or sometimes, even public execution. The Thieves' Guild controls the law, and have free reign as a result.`
-      } else if (town.roll.law > 70) {
-        town.law = `Law in ${town.name} is very much in favour of the Thieves' Guild that runs the town. Their political power renders them virtually immune to the law, and criminals fear them more than the judicial system.`
-      } else if (town.roll.law > 60) {
-        town.law = `Law in ${town.name} is in favour of the Thieves' Guild that runs the town. Their political power renders them largely above the law, and criminals fear them more than the judicial system.`
-      } else if (town.roll.law > 50) {
-        town.law = `Law in ${town.name} is in favour of the Thieves' Guild that run the kleptocratic government. Their political power renders them mostly the law, and crime is rampant.`
-      } else if (town.roll.law > 40) {
-        town.law = `Law in ${town.name} is slack, perhaps intentionally so- the Thieves' Guild that control the government would not be above bending the laws in their favour.`
-      } else if (town.roll.law > 30) {
-        town.law = `Law in ${town.name} is quite slack as a result of a kleptocratic government. Those not aligned with the Thieves' Guild can expect harsh fines for crime.`
-      } else if (town.roll.law > 20) {
-        town.law = `Law in ${town.name} is almost non existant for the members of the kleptocratic government that controls ${town.name}. For the common people, though, one can expect to lose a hand for stealing a loaf of bread without paying your Guild membership fees.`
-      } else if (town.roll.law <= 20) {
-        town.law = `Law in ${town.name} is basically non existant for the Thieves' Guild members that control the government. Common folk can lose a hand or their head for stealing a loaf of bread without paying their Guild membership fees, though.`
-      }
       break
     default:
       if (town.roll.law > 90) {
         town.roll.guardFunding += 25
-        town.law = `Law in ${town.name} is overwhelmingly punitive, and those that breach the complex and codified list of laws can expect hard labor, incarceration, or public execution. Crime is seen as a stain, which cannot be erased, and only through retribution can it be redeemed.`
       } else if (town.roll.law > 80) {
         town.roll.guardFunding += 20
-        town.law = `Law in ${town.name} is extremely punitive, and those that breach the complex list of laws can expect hard labor, incarceration, or sometimes public execution. Crime is an ugly stain on humanity, to be punished wherever possible.`
       } else if (town.roll.law > 70) {
         town.roll.guardFunding += 15
-        town.law = `Law in ${town.name} is very much punitive based, and those that breach the laws can expect hard labor, incarceration, or sometimes public execution. Crime is an ugly stain on humanity, to be punished.`
       } else if (town.roll.law > 60) {
         town.roll.guardFunding += 15
-        town.law = `Law in ${town.name} is punitive based, and those that breach the laws can expect hard labor, incarceration, fines, or, in some instances, public execution. Crime is an ugly stain on humanity, to be removed wherever possible.`
       } else if (town.roll.law > 50) {
         town.roll.guardFunding += 15
-        town.law = `Law in ${town.name} is punitive, and those that breach the laws can expect hard labor, incarceration, fines, or, in rare instances, execution.`
       } else if (town.roll.law > 40) {
         town.roll.guardFunding += 10
-        town.law = `Law in ${town.name} is somewhat reform-based, and those that breach the laws can expect hard labor, incarceration, or fines. The death penalty is typically reserved for traitors to the state.`
       } else if (town.roll.law > 30) {
         town.roll.guardFunding += 10
-        town.law = `Law in ${town.name} is reform-based, and those that breach the laws can expect incarceration, fines, or going to an asylum for reform. The death penalty is reserved only for traitors to the state.`
       } else if (town.roll.law > 20) {
         town.roll.guardFunding += 5
-        town.law = `Law in ${town.name} is reform-based, and those that breach the laws can expect incarceration, fines, asylums, or work-release programs to provide them with the chance to not become a recidivist. The death penalty is reserved only for traitors to the state.`
-      } else if (town.roll.law <= 20) {
-        town.law = `Law in ${town.name} is reform-based, and those that breach the laws can expect fines, penalties, or work-release programs to provide them with the best possible chance to not become a recidivist.`
       }
   }
 
