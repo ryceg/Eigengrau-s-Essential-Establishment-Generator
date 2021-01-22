82b04dce79d801f6ecde85716c3f05086c7e4b6b
Rhys <36913739+ryceg@users.noreply.github.com>
2021-01-22 15:41:26 +1100

    Add fifth edition books

--- lib/src/worldType.ts
+++ lib/src/worldType.ts
@@ -18,3 +18,90 @@ export type AlignmentsAbbreviated = 'LG' | 'NG' | 'CG' | 'LN' | 'LG' | 'LE' | 'N
 export type Alignments = 'Lawful Good' | 'Neutral Good' | 'Chaotic Good' | 'Lawful Neutral' | 'Lawful Good' | 'Lawful Evil' | 'Neutal' | 'Lawful Neutral' | 'Chaotic Neutral' | 'Chaotic Evil' | 'Neutral Evil'
 
 export type ClericDomains = 'arcana' | 'death' | 'forge' | 'grave' | 'knowledge' | 'life' | 'light' | 'nature' | 'order' | 'peace' | 'tempest' | 'trickery' | 'twilight' | 'war'
+export interface ClericDomainInfo {
+  source: FifthEditionAbbr
+  page?: number
+}
+
+export const ClericDomain: Record<ClericDomains, ClericDomainInfo> = {
+  arcana: {
+    source: 'SCAG'
+  },
+  death: {
+    source: 'DMG'
+  },
+  forge: {
+    source: 'XGtE'
+  },
+  grave: {
+    source: 'XGtE'
+  },
+  knowledge: {
+    source: 'PHB'
+  },
+  life: {
+    source: 'PHB'
+  },
+  light: {
+    source: 'PHB'
+  },
+  nature: {
+    source: 'PHB'
+  },
+  order: {
+    source: 'TCoE'
+  },
+  peace: {
+    source: 'TCoE'
+  },
+  tempest: {
+    source: 'PHB'
+  },
+  trickery: {
+    source: 'PHB'
+  },
+  twilight: {
+    source: 'TCoE'
+  },
+  war: {
+    source: 'PHB'
+  }
+}
+
+export type FifthEditionAbbr =
+  /** 'Acquistions Incorporated' */
+  'AcInc'
+  /** 'Dungeon Master\'s Guide' */
+  | 'DMG'
+  /** 'Explorer\'s Guidet to Wildemount' */
+  | 'EGtW'
+  /** 'Eberron: Rising From The Last War' */
+  | 'ERLW'
+  /** 'Elemental Evil Player\'s Companion' */
+  | 'EEPC'
+  /** 'Guildmasters\' Guide to Ravnica' */
+  | 'GGtR'
+  /** 'Icewind Dale - Rime of the Frostmaiden' */
+  | 'RotF'
+  /** 'Locathah Rising' */
+  | 'LR'
+  /** 'Monster Manual' */
+  | 'MM'
+  /** 'Mystic Odysseys of Theros' */
+  | 'MOoT'
+  /** 'Mordenkainen\'s Tome of Foes ' */
+  | 'MToF'
+  /** 'One Grung Above Affiliate Link' */
+  | 'OGA'
+  /** 'Players Handbook' */
+  | 'PHB'
+  /** 'Sword Coast Adventurer\'s Guide' */
+  | 'SCAG'
+  /** 'Tasha\'s Cauldron of Everything' */
+  | 'TCoE'
+  /** 'Volo\'s Guide to Monsters' */
+  | 'VGtM'
+  /** 'Wayfarer\'s Guide to Eberron Affiliate Link' */
+  | 'WGtE'
+  /** 'Xanathar\'s Guide to Everything' */
+  | 'XGtE'
