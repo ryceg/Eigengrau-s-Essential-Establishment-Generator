f1971f94b16f55fa7cfe4d2ad92057c7456c8ae6
ryceg <rhysg3@gmail.com>
2021-01-25 22:01:05 +1100

    fetchDeity

--- lib/religion/createTownReligion.ts
+++ lib/religion/createTownReligion.ts
@@ -1,12 +1,30 @@
 import { Town } from '../town/_common'
-import { random } from '../src/random'
-import { PantheonTypes } from './religion'
+import { Deity, PantheonTypes } from './religion'
+import { getPredominantRace } from '../town/getPredominantRace'
+import { calcPercentage } from '../src/calcPercentage'
+import { weightedRandomFetcher } from '../src/weightedRandomFetcher'
 
-export const createTownReligion = (town: Town, pantheon: PantheonTypes, god: string) => {
-  if (!pantheon) pantheon = 'greek'
-  if (!god) god = pickDeity(town, pantheon)
+export const createTownReligion = (town: Town, pantheon: PantheonTypes, deity: string) => {
+  if (!pantheon) town.religion.pantheon = 'greek'
+  if (!deity) town.religion.deity = fetchDeity(town)
 }
 
-const pickDeity = (town: Town, pantheon: PantheonTypes): string => {
-  return random(lib.religion.pantheon[pantheon].gods).name
+export const fetchDeity = (town: Town, godArray?: Deity[]): string => {
+  // if (State.metadata.has('pantheon') === true) {
+  //   godArray = State.metadata.get('pantheon')
+  // }
+  if (!godArray) godArray = lib.religion.pantheon[town.religion.pantheon || 'greek'].gods
+  const predominantRace = getPredominantRace(town._demographicPercentile)
+  const temp: {
+    probability: number,
+    god: string
+  }[] = []
+  for (const god of godArray) {
+    temp.push({
+      probability: calcPercentage(god.probabilityWeightings?.npc.race[predominantRace.primaryRace] || 10, predominantRace.percentile),
+      god: god.name
+    })
+  }
+  const pickedDeity = weightedRandomFetcher(town, temp, undefined, undefined, 'object') as { probability: number, god: string }
+  return pickedDeity.god
 }
