0b84c57a054ff9deaafb4e67557ff5f5c7cfc7d5
Rhys <36913739+ryceg@users.noreply.github.com>
2021-02-14 11:19:23 +1100

    Fix pantheon not showing up

--- src/Religion/fetchPantheons.js
+++ src/Religion/fetchPantheons.js
@@ -57,3 +57,19 @@ setup.getCustomPantheon = () => {
     return State.metadata.get('pantheon')
   }
 }
+
+$(document).on(':dialogopened', function () {
+  if ($('#ui-dialog-body').hasClass('settings')) {
+    setup.addSettingButton({
+      target: 'showbiomegeneration',
+      name: 'pantheon',
+      description: 'Choose a pantheon to use.',
+      buttonDescription: 'Open pantheon settings'
+    },
+    () => setup.openDialog({
+      header: 'Pantheon Setup',
+      passage: 'ImportPantheon',
+      rerender: true
+    }))
+  }
+})
