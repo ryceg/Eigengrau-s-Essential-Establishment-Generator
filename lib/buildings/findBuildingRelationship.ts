c2d984bf58238e6fd32398cb5fb9a2dcaccd6495
ryceg <rhysg3@gmail.com>
2021-03-15 19:46:41 +1100

    Add relationships function, closes #546

--- lib/buildings/findBuildingRelationship.ts
+++ lib/buildings/findBuildingRelationship.ts
@@ -32,3 +32,19 @@ export function findBuildingRelationship (town: Town, building: Building | null,
 
   return foundRelationships
 }
+
+export function findIndexOfBuildingRelationship (town: Town, building: Building | null, npc: NPC | null) {
+  const foundRelationshipsIndex = town.buildingRelations.findIndex(relation => {
+    if (building && npc) {
+      return relation.buildingKey === building.key && relation.npcKey === npc.key
+    }
+    if (building) {
+      return relation.buildingKey === building.key
+    }
+    if (npc) {
+      return relation.npcKey === npc.key
+    }
+    return false
+  })
+  return foundRelationshipsIndex
+}
