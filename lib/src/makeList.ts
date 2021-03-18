5284b347764cc9fa60ab40735d06f5048a042fbd
ryceg <rhysg3@gmail.com>
2021-03-18 17:29:23 +1100

    Change makeList to accept limit

--- lib/src/makeList.ts
+++ lib/src/makeList.ts
@@ -1,10 +1,17 @@
-export const makeList = (array: string[], noAnd?: boolean): string => {
-  if (noAnd) return commas(array, 0)
+export const makeList = (array: string[], opts?: {
+  noAnd: boolean
+  limit: number
+}): string => {
+  opts = Object.assign({
+    noAnd: false,
+    limit: -1
+  }, opts)
+  if (opts.noAnd) return commas(array, 0)
   if (array.length === 1) return array[0]
   if (array.length === 2) return `${array[0]} and ${array[1]}`
   if (array.length === 3) return `${array[0]}, ${array[1]}, and ${array[2]}`
   if (array.length > 3) {
-    let temp = commas(array, 0, -1)
+    let temp = commas(array, 0, opts.limit)
     temp += `and ${array[array.length - 1]}`
     return temp
   }
