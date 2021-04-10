762284f737ce171990be54d0faa8f7da9d9234d4
ryceg <rhysg3@gmail.com>
2021-04-10 21:20:36 +1000

    Fix custom pantheon importing

--- lib/religion/getPantheon.ts
+++ lib/religion/getPantheon.ts
@@ -3,59 +3,62 @@ import { Town } from '../../lib/town/_common'
 import { compileWeightToPercentile, getDeityPercentagesList, getTownDeityWeightings } from './createTownReligion'
 import { Pantheon, religion } from './religion'
 
-export const getDeity = (town: Town, deity: string) => {
+export const getDeity = (town: Town, deity: string, customPantheon?: Pantheon) => {
   return findInArray(
-    getPantheon(town).gods, 'name', deity)
+    getPantheon(town, customPantheon).gods, 'name', deity)
 }
 
-export const getPantheon = (town: Town): Pantheon => {
-  if (isUsingCustomPantheon(town)) return getCustomPantheon(town)
+export const getPantheon = (town: Town, customPantheon?: Pantheon): Pantheon => {
+  if (isUsingCustomPantheon(town)) return getCustomPantheon(town, customPantheon)
   return religion.pantheon[town.religion.pantheon]
 }
 
-export const getPantheonNames = (town: Town) => {
-  return Object.keys(getAllPantheons(town))
+export const getPantheonNames = (town: Town, customPantheon?: Pantheon) => {
+  return Object.keys(getAllPantheons(town, customPantheon))
 }
 
-export const getAllPantheons = (town: Town) => {
+export const getAllPantheons = (town: Town, customPantheon?: Pantheon) => {
   const pantheons: Record<string, Pantheon> = Object.assign({}, religion.pantheon)
-  if (seeIfCustomPantheonExists(town)) pantheons[getCustomPantheon(town).name] = getCustomPantheon(town)
+  if (seeIfCustomPantheonExists(town, customPantheon)) pantheons[getCustomPantheon(town, customPantheon).name] = getCustomPantheon(town, customPantheon)
   return pantheons
 }
 
+/** If the pantheon being used doesn't exist in the data, it's obviously custom. */
 export const isUsingCustomPantheon = (town: Town) => {
   if (religion.pantheon[town.religion.pantheon]) return false
   return true
 }
 
-export const seeIfCustomPantheonExists = (town: Town): boolean => {
+export const seeIfCustomPantheonExists = (town: Town, customPantheon?: Pantheon): boolean => {
+  if (customPantheon) return true
   if (town.religion.customPantheon) return true
   return false
 }
 
-export const getCustomPantheon = (town: Town): Pantheon => {
+export const getCustomPantheon = (town: Town, customPantheon?: Pantheon): Pantheon => {
   if (town.religion.customPantheon) return town.religion.customPantheon
+  if (customPantheon) return customPantheon
   throw new Error('Custom panthon not defined!')
 }
 
-export const getPantheonPercentages = (town: Town) => {
+export const getPantheonPercentages = (town: Town, customPantheon?: Pantheon) => {
   console.log('Getting pantheon percentages...')
   return compileWeightToPercentile(
-    getTownDeityWeightings(town, getPantheon(town).gods)
+    getTownDeityWeightings(town, getPantheon(town, customPantheon).gods)
   )
 }
 
-export const getCulledPantheonPercentages = (town: Town) => {
+export const getCulledPantheonPercentages = (town: Town, customPantheon?: Pantheon) => {
   console.log('Getting pantheon percentages...')
   const temp = compileWeightToPercentile(
-    getTownDeityWeightings(town, getPantheon(town).gods)
+    getTownDeityWeightings(town, getPantheon(town, customPantheon).gods)
   )
   return Object.fromEntries(
     Object.entries(temp).filter(([, value]) => value > 0))
 }
 
-export const getPantheonPercentagesReadout = (town: Town) => {
-  const deities: [string, number][] = getDeityPercentagesList(getPantheonPercentages(town))
+export const getPantheonPercentagesReadout = (town: Town, customPantheon?: Pantheon) => {
+  const deities: [string, number][] = getDeityPercentagesList(getPantheonPercentages(town, customPantheon))
   let text = ''
   for (const [deity, percentage] of deities) {
     if (percentage > 0) {
