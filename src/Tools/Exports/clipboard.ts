32bcb7153f257971594867a3f36a4a3c9bac3717
ryceg <rhysg3@gmail.com>
2021-05-17 17:57:45 +1000

    Add ignore

--- src/Tools/Exports/clipboard.ts
+++ src/Tools/Exports/clipboard.ts
@@ -7,6 +7,8 @@ function updateClipboard (copyText: string) {
   navigator.clipboard.writeText(copyText).then(function () {
     $(document).trigger({
       type: ':notify',
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       message: 'Copied to the clipboard successfully!',
       time: false,
       classes: false
@@ -14,6 +16,8 @@ function updateClipboard (copyText: string) {
   }, function () {
     $(document).trigger({
       type: ':notify',
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       message: 'Copy to the clipboard failed :(',
       time: false,
       classes: false
