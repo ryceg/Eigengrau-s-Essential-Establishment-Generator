da8455cfcab0cd51651b552a3fe4408c8e514807
ryceg <rhysg3@gmail.com>
2021-06-20 16:22:49 +1000

    add func
    Signed-off-by: ryceg <rhysg3@gmail.com>

--- src/Dialog/openDialog.ts
+++ src/Dialog/openDialog.ts
@@ -6,9 +6,11 @@ interface DialogArguments {
   classNames?: string
   rerender?: true
   dialogOpts?: DialogOptions
+  func?(): unknown
 }
 
 export const openDialog = (args: DialogArguments) => {
+  if (args.func) args.func()
   Dialog.setup(args.header, args.classNames)
   Dialog.wiki(Story.get(args.passage).processText())
   if (args.rerender) {
