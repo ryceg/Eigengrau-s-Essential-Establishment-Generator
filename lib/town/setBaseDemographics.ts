9c7b3d2c89a8ca98afd5987070f9fb05ec60a9e7
Bret Buchanan <bretjb@gmail.com>
2022-03-24 07:16:24 -0500

    Removing unused and unnecessary file

--- lib/town/setBaseDemographics.ts
+++
@@ -1,9 +0,0 @@
-import { RaceName, Town } from '@lib'
-
-export const setBaseDemographics = (town: Town, newDemographics: Record<RaceName, number>) => {
-  console.log('Setting base demographics.')
-  Object.keys(newDemographics).forEach((byRace) => {
-    const race = byRace as RaceName
-    town._baseDemographics[race] = newDemographics[race]
-  })
-}
