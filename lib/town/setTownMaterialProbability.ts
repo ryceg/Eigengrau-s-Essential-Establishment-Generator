f2d43d2ca0382f983396d9861352e07b61c6a141
Victor Glindås <glinkis@gmail.com>
2022-04-01 23:13:51 +0200

    Revert fix: remove redundant initial town material probability assignment

--- lib/town/setTownMaterialProbability.ts
+++ lib/town/setTownMaterialProbability.ts
@@ -3,7 +3,7 @@ import { keys } from '../src/utils'
 import { Town } from './_common'
 
 export function setMaterialProbability (town: Town, possibleMaterials: MaterialTypes[]) {
-  for (const material of keys(structureMaterialData.types)) {
+  for (const material of keys(town.materialProbability)) {
     for (const possibleMaterial of possibleMaterials) {
       if (possibleMaterial !== material && !structureMaterialData.types[possibleMaterial].alwaysAvailable) {
         town.materialProbability[possibleMaterial].probability = 0
