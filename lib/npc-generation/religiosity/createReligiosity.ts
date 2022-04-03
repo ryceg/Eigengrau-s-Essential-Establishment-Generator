4f5a548c78bbfdab43fe47e17ef5b7bac219bc13
Victor Glindås <glinkis@gmail.com>
2022-04-03 12:54:41 +0200

    remove lib references from createReligiosity

--- lib/npc-generation/religiosity/createReligiosity.ts
+++ lib/npc-generation/religiosity/createReligiosity.ts
@@ -2,15 +2,16 @@
 import { Town } from '../../town/_common'
 import { NPC } from '../_common'
 import { random } from '../../src/random'
+import { dice, fm } from '../../src/dice'
+import { religion, ReligionStrength } from '../../religion/religion'
 import { getRolledFromTable } from '../../src/rollFromTable'
 import { getDeityProbabilities } from './getDeityProbabilities'
 import { pickDeity } from './pickDeity'
-import { ReligionStrength } from '../../religion/religion'
 
 // uses setup.npcData.religion.strength
 export function createReligiosity (town: Town, npc: NPC) {
   console.log(`Creating religion strength for ${npc.name}`)
-  npc.roll.religiosity = lib.fm(lib.dice(2, 40) + 10, town.roll.religiosity - 50)
+  npc.roll.religiosity = fm(dice(2, 40) + 10, town.roll.religiosity - 50)
   npc.roll.religiosity = Math.clamp(npc.roll.religiosity, 1, 100)
   if (npc.religion.strength) {
     npc.roll.religiosity = getReligiosity(npc.religion.strength)
@@ -21,7 +22,7 @@ export function createReligiosity (town: Town, npc: NPC) {
 }
 
 function getReligiosity (religionStrength: ReligionStrength): number {
-  for (const [threshold, strength] of lib.religion.strength) {
+  for (const [threshold, strength] of religion.strength) {
     if (strength === religionStrength) {
       return threshold + random(1, 5)
     }
@@ -30,5 +31,5 @@ function getReligiosity (religionStrength: ReligionStrength): number {
 }
 
 function getReligionStrength (religiosityRoll: number): ReligionStrength {
-  return getRolledFromTable(lib.religion.strength, religiosityRoll) || 'quiet true believer'
+  return getRolledFromTable(religion.strength, religiosityRoll) || 'quiet true believer'
 }
