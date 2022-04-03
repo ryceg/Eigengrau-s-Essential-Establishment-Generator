605e4baf9ac225ef26100d34277c8e3a4f0bba3e
Victor Glindås <glinkis@gmail.com>
2022-04-03 12:42:50 +0200

    remove lib reference from getRaceReadout

--- lib/town/getRaceReadout.ts
+++ lib/town/getRaceReadout.ts
@@ -1,5 +1,10 @@
-import { Town } from '@lib'
+import { articles } from '../src/articles'
+import { Town } from './_common'
+import { getTownType } from './getTownType'
+import { getPredominantRaceFromBase } from './getPredominantRace'
 
 export const getRaceReadout = (town: Town) => {
-  return `${town.name} is ${lib.articles.output(lib.getTownType(town))} comprised ${lib.getPredominantRaceFromBase(town.baseDemographics).amountDescriptive}.`
+  const townType = getTownType(town)
+  const predominantRace = getPredominantRaceFromBase(town.baseDemographics)
+  return `${town.name} is ${articles.output(townType)} comprised ${predominantRace.amountDescriptive}.`
 }
