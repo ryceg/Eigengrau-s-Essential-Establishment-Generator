cf2ab925d46f8ca85897b4ae192db79acd70b81f
Victor Glindås <glinkis@gmail.com>
2022-04-05 22:01:54 +0200

    formatting

--- lib/religion/Components/deityComponents.ts
+++ lib/religion/Components/deityComponents.ts
@@ -1,14 +1,16 @@
 import { Deity, DeityStatus } from '../religion'
 
-export const deityIsWas = (status: DeityStatus): string => {
+export function deityIsWas (status: DeityStatus): string {
   if (status === 'dead') return 'was'
   return 'is'
 }
-export const deityUsesUsed = (status: DeityStatus): string => {
+
+export function deityUsesUsed (status: DeityStatus): string {
   if (status === 'dead') return 'used'
   return 'uses'
 }
-export const deityStatus = (deity: Deity): string => {
+
+export function deityStatus (deity: Deity): string {
   if (deity.status === 'dead') return `${deity.name} is currently dead.`
   if (deity.status === 'imprisoned') return `${deity.name} is currently imprisoned.`
   if (deity.status === 'dormant') return `${deity.name} is currently dormant.`
