17b1439fca15e9bebd302342a496d5255cc0fb25
Victor Glindås <glinkis@gmail.com>
2022-04-06 20:38:11 +0200

    inline castle duneon name data

--- lib/castle/dungeonName.ts
+++ lib/castle/dungeonName.ts
@@ -1,9 +1,57 @@
-import jsonData from './dungeonName.data.json'
-export interface DungeonNameData {
+interface DungeonNameData {
   unique: string[]
   adjectives: string[]
   nouns: string[]
   verbs: string[]
   wordNoun: string[]
 }
-export const dungeonName: DungeonNameData = jsonData
+
+export const dungeonName: DungeonNameData = {
+  unique: [
+    'Crag Rock',
+    "Hell's Hole",
+    'Ashburn Grave',
+    "Hope's Lament",
+    "Killer's Cradle",
+    'Tenth Hell',
+    'Bloodrock',
+    'Folly Hole',
+    "Cerberus' Den",
+    'Necropolis',
+    'Blackthorn',
+    'The Spike'
+  ],
+  adjectives: [
+    'Dark',
+    'Cold',
+    'Penitence',
+    'Sorrowful',
+    'Death',
+    'Fear',
+    'Heart',
+    'Sunken',
+    'Revenant'
+  ],
+  nouns: [
+    'Blade',
+    'Blood',
+    'Door',
+    'Den',
+    'Pit',
+    'Jail',
+    'Prison',
+    'Cell'
+  ],
+  verbs: [
+    'Embrace',
+    'Kiss',
+    'Touch',
+    'Decay'
+  ],
+  wordNoun: [
+    'dungeon',
+    'oubliette',
+    'jail',
+    'prison'
+  ]
+}
