85cb0ce9b3ae24516cee7323d63e85aa0de73509
Victor Glindås <glinkis@gmail.com>
2022-04-07 04:09:35 +0200

    Inline complex castle json (#718)
    * inline castle ruler types
    
    * inline castle location data
    
    * inline castle siege data
    
    * inline castle dungeon prisoner data
    
    * remove obsolete json
    
    * inline castle dungeon jailer data
    
    * inline castle dungeon data
    
    * inline castle defense data
    
    * inline castle duneon name data
    
    * remove obsolete json
    
    * inline castle names data
    
    Co-authored-by: Rhys <36913739+ryceg@users.noreply.github.com>

--- lib/castle/castleDefense.ts
+++ lib/castle/castleDefense.ts
@@ -1,4 +1,3 @@
-import jsonData from './castleDefense.data.json'
 
 interface CastleDefenseData {
   reason: string[]
@@ -6,4 +5,37 @@ interface CastleDefenseData {
   outerWalls: string[]
 }
 
-export const castleDefense: CastleDefenseData = jsonData
+export const castleDefense: CastleDefenseData = {
+  reason: [
+    'the surrounding lands are part of a long-standing territorial dispute',
+    'the surrounding land is held sacred',
+    'the nearby lands are home to a rare herb, tree, or creature that has magical uses'
+  ],
+  outerWalls: [
+    'very high stone walls',
+    'incredibly thick stone walls',
+    'a series of curtain walls and gatehouses',
+    'a treacherous climb to reach the castle walls',
+    'a moat filled with putrescent water',
+    'a moat filled with thick, boot-sucking mud',
+    'a moat filled with sharp spikes',
+    'a moat that is home to one or more dangerous aquatic beasts',
+    'an immense barbican',
+    'a narrow footbridge to reach the postern',
+    'hidden pitfalls full of sharp spikes',
+    'a retractable drawbridge over a muddy moat',
+    'caged war dogs'
+  ],
+  innerWalls: [
+    'hundreds of arrow slits',
+    "one of the world's largest dual-portcullis gates",
+    'a winding climb to reach the entrance',
+    'several covered parapets with murder holes under which intruders must pass',
+    'a wide courtyard surrounded by flanking towers in the curtain wall',
+    'an unusual or hidden means of entry',
+    'several heavy-duty, mounted balistas',
+    'several mounted buckets of hot tar',
+    'a set of trapped stairs going up to the entrance that can be turned into a ramp',
+    'a set of heavy iron doors'
+  ]
+}
--- lib/castle/dungeon.ts
+++ lib/castle/dungeon.ts
@@ -1,6 +1,5 @@
-import jsonData from './dungeon.data.json'
 
-export interface DungeonData {
+ interface DungeonData {
   knownFor: string[]
   secret: string[]
   location: {
@@ -22,4 +21,137 @@ export interface DungeonData {
   }
 }
 
-export const dungeon: DungeonData = jsonData
+export const dungeon: DungeonData = {
+  knownFor: [
+    'many prisoners dying in a terrible plague',
+    'a mass escape in the past',
+    'the escape of a famous criminal',
+    'being the final home of a famous criminal',
+    'being the final home of a legendary hero',
+    'being haunted by vengeful ghosts',
+    'its horrific torture pits',
+    'never suffering a successful escape',
+    'its quirky jailer',
+    'the quality of its meals'
+  ],
+  secret: [
+    'a secret tunnel to the outside',
+    'the remnants of a long-lost hero',
+    'the preserved head of an ancient villain',
+    'a terrible beast to which prisoners are fed',
+    'a missing lord or lady',
+    'a famous jewel by a notorious prisoner',
+    'a unique and terrible torture device',
+    'the corpse of a deposed king or queen'
+  ],
+  location: {
+    castle: [
+      'in a tower set apart from the main castle',
+      'beneath the keep',
+      'in a flanking tower of the castle',
+      'beneath the flanking tower in the castle',
+      'in a corner tower of the castle',
+      'beneath the corner tower'
+    ],
+    standalone: [
+      'at the bottom of a deep ravine',
+      'on top of a cliff with a long drop down',
+      'on the inside of a bend in a river',
+      'built inside of a tall mountain',
+      'surrounded on three sides by mountains',
+      'on top of a hill in the middle of a city',
+      'suspended off of the underside of an overhang, dangling with a precarious path up to it',
+      'in the middle of a salt pan',
+      'deep into the steppes, days away from civilization',
+      'submerged in the middle of a lake - halfway to the bottom',
+      'chiseled into an enormous glacier',
+      'buried underneath sand in a desert, with few markers of its location',
+      'built into the branches of a series of trees',
+      'built inside a hollowed out giant tree',
+      'deep underground, near the bottom of a deep system of caverns',
+      'clinging on the roof of a giant cave with one set of stairs leading to the top'
+    ]
+  },
+  age: [
+    'as part of the original castle',
+    'as a later addition',
+    'for another purpose originally',
+    'long before most of the castle',
+    'by the original owner of the castle'
+  ],
+  format: [
+    'a sprawling maze of twisting passages',
+    'a sprawling maze of narrow passages',
+    'organized into small, neat rows of cells or pits',
+    'organized around a large central cell block or pit',
+    'only a few rooms',
+    'an endless series of long corridors',
+    'an endless series of small rooms and staircases',
+    'a series of corridors with very low ceilings'
+  ],
+  cells: {
+    prisoners: {
+      treatment: [
+        'humanely; they receive reasonable meals, some exercise, and healing when needed',
+        'like dogs; they receive poor quality meals and enough healing to keep them alive',
+        'like rats; they receive terrible meals and are plagued by sickness',
+        'like they don’t exist; occasionally they receive food'
+      ]
+    },
+    condition: [
+      'well-maintained; the walls are solid',
+      'aging, but sturdy; the walls have some cracks',
+      'decrepit; the walls are crumbling',
+      'so dark it’s difficult to say what condition they are in'
+    ],
+    format: [
+      'individual cells, in complete isolation',
+      'individual cells, but they can see and hear other prisoners',
+      'individual cells, but they can hear other prisoners',
+      'cells that accommodate up to two prisoners',
+      'cells that accommodate up to two prisoners, each shackled to the wall',
+      'cells that accommodate up to four prisoners',
+      'cells that accommodate up to four prisoners, each shackled to the wall',
+      'a large chamber with many other prisoners, each shackled to the wall',
+      'individual pits or wells, open at the top',
+      'one or more huge pit with many other prisoners'
+    ]
+  },
+  rooms: {
+    type: [
+      'a dungeon cell',
+      'another dungeon cell',
+      'a passageway connecting cell blocks',
+      'a guardroom',
+      'the barracks',
+      'the jailer’s quarters',
+      'a yard or large indoor space for exercise',
+      'a small dining room',
+      'an interrogation room',
+      'a torture chamber'
+    ],
+    feature: [
+      'a wooden door reinforced with steel bands',
+      'steel bars where you expected a stone wall',
+      'empty manacles along the wall',
+      'an empty sconce to hold a torch',
+      'distant torchlight',
+      'the floor is uneven',
+      'a crack in the stone floor',
+      'a mouse skittering underfoot',
+      'the stench of rotting flesh',
+      'the scent of stale urine',
+      'a putrid smell',
+      'a dank and moldy odor',
+      'an uncomfortable groaning',
+      'a faint scratching sound',
+      'an odd tapping sound',
+      'the squeaking of rats',
+      'the shouting of distant voices',
+      'howls of agony',
+      'horrific screams',
+      'the clanking of chains',
+      'the distinct smell of stale blood'
+    ]
+  }
+}
