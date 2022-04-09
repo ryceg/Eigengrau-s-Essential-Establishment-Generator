3df32bd448ccc11e98a3b5cd772499a2cad8be27
Victor Glindås <glinkis@gmail.com>
2022-04-09 14:21:29 +0200

    remove tax getters (#732)

--- lib/npc-generation/npcFinances.ts
+++ lib/npc-generation/npcFinances.ts
@@ -1,11 +1,9 @@
-import { logger } from '../logger'
 import { NPC } from './_common'
 import { calcPercentage } from '../src/calcPercentage'
 import { Town } from '../town/_common'
+import { getTaxRate } from '../town/getTaxRate'
 import { findProfession } from '../src/findProfession'
 import { LifestyleStandard, lifestyleStandards } from './lifestyleStandards'
-import { socialClass } from './socialClass'
-import { keys } from '../src/utils'
 
 /** `town.roll.wealth` increases or decreases npc.professionLuck by up to 10%, reflecting the strength of the economy. The expected range is between -25 and 25. */
 export function wageVariation (town: Town, npc: NPC): number {
@@ -21,7 +19,7 @@ export function npcGrossIncome (town: Town, npc: NPC): number {
 
 export function npcNetIncome (town: Town, npc: NPC): number {
   // logger.info(`Returning ${npc.name}'s net income...`)
-  return Math.round(calcPercentage(npcGrossIncome(town, npc), -npcTaxRate(town, npc)))
+  return Math.round(calcPercentage(npcGrossIncome(town, npc), -npcTaxRate(town)))
 }
 
 export function npcLifestyleStandard (town: Town, npc: NPC): LifestyleStandard {
@@ -50,18 +48,6 @@ export function npcProfit (town: Town, npc: NPC): number {
   return Math.round(npcNetIncome(town, npc) - npcTotalLifestyleExpenses(town, npc))
 }
 
-export function npcTaxRate (town: Town, npc: NPC): number {
-  let totalTax = 0
-
-  for (const tax of keys(town.taxes)) {
-    if (tax === 'land') {
-      totalTax += town.taxes[tax] * (socialClass[npc.socialClass].landRate || 1)
-    } else if (typeof town.taxes[tax] === 'number') {
-      totalTax += town.taxes[tax]
-    } else {
-      logger.error(`non-integer tax! ${town.taxes[tax]}`)
-    }
-  }
-
-  return Math.round(totalTax * 100) / 100
+export function npcTaxRate (town: Town): number {
+  return getTaxRate(town)
 }
