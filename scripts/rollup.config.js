8912bd40b2eba47ba3f9d88c648097dc29947396
Victor Glindås <glinkis@gmail.com>
2022-04-09 17:08:32 +0200

    use rollup esbuild plugin

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,29 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { default: babel } = require('@rollup/plugin-babel')
+const { default: esbuild } = require('rollup-plugin-esbuild')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
-const replace = require('@rollup/plugin-replace')
 const json = require('@rollup/plugin-json')
-const { terser } = require('rollup-plugin-terser')
 
 const env = process.env.NODE_ENV
 const isProduction = env === 'production'
 
 const plugins = [
-  // avoids issues with the Node-specific variable `process`.
-  replace({
-    'process.env.NODE_ENV': JSON.stringify(env),
-    'preventAssignment': true
-  }),
   json(),
-  babel({ extensions: ['.js', '.ts'], babelHelpers: 'bundled' }),
+  esbuild({
+    minify: isProduction,
+    target: 'es2018',
+    define: {
+      // avoids issues with the Node-specific variable `process`.
+      'process.env.NODE_ENV': JSON.stringify(env),
+      'preventAssignment': true
+    }
+  }),
   nodeResolve({ browser: true }),
-  commonjs({ extensions: ['.js', '.ts', '.json'] })
+  commonjs({ extensions: ['.js'] })
 ]
-
-if (isProduction) {
-  plugins.push(terser())
-}
 
 module.exports = [
   {
