39f83cf47567efd73f64b6f8d04e1cb178c6b4b0
Victor Glindås <glinkis@gmail.com>
2022-04-09 22:09:53 +0200

    update misc

--- lib/src/getIllustration.ts
+++ lib/src/getIllustration.ts
@@ -4,36 +4,18 @@ type Illustration =
 | 'town-illustration'
 | 'city-illustration'
 
-const getPath = () => {
-  if (process.env.NODE_ENV === 'production' && location.origin !== 'file://') {
-    return './'
-  }
-  return '../'
-}
-
-const addLocalSourceSet = (illustration: Illustration, sizes: string[] = ['360', '411', '500', '576', '768', '992', '1200']) => {
-  let img = ''
-  const path = getPath()
-  for (const size of sizes) {
-    img += `${path}src/Resources/img/hero/${illustration}-x${size}.jpg ${size}w, `
-  }
-  return img
-}
-
-export const getCustomImage = (url: URL, id = 'illustration') => {
+export const getCustomImage = (src: string, id = 'illustration') => {
   const img = document.createElement('img')
   img.id = id
-  img.src = url as unknown as string
+  img.src = src
   img.alt = 'A custom-defined image.'
   return img.outerHTML
 }
 
 export const getLocalImage = (illustration: Illustration) => {
   const img = document.createElement('img')
-  const path = getPath()
   img.id = 'illustration'
-  img.src = `${path}src/Resources/img/hero/${illustration}.jpg`
-  if (process.env.NODE_ENV === 'production' && location.origin !== 'file://') img.srcset = `${addLocalSourceSet(illustration)} ${path}src/Resources/img/hero/${illustration}.jpg`
+  img.src = `./Resources/img/hero/${illustration}.jpg`
   img.alt = `An image depicting ${lib.articles.output(illustration)}, created by artist Juho Huttunen.`
   return img.outerHTML
 }
