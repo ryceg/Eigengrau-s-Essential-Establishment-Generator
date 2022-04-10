ccf0104711c1593cc6865e821d8cd0dfdbc40efe
Victor Glindås <glinkis@gmail.com>
2022-04-10 12:09:25 +0200

    enable image source sets and sizes

--- lib/src/getIllustration.ts
+++ lib/src/getIllustration.ts
@@ -4,6 +4,18 @@ type Illustration =
 | 'town-illustration'
 | 'city-illustration'
 
+const addLocalSizes = (sizes: number[]) => {
+  return sizes.map(size => `(max-width: ${size}px) ${size}px`).join(', ')
+}
+
+const addLocalSourceSet = (illustration: Illustration, sizes: number[]) => {
+  const sources = sizes.map(size => {
+    return `./static/hero/${illustration}-${size}.jpg ${size}w`
+  })
+  sources.push(`./static/hero/${illustration}.jpg`)
+  return sources.join(', ')
+}
+
 export const getCustomImage = (src: string, id = 'illustration') => {
   const img = document.createElement('img')
   img.id = id
@@ -16,6 +28,8 @@ export const getLocalImage = (illustration: Illustration) => {
   const img = document.createElement('img')
   img.id = 'illustration'
   img.src = `./static/hero/${illustration}.jpg`
+  img.sizes = addLocalSizes([640])
+  img.srcset = addLocalSourceSet(illustration, [640])
   img.alt = `An image depicting ${lib.articles.output(illustration)}, created by artist Juho Huttunen.`
   return img.outerHTML
 }
