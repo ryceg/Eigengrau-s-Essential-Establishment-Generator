4401c4d8e25622a9c8e11eca943297510b880132
Rhys <36913739+ryceg@users.noreply.github.com>
2022-04-12 18:11:30 +1000

    add default rol modifier

---
+++ lib/src/defaultRollModifier.ts
@@ -0,0 +1,28 @@
+/**
+ * A nice little default scaffold to work off of.
+ * @param roll A roll bounded between 1 - 100
+ * @returns The modifier, which is intended to be consumed either as a fairmath, or taken straight.
+ */
+export function defaultRollModifier (roll: number, customTable?: [number, number][]): number {
+  const modifierTable: [number, number][] = [
+    [95, 35],
+    [90, 20],
+    [80, 15],
+    [70, 10],
+    [60, 5],
+    [55, 3],
+    [50, 1],
+    [45, -1],
+    [40, -2],
+    [30, -5],
+    [20, -10],
+    [10, -15],
+    [0, -20]
+  ]
+  for (const [rollValue, modifier] of customTable || modifierTable) {
+    if (roll >= rollValue) {
+      return modifier
+    }
+  }
+  throw new Error('Roll was out of bounds!')
+}
