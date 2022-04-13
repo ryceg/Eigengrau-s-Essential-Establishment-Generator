5f8c6d037ecb1a992675c859595452649ada0449
Victor Glindås <glinkis@gmail.com>
2022-04-13 20:01:17 +0200

    extract ship roll data to lib

--- src/Docks/docks.js
+++ src/Docks/docks.js
@@ -69,7 +69,7 @@ setup.initDocks = () => {
 
         const rollDataVariables = ['size', 'cleanliness']
         rollDataVariables.forEach(function (propName) {
-          lib.defineRollDataGetter(ship, setup.docks.ships.rollData[propName].rolls, propName)
+          lib.defineRollDataGetter(ship, lib.shipRollData[propName].rolls, propName)
         })
         return ship
       },
@@ -541,32 +541,6 @@ setup.initDocks = () => {
           ageStage: 'young adult',
           note: 'Inherited the ship.',
           hasClass: false
-        }
-      },
-      rollData: {
-        size: {
-          rolls: [
-          // it is a _______ $ship.type
-            [90, 'huge'],
-            [80, 'impressively sized'],
-            [60, 'somewhat impressive'],
-            [50, 'average sized'],
-            [30, 'somewhat unimpressive'],
-            [20, 'cluttered'],
-            [10, 'cluttered and cramped']
-          ]
-        },
-        cleanliness: {
-          rolls: [
-          // ... that is
-            [80, 'absolutely spotless; it must have been cleaned recently, as no barnacles adorn the bottom of the ship'],
-            [80, 'spotless, save for a couple barnacles'],
-            [70, 'in quite good condition, with only a couple barnacles clinging on to the bottom'],
-            [50, 'in reasonable condition, with some barnacles hanging on, creating drag at speed'],
-            [40, 'in need of a good clean, with barnacles clinging to the bottom of the ship'],
-            [30, 'in desperate need of a thorough de-barnacleing, as well as a general clean'],
-            [20, 'filthy and laden with barnacles covering the bottom of the boat']
-          ]
         }
       }
     }
