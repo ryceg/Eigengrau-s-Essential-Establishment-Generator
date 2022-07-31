9e0ea68c5d64eb233e3185c1190046bcf085c11b
Rhys <36913739+ryceg@users.noreply.github.com>
2022-07-31 14:01:30 +1000

    fix firefox

--- lib/isLocalCopy.ts
+++ lib/isLocalCopy.ts
@@ -1,6 +1,6 @@
 export function isLocalCopy (): boolean {
-  if (!window.location.origin.includes('file://') && window.location.origin !== null) {
-    return false
-  }
-  return true
+  if (window.location.origin.includes('file://')) return true
+  if (window.location.origin.includes('localhost')) return true
+  if (window.location.origin === 'null') return true
+  return false
 }
