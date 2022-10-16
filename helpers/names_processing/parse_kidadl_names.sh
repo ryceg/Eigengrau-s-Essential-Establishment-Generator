69c17d2ae38cd5ed9e80a5273a1ca34e25bf9339
badrath <writinh20@gmail.com>
2022-10-16 13:23:44 -0500

    cleaned up name processing files not useful for the Eigengraus application itself

--- helpers/names_processing/parse_kidadl_names.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-f_in="$1"
-
-# sed 's/\.//g' "$f_in" | sed 's/, .*$//g'
-sed -E "s/^[0-9]+\. //g" "$f_in" | sed "s/, .*$//g" | sed -E "s/ +\(.+$//g" | grep -v "^$" | sed "s/^/'/" | sed "s/$/',/" | sort
